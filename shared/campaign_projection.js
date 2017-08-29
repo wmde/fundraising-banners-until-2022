0db48b591bce0e87885255b415bce35ad9038cd3
Kai_WMDE <kai.nissen@wikimedia.de>
2017-08-29 17:40:55 +0200

    add number of donors projection

--- shared/campaign_projection.js
+++ shared/campaign_projection.js
@@ -1,8 +1,10 @@
-function CampaignProjection( campaignStartDate, campaignEndDate, baseDonationSum, donationAmountPerMinute, randomFactor ) {
+function CampaignProjection( campaignStartDate, campaignEndDate, baseDonationSum, donationAmountPerMinute, donorsBase, donorsPerMinute, randomFactor ) {
 	this.campaignStartDate = campaignStartDate;
 	this.campaignEndDate = campaignEndDate;
 	this.baseDonationSum = baseDonationSum;
 	this.donationAmountPerMinute = donationAmountPerMinute;
+	this.donorsBase = donorsBase;
+	this.donorsPerMinute = donorsPerMinute;
 	this.randomFactor = randomFactor || Math.floor( ( Math.random() ) + 0.5 - 0.2 );
 }
 
@@ -20,23 +22,23 @@ CampaignProjection.prototype.getProjectedDonationSum = function ( randomDeviatio
 	return minutesPassed * increasePerMinute + this.baseDonationSum;
 };
 
-function getApprDonatorsRaw( rand ) {
-	var startDonators = donorsBase,
-		secsPast = getSecsPassed();
+CampaignProjection.prototype.getApprDonatorsRaw = function ( rand ) {
+	const startDonators = this.donorsBase,
+		secsPast = this.getSecondsSinceCampaignStart();
 
-	return startDonators + getApprDonatorsFor( secsPast, rand );
-}
+	return startDonators + this.getApprDonatorsFor( secsPast, rand );
+};
 
-function getApprDonatorsFor( secsPast, rand ) {
-	var apprDonatorsMinute = donorsPerMinApproximation,
-		randFactor = 0;
+CampaignProjection.prototype.getApprDonatorsFor = function ( secsPast, rand ) {
+	const apprDonatorsMinute = this.donorsPerMinute;
+	let randFactor = 0;
 
 	if ( rand === true ) {
-		randFactor = Math.floor( ( Math.random() ) + 0.5 - 0.2 );
+		randFactor = this.randomFactor;
 	}
 
 	return ( secsPast / 60 * ( apprDonatorsMinute * ( 100 + randFactor ) ) / 100 );
-}
+};
 
 function getDigitGroupingCharacter() {
 	switch ( mw.config.get( 'wgContentLanguage' ) ) {
--- test/campaign_projection_test.js
+++ test/campaign_projection_test.js
@@ -5,6 +5,13 @@ const CampaignProjection = require( '../shared/campaign_projection' );
 
 describe( 'CampaignProjection', function () {
 
+	const BASE_DONATION_SUM = 123456,
+		DONATION_SUM_PER_MINUTE = 10,
+		DONORS_BASE_NUMBER = 1234,
+		DONORS_PER_MINUTE = 1,
+		PROJECTION_DEVIATION = 0.2,
+		NO_PROJECTION_DEVIATION = 0;
+
 	before( function () {
 		MockDate.set( new Date( '2017-11-05 00:00:00' ) );
 	} );
@@ -12,29 +19,58 @@ describe( 'CampaignProjection', function () {
 	describe( '#getProjectedDonationSum()', function () {
 
 		it( 'should return donation sum projection', function () {
-			const BASE_DONATION_SUM = 123456,
-				DONATION_SUM_PER_MINUTE = 10,
-				campaignProjection = new CampaignProjection(
-					new Date( '2017-11-01 00:00:00' ),
-					new Date( '2018-01-01 00:00:00' ),
-					BASE_DONATION_SUM,
-					DONATION_SUM_PER_MINUTE,
-					0
-				);
+			const campaignProjection = new CampaignProjection(
+				new Date( '2017-11-01 00:00:00' ),
+				new Date( '2018-01-01 00:00:00' ),
+				BASE_DONATION_SUM,
+				DONATION_SUM_PER_MINUTE,
+				DONORS_BASE_NUMBER,
+				DONORS_PER_MINUTE,
+				NO_PROJECTION_DEVIATION
+			);
 			assert.equal( campaignProjection.getProjectedDonationSum( false ), 181056 );
 		} );
 
 		it( 'should return deviating donation sum projection', function () {
-			const BASE_DONATION_SUM = 123456,
-				DONATION_SUM_PER_MINUTE = 10,
-				campaignProjection = new CampaignProjection(
-					new Date( '2017-11-01 00:00:00' ),
-					new Date( '2018-01-01 00:00:00' ),
-					BASE_DONATION_SUM,
-					DONATION_SUM_PER_MINUTE,
-					0.2
-				);
+			const campaignProjection = new CampaignProjection(
+				new Date( '2017-11-01 00:00:00' ),
+				new Date( '2018-01-01 00:00:00' ),
+				BASE_DONATION_SUM,
+				DONATION_SUM_PER_MINUTE,
+				DONORS_BASE_NUMBER,
+				DONORS_PER_MINUTE,
+				PROJECTION_DEVIATION
+			);
 			assert.equal( campaignProjection.getProjectedDonationSum( true ), 181171.2 );
+		} );
+
+	} );
+
+	describe( '#getProjectedNumberOfDonors()', function () {
+		it( 'should return number of donor projection', function () {
+			const campaignProjection = new CampaignProjection(
+				new Date( '2017-11-01 00:00:00' ),
+				new Date( '2018-01-01 00:00:00' ),
+				BASE_DONATION_SUM,
+				DONATION_SUM_PER_MINUTE,
+				DONORS_BASE_NUMBER,
+				DONORS_PER_MINUTE,
+				NO_PROJECTION_DEVIATION
+			);
+			assert.equal( campaignProjection.getApprDonatorsRaw( false ), 6994 );
+		} );
+
+		it( 'should return deviating number of donor projection', function () {
+			const campaignProjection = new CampaignProjection(
+				new Date( '2017-11-01 00:00:00' ),
+				new Date( '2018-01-01 00:00:00' ),
+				BASE_DONATION_SUM,
+				DONATION_SUM_PER_MINUTE,
+				DONORS_BASE_NUMBER,
+				DONORS_PER_MINUTE,
+				PROJECTION_DEVIATION
+			);
+			assert.equal( campaignProjection.getApprDonatorsRaw( true ), 7005.52 );
 		} );
 
 	} );
