6dec66297474ccbd38ae524df6d0b80688461499
Kai_WMDE <kai.nissen@wikimedia.de>
2017-08-30 12:36:08 +0200

    use options object as constructor parameter

--- shared/campaign_projection.js
+++ shared/campaign_projection.js
@@ -1,13 +1,12 @@
-function CampaignProjection( campaignStartDate, campaignEndDate, baseDonationSum, donationAmountPerMinute,
-							 donorsBase, donorsPerMinute, digitGroupingCharacter, randomFactor ) {
-	this.campaignStartDate = campaignStartDate;
-	this.campaignEndDate = campaignEndDate;
-	this.baseDonationSum = baseDonationSum;
-	this.donationAmountPerMinute = donationAmountPerMinute;
-	this.donorsBase = donorsBase;
-	this.donorsPerMinute = donorsPerMinute;
-	this.digitGroupingCharacter = digitGroupingCharacter;
-	this.randomFactor = randomFactor || Math.floor( ( Math.random() ) + 0.5 - 0.2 );
+function CampaignProjection( options ) {
+	this.campaignStartDate = options.campaignStartDate || new Date();
+	this.campaignEndDate = options.campaignEndDate || new Date();
+	this.baseDonationSum = options.baseDonationSum || 0;
+	this.donationAmountPerMinute = options.donationAmountPerMinute || 0;
+	this.donorsBase = options.donorsBase || 0;
+	this.donorsPerMinute = options.donorsPerMinute || 0;
+	this.digitGroupingCharacter = options.digitGroupingCharacter || '.';
+	this.deviation = options.deviation || Math.floor( ( Math.random() ) + 0.5 - 0.2 );
 }
 
 CampaignProjection.prototype.getSecondsSinceCampaignStart = function () {
@@ -19,14 +18,14 @@ CampaignProjection.prototype.getSecondsSinceCampaignStart = function () {
 
 CampaignProjection.prototype.getProjectedDonationSum = function ( randomDeviation ) {
 	return this.formatNumber(
-		this.calculateProjection( this.baseDonationSum, this.donationAmountPerMinute, randomDeviation ? this.randomFactor : 0 ),
+		this.calculateProjection( this.baseDonationSum, this.donationAmountPerMinute, randomDeviation ? this.deviation : 0 ),
 		this.digitGroupingCharacter
 	);
 };
 
 CampaignProjection.prototype.getProjectedNumberOfDonors = function ( randomDeviation ) {
 	return this.formatNumber(
-		this.calculateProjection( this.donorsBase, this.donorsPerMinute, randomDeviation ? this.randomFactor : 0 ),
+		this.calculateProjection( this.donorsBase, this.donorsPerMinute, randomDeviation ? this.deviation : 0 ),
 		this.digitGroupingCharacter
 	);
 };
--- test/campaign_projection_test.js
+++ test/campaign_projection_test.js
@@ -11,16 +11,16 @@ describe( 'CampaignProjection', function () {
 		PROJECTION_DEVIATION = 0.2,
 		GERMAN_DIGIT_GROUPING_CHARACTER = '.',
 		ENGLISH_DIGIT_GROUPING_CHARACTER = ',',
-		campaignProjection = new CampaignProjection(
-			new Date( '2017-11-01 00:00:00' ),
-			new Date( '2018-01-01 00:00:00' ),
-			BASE_DONATION_SUM,
-			DONATION_SUM_PER_MINUTE,
-			DONORS_BASE_NUMBER,
-			DONORS_PER_MINUTE,
-			GERMAN_DIGIT_GROUPING_CHARACTER,
-			PROJECTION_DEVIATION
-		);
+		defaultOptions = {
+			campaignStartDate: new Date( '2017-11-01 00:00:00' ),
+			campaignEndDate: new Date( '2018-01-01 00:00:00' ),
+			baseDonationSum: BASE_DONATION_SUM,
+			donationAmountPerMinute: DONATION_SUM_PER_MINUTE,
+			donorsBase: DONORS_BASE_NUMBER,
+			donorsPerMinute: DONORS_PER_MINUTE,
+			digitGroupingCharacter: GERMAN_DIGIT_GROUPING_CHARACTER,
+			deviation: PROJECTION_DEVIATION
+		};
 
 	before( function () {
 		MockDate.set( new Date( '2017-11-05 00:00:00' ) );
@@ -29,25 +29,18 @@ describe( 'CampaignProjection', function () {
 	describe( '#getProjectedDonationSum()', function () {
 
 		it( 'should return donation sum projection', function () {
+			const campaignProjection = new CampaignProjection( defaultOptions );
 			assert.equal( campaignProjection.getProjectedDonationSum( false ), '181.056' );
 		} );
 
 		it( 'should return deviating donation sum projection', function () {
+			const campaignProjection = new CampaignProjection( defaultOptions );
 			assert.equal( campaignProjection.getProjectedDonationSum( true ), '181.171' );
 		} );
 
 		it( 'should return deviating donation sum projection with digits grouped by comma', function () {
-			const campaignProjection = new CampaignProjection(
-				new Date( '2017-11-01 00:00:00' ),
-				new Date( '2018-01-01 00:00:00' ),
-				BASE_DONATION_SUM,
-				DONATION_SUM_PER_MINUTE,
-				DONORS_BASE_NUMBER,
-				DONORS_PER_MINUTE,
-				ENGLISH_DIGIT_GROUPING_CHARACTER,
-				PROJECTION_DEVIATION
-			);
-
+			const options = Object.assign( {}, defaultOptions, { digitGroupingCharacter: ENGLISH_DIGIT_GROUPING_CHARACTER } ),
+				campaignProjection = new CampaignProjection( options );
 			assert.equal( campaignProjection.getProjectedDonationSum( true ), '181,171' );
 		} );
 
@@ -55,24 +48,18 @@ describe( 'CampaignProjection', function () {
 
 	describe( '#getProjectedNumberOfDonors()', function () {
 		it( 'should return number of donor projection', function () {
+			const campaignProjection = new CampaignProjection( defaultOptions );
 			assert.equal( campaignProjection.getProjectedNumberOfDonors( false ), '6.994' );
 		} );
 
 		it( 'should return deviating number of donor projection', function () {
+			const campaignProjection = new CampaignProjection( defaultOptions );
 			assert.equal( campaignProjection.getProjectedNumberOfDonors( true ), '7.005' );
 		} );
 
 		it( 'should return deviating number of donor projection with digits grouped by comma', function () {
-			const campaignProjection = new CampaignProjection(
-				new Date( '2017-11-01 00:00:00' ),
-				new Date( '2018-01-01 00:00:00' ),
-				BASE_DONATION_SUM,
-				DONATION_SUM_PER_MINUTE,
-				DONORS_BASE_NUMBER,
-				DONORS_PER_MINUTE,
-				ENGLISH_DIGIT_GROUPING_CHARACTER,
-				PROJECTION_DEVIATION
-			);
+			const options = Object.assign( {}, defaultOptions, { digitGroupingCharacter: ENGLISH_DIGIT_GROUPING_CHARACTER } ),
+				campaignProjection = new CampaignProjection( options );
 			assert.equal( campaignProjection.getProjectedNumberOfDonors( true ), '7,005' );
 		} );
 
