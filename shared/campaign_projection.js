aa28874e25dba901259f5c1251c0992481a526b8
Kai_WMDE <kai.nissen@wikimedia.de>
2017-08-30 13:37:30 +0200

    hide 'private' methods

--- shared/campaign_projection.js
+++ shared/campaign_projection.js
@@ -1,3 +1,20 @@
+function getSecondsSinceCampaignStart( that ) {
+	const maxSecs = Math.floor( ( that.campaignEndDate.getTime() - that.campaignStartDate.getTime() ) / 1000 ),
+		secsPassed = Math.floor( ( new Date().getTime() - that.campaignStartDate.getTime() ) / 1000 );
+
+	return Math.max( 0, Math.min( maxSecs, secsPassed ) );
+}
+
+function calculateProjection( that, baseValue, increasePerMinute, randomFactor ) {
+	const minutesPassed = getSecondsSinceCampaignStart( that ) / 60,
+		increase = increasePerMinute * ( 100 + randomFactor ) / 100;
+	return minutesPassed * increase + baseValue;
+}
+
+function formatNumber( that, number ) {
+	return parseInt( number ).toString().replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + that.digitGroupingCharacter );
+}
+
 function CampaignProjection( options ) {
 	this.campaignStartDate = options.campaignStartDate || new Date();
 	this.campaignEndDate = options.campaignEndDate || new Date();
@@ -9,35 +26,18 @@ function CampaignProjection( options ) {
 	this.deviation = options.deviation || Math.floor( ( Math.random() ) + 0.5 - 0.2 );
 }
 
-CampaignProjection.prototype.getSecondsSinceCampaignStart = function () {
-	const maxSecs = Math.floor( ( this.campaignEndDate.getTime() - this.campaignStartDate.getTime() ) / 1000 ),
-		secsPassed = Math.floor( ( new Date().getTime() - this.campaignStartDate.getTime() ) / 1000 );
-
-	return Math.max( 0, Math.min( maxSecs, secsPassed ) );
-};
-
 CampaignProjection.prototype.getProjectedDonationSum = function ( randomDeviation ) {
-	return this.formatNumber(
-		this.calculateProjection( this.baseDonationSum, this.donationAmountPerMinute, randomDeviation ? this.deviation : 0 ),
-		this.digitGroupingCharacter
+	return formatNumber(
+		this,
+		calculateProjection( this, this.baseDonationSum, this.donationAmountPerMinute, randomDeviation ? this.deviation : 0 )
 	);
 };
 
 CampaignProjection.prototype.getProjectedNumberOfDonors = function ( randomDeviation ) {
-	return this.formatNumber(
-		this.calculateProjection( this.donorsBase, this.donorsPerMinute, randomDeviation ? this.deviation : 0 ),
-		this.digitGroupingCharacter
+	return formatNumber(
+		this,
+		calculateProjection( this, this.donorsBase, this.donorsPerMinute, randomDeviation ? this.deviation : 0 )
 	);
-};
-
-CampaignProjection.prototype.calculateProjection = function ( baseValue, increasePerMinute, randomFactor ) {
-	const minutesPassed = this.getSecondsSinceCampaignStart() / 60,
-		increase = increasePerMinute * ( 100 + randomFactor ) / 100;
-	return minutesPassed * increase + baseValue;
-};
-
-CampaignProjection.prototype.formatNumber = function ( number ) {
-	return parseInt( number ).toString().replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + this.digitGroupingCharacter );
 };
 
 module.exports = CampaignProjection;
