340b5e04ceedae1501cccc5f8205a75cf867103b
Kai_WMDE <kai.nissen@wikimedia.de>
2017-08-30 12:16:38 +0200

    refactor unit test

--- test/campaign_projection_test.js
+++ test/campaign_projection_test.js
@@ -1,5 +1,4 @@
 const MockDate = require( 'mockdate' );
-
 const assert = require('assert');
 const CampaignProjection = require( '../shared/campaign_projection' );
 
@@ -12,7 +11,16 @@ describe( 'CampaignProjection', function () {
 		PROJECTION_DEVIATION = 0.2,
 		GERMAN_DIGIT_GROUPING_CHARACTER = '.',
 		ENGLISH_DIGIT_GROUPING_CHARACTER = ',',
-		NO_PROJECTION_DEVIATION = 0;
+		campaignProjection = new CampaignProjection(
+			new Date( '2017-11-01 00:00:00' ),
+			new Date( '2018-01-01 00:00:00' ),
+			BASE_DONATION_SUM,
+			DONATION_SUM_PER_MINUTE,
+			DONORS_BASE_NUMBER,
+			DONORS_PER_MINUTE,
+			GERMAN_DIGIT_GROUPING_CHARACTER,
+			PROJECTION_DEVIATION
+		);
 
 	before( function () {
 		MockDate.set( new Date( '2017-11-05 00:00:00' ) );
@@ -21,30 +29,10 @@ describe( 'CampaignProjection', function () {
 	describe( '#getProjectedDonationSum()', function () {
 
 		it( 'should return donation sum projection', function () {
-			const campaignProjection = new CampaignProjection(
-				new Date( '2017-11-01 00:00:00' ),
-				new Date( '2018-01-01 00:00:00' ),
-				BASE_DONATION_SUM,
-				DONATION_SUM_PER_MINUTE,
-				DONORS_BASE_NUMBER,
-				DONORS_PER_MINUTE,
-				GERMAN_DIGIT_GROUPING_CHARACTER,
-				NO_PROJECTION_DEVIATION
-			);
 			assert.equal( campaignProjection.getProjectedDonationSum( false ), '181.056' );
 		} );
 
 		it( 'should return deviating donation sum projection', function () {
-			const campaignProjection = new CampaignProjection(
-				new Date( '2017-11-01 00:00:00' ),
-				new Date( '2018-01-01 00:00:00' ),
-				BASE_DONATION_SUM,
-				DONATION_SUM_PER_MINUTE,
-				DONORS_BASE_NUMBER,
-				DONORS_PER_MINUTE,
-				GERMAN_DIGIT_GROUPING_CHARACTER,
-				PROJECTION_DEVIATION
-			);
 			assert.equal( campaignProjection.getProjectedDonationSum( true ), '181.171' );
 		} );
 
@@ -59,6 +47,7 @@ describe( 'CampaignProjection', function () {
 				ENGLISH_DIGIT_GROUPING_CHARACTER,
 				PROJECTION_DEVIATION
 			);
+
 			assert.equal( campaignProjection.getProjectedDonationSum( true ), '181,171' );
 		} );
 
@@ -66,30 +55,10 @@ describe( 'CampaignProjection', function () {
 
 	describe( '#getProjectedNumberOfDonors()', function () {
 		it( 'should return number of donor projection', function () {
-			const campaignProjection = new CampaignProjection(
-				new Date( '2017-11-01 00:00:00' ),
-				new Date( '2018-01-01 00:00:00' ),
-				BASE_DONATION_SUM,
-				DONATION_SUM_PER_MINUTE,
-				DONORS_BASE_NUMBER,
-				DONORS_PER_MINUTE,
-				GERMAN_DIGIT_GROUPING_CHARACTER,
-				NO_PROJECTION_DEVIATION
-			);
 			assert.equal( campaignProjection.getProjectedNumberOfDonors( false ), '6.994' );
 		} );
 
 		it( 'should return deviating number of donor projection', function () {
-			const campaignProjection = new CampaignProjection(
-				new Date( '2017-11-01 00:00:00' ),
-				new Date( '2018-01-01 00:00:00' ),
-				BASE_DONATION_SUM,
-				DONATION_SUM_PER_MINUTE,
-				DONORS_BASE_NUMBER,
-				DONORS_PER_MINUTE,
-				GERMAN_DIGIT_GROUPING_CHARACTER,
-				PROJECTION_DEVIATION
-			);
 			assert.equal( campaignProjection.getProjectedNumberOfDonors( true ), '7.005' );
 		} );
 
