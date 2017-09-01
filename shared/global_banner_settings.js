faa2f097b90644431b0c12a06ddd9e43d86a513a
Kai_WMDE <kai.nissen@wikimedia.de>
2017-09-01 13:33:26 +0200

    keep previously used value key for legacy reasons

--- shared/global_banner_settings.js
+++ shared/global_banner_settings.js
@@ -5,7 +5,7 @@ module.exports = {
     'donators-base': 0,
     'appr-donations-per-minute': 10,
     'appr-donators-per-minute': 1,
-    'impressions-per-day-in-million': 8,
+    'impressions-per-day-in-million': 8, 'milion-impressions-per-day': 8, /** @deprecated kept for legacy reasons */
     allBannersCookie: 'banner_impression_count',
     singleBannerCookie: 'single_banner_impression_count',
     'campaign-start-date': '2017-11-06',
