23c51a4fb5004fdb107c7a287b8fd2b511341727
Pablo Grass <pablo.grass@wikimedia.de>
2017-09-22 17:43:43 +0200

    consistent indentation

--- webpack.config.js
+++ webpack.config.js
@@ -5,27 +5,29 @@ const CommonConfig = require( './webpack.common.js' );
 const webpack = require( 'webpack' );
 
 module.exports = Merge( CommonConfig, {
-  entry: {
-    'loader': './webpack/loader.js'
-  },
-  plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.DefinePlugin( {
-          CAMPAIGNS: JSON.stringify( toml.parse( fs.readFileSync( 'campaign_info.toml', 'utf8' ) ) )
-      } )
-  ],
-  devServer: {
-      port: 8084,
-      hot: true,
-      contentBase: './dist',
-	  headers: {
-		  'Access-Control-Allow-Origin': '*'
-	  },
-      proxy: [{
-          context: [ '/wiki', '/w', '/static' ],
-          target: 'https://de.wikipedia.org',
-          secure: false,
-          headers: { host:'de.wikipedia.org' }
-      }]
-  }
+	entry: {
+		'loader': './webpack/loader.js'
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.DefinePlugin( {
+			CAMPAIGNS: JSON.stringify( toml.parse( fs.readFileSync( 'campaign_info.toml', 'utf8' ) ) )
+		} )
+	],
+	devServer: {
+		port: 8084,
+		hot: true,
+		contentBase: './dist',
+		headers: {
+			'Access-Control-Allow-Origin': '*'
+		},
+		proxy: [
+			{
+				context: [ '/wiki', '/w', '/static' ],
+				target: 'https://de.wikipedia.org',
+				secure: false,
+				headers: { host: 'de.wikipedia.org' }
+			}
+		]
+	}
 } );
