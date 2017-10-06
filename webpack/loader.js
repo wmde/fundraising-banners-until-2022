dd0a7e6ec3481ac03550be447467ea4074586534
Pablo Grass <pablo.grass@wikimedia.de>
2017-10-06 17:47:13 +0200

    simplified loader

--- webpack/loader.js
+++ webpack/loader.js
@@ -8,19 +8,14 @@ const CampaignConfig = require( './campaign_config' );
 const bannerSelectionListTemplate = require( './banner_selection_list.hbs' );
 
 const currentUrl = url.parse( window.location.href, true );
-const container = $( '#WMDE-Banner-Container' );
 
 const Handlebars = require('handlebars/runtime');
 Handlebars.registerHelper('bannerlink', function( campaign, bannername ) {
 	return campaign.preview_link.replace( '{{banner}}', bannername );
 });
 
-function getMediawikiBannerLinksHtml( ) {
-	return bannerSelectionListTemplate( { campaigns: CAMPAIGNS } );
-}
-
 if ( !currentUrl.query.banner && !currentUrl.query.bannertest ) {	// different wikipedia.org & wikipedia.de banner params
-	$( 'body' ).html( getMediawikiBannerLinksHtml() );
+	$( 'body' ).html( bannerSelectionListTemplate( { campaigns: CAMPAIGNS } ) );
 }
 
 // inject tracking data for current banner.
@@ -29,6 +24,7 @@ if ( currentUrl.query.devbanner ) {
 	const campaigns = new CampaignConfig( CAMPAIGNS );
 	const pages = campaigns.getConfigForPages();
 	const currentBanner = currentUrl.query.devbanner;
+	const container = $( '#WMDE-Banner-Container' );
 	if ( pages[ currentBanner ] ) {
 		console.log( 'current banner', currentBanner, pages[ currentBanner ].tracking, pages[ currentBanner ].campaign_tracking );
 		container.data( 'tracking', pages[ currentBanner ].tracking );
@@ -37,4 +33,3 @@ if ( currentUrl.query.devbanner ) {
 }
 
 // TODO banner loading functions for non-proxied development (with `localbanner` param)
-
