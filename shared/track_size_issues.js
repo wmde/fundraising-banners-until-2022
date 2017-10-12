7572246832bcc9447ceee7d35ca056a885db394a
Kai_WMDE <kai.nissen@wikimedia.de>
2017-10-12 13:07:43 +0200

    extract tracking data generation

--- shared/track_size_issues.js
+++ shared/track_size_issues.js
@@ -6,44 +6,46 @@ function SizeIssueIndicator( thresholdInPixels ) {
 
 /**
  * Get number of pixels remaining in viewport after the banner height was subtracted.
- * @param {jQuery} bannerElement
+ * @param {jQuery} $bannerElement
  * @return {number}
  */
-function getVisibleWikipedia( bannerElement ) {
-	return $( window ).height() - bannerElement.height();
+function getVisibleWikipedia( $bannerElement ) {
+	return $( window ).height() - $bannerElement.height();
 }
 
 /**
+ * Get banner height and screen/window dimensions as a concatenated string
+ * @param {number} bannerHeight
+ * @return {string} tracking data
+ */
+SizeIssueIndicator.prototype.generateTrackingData = function( bannerHeight ) {
+	return [
+		bannerHeight,
+		$( window ).width() + 'x' + $( window ).height(),
+		screen.width + 'x' + screen.height,
+		window.outerWidth + 'x' + window.outerHeight
+	].join( '--' )
+};
+
+/**
  * Check if Banner takes too much screen space and track the incident
- * @param {jQuery} bannerElement
+ * @param {jQuery} $bannerElement
  * @param {string} trackingLink
  * @param {number} trackRatio
  */
-SizeIssueIndicator.prototype.trackSizeIssues = function( bannerElement, trackingLink, trackRatio ) {
-	var bannerHeight = bannerElement.height(),
-		viewportHeight = $( window ).height(),
-		viewportWidth = $( window ).width(),
-		threshold = this.thresholdInPixels, // Pixels
-		resolutions = '';
-	
-	if ( getVisibleWikipedia( bannerElement ) > threshold ) {
+SizeIssueIndicator.prototype.trackSizeIssues = function( $bannerElement, trackingLink, trackRatio ) {
+	if ( getVisibleWikipedia( $bannerElement ) > this.thresholdInPixels ) {
 		return;
 	}
 	if ( Math.random() > trackRatio ) {
 		return;
 	}
 	// Avoid multiple tracking when trackSpaceIssues is called multiple times
-	if ( $( '#WMDE_Banner-track-sizes', bannerElement ).length > 0 ) {
+	if ( $( '#WMDE_Banner-track-sizes', $bannerElement ).length > 0 ) {
 		return;
 	}
-	resolutions = [
-		bannerHeight,
-		viewportWidth + 'x' + viewportHeight,
-		screen.width + 'x' + screen.height,
-		window.outerWidth + 'x' + window.outerHeight
-	].join( '--' );
-	
-	var trackUrl = trackingLink.replace( /\/$/, '' ) + '/' + resolutions;
+
+	var trackUrl = trackingLink.replace( /\/$/, '' ) + '/' + this.generateTrackingData( $bannerElement.height() );
 	bannerElement.append( $( '<img id="WMDE_Banner-track-sizes" width="1" height="1">' ).attr( 'src', trackUrl ) );
 };
 
