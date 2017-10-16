876c3452e2c00f6869439bd0f5934018807c6705
Pablo Grass <pablo.grass@wikimedia.de>
2017-10-16 18:57:36 +0200

    describing the interface without violations

--- shared/skin/Skin.js
+++ shared/skin/Skin.js
@@ -1,12 +1,25 @@
 'use strict';
 
 module.exports = class Skin {
-	addSpace( bannerHeight ) {
+	/**
+	 * Children must implement this
+	 *
+	 * @param {number} bannerHeight
+	 */
+	addSpace( bannerHeight ) { // eslint-disable-line no-unused-vars
 	}
 
-	addSpaceInstantly( bannerHeight ) {
+	/**
+	 * Children must implement this
+	 *
+	 * @param {number} bannerHeight
+	 */
+	addSpaceInstantly( bannerHeight ) { // eslint-disable-line no-unused-vars
 	}
 
+	/**
+	 * Children must implement this
+	 */
 	removeSpace() {
 	}
 
