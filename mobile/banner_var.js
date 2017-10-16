dc9917c9a60f5d35edfdf0d0d483ec1161128003
Jeroen De Dauw <jeroendedauw@gmail.com>
2017-10-16 14:23:33 +0200

    Updated mobile CTRL and VAR for Sofort test
    https://phabricator.wikimedia.org/T176720
    
    * Renamed Überweisung to Überweisungszusage
    * Added Sofort-Überweisung button that goes to fun app with payment type SUB
    * SMS button is now 50% width instead of 100%
    * CTRL adds pmt=0 to url when sending user to fun app

--- mobile/banner_var.js
+++ mobile/banner_var.js
@@ -31,7 +31,7 @@ const getCustomDayName = require( '../shared/custom_day_name' );
 const animateHighlight = require( '../shared/animate_highlight' );
 import TrackingEvents from '../shared/tracking_events';
 
-const bannerTemplate = require('./templates/banner_html.hbs');
+const bannerTemplate = require('./templates/banner_html_var.hbs');
 
 const $ = require( 'jquery' );
 
@@ -109,6 +109,17 @@ $( '#btn-bez' ).click( function () {
 	var $checkedAmountElement = $( 'input[name=betrag_auswahl]:checked' );
 	if( $checkedAmountElement.length > 0 ) {
 		$( '#zahlweise' ).val( 'BEZ' );
+		$( '#betrag' ).val( $checkedAmountElement.val() );
+		$( '#form' ).submit();
+	} else {
+		alert( 'Bitte wählen Sie einen Spendenbetrag aus.' );
+	}
+} );
+
+$( '#btn-sofort' ).click( function () {
+	var $checkedAmountElement = $( 'input[name=betrag_auswahl]:checked' );
+	if( $checkedAmountElement.length > 0 ) {
+		$( '#zahlweise' ).val( 'SUB' );
 		$( '#betrag' ).val( $checkedAmountElement.val() );
 		$( '#form' ).submit();
 	} else {
