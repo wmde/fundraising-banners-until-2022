c18a3f9c21cba7574f46015b3d518e65d51cfa4b
Pablo Grass <pablo.grass@wikimedia.de>
2017-10-16 18:28:39 +0200

    rm unused function (redundant l10n)

--- shared/banner_functions.js
+++ shared/banner_functions.js
@@ -87,27 +87,6 @@ module.exports = function ( GlobalBannerSettings, Translations ) {
 		}
 	}
 
-	function getCurrentDayInEnglish() {
-		switch ( new Date().getDay() ) {
-			case 0:
-				return 'Sunday';
-			case 1:
-				return 'Monday';
-			case 2:
-				return 'Tuesday';
-			case 3:
-				return 'Wednesday';
-			case 4:
-				return 'Thursday';
-			case 5:
-				return 'Friday';
-			case 6:
-				return 'Saturday';
-			default:
-				return '';
-		}
-	}
-
 	function getDigitGroupingCharacter() {
 		switch ( mw.config.get( 'wgContentLanguage' ) ) {
 			case 'de':
