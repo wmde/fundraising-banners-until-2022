6add4051ff7dfdf71856f960dca890f1d7721843
Gabriel Birke <gabriel.birke@wikimedia.de>
2017-11-06 11:41:37 +0100

    Copy desktop banners to EN
    Start with some customizations, but the english banner still needs work.

---
+++ shared/messages/en.js
@@ -0,0 +1,8 @@
+const Translations = {
+	'no-interval-message': 'Please select a donation interval.',
+	'amount-empty-message': 'Please select a donation amount.',
+	'amount-too-low-message': 'The minimum donation amount is 1 Euro.',
+	'amount-too-high-message': 'The donation amount is too high.'
+};
+
+export default Translations;
