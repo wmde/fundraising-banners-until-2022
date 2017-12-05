bcca1349a32de55f154ede886c61d4cb2af56c0b
WMDE-Fisch <christoph.jauera@wikimedia.de>
2017-12-05 17:30:03 +0100

    Initial commit christmas tree banner
    - html and css layout changed to tree mock
    - added a progress christmas tree
     - js code here is basically the progress bar just set to height
    - added interactive presents to change custom amount
     - number off presents can be increased and reduced
     - code could be seperated
    - layout for different display sizes are added
     - an very small displays the layout changes to show buttons
       in the form instead of presents

---
+++ shared/progress_tree/progress_tree.js
@@ -0,0 +1,100 @@
+require( './style.pcss' );
+
+/**
+ * Display a progress tree
+ *
+ * Next refactoring steps:
+ * - Set "--lateprogress" modifier depending on the space the "inner" element would need vs the remaining free
+ *      space inside the progress bar.
+ * - Use campaign_days.js from https://github.com/wmde/fundraising-banners/pull/27 to calculate remaining days
+ * - Convert to ES2015 class
+ *
+ * @param {object} GlobalBannerSettings
+ * @param {CampaignProjection} campaignProjection
+ * @param {object} options
+ * @constructor
+ */
+function ProgressTree( GlobalBannerSettings, campaignProjection, options ) {
+	this.GlobalBannerSettings = GlobalBannerSettings;
+	this.campaignProjection = campaignProjection;
+	this.options = Object.assign( {
+		minSpace: 90
+	}, options || {} );
+}
+
+ProgressTree.prototype.animate = function () {
+	var donationFillElement = $( '.tree__white' ),
+		donationValueElement = $( '.js-donation_value' ),
+		remainingValueElement = $( '.js-value_remaining' ),
+		donationTarget, donationsCollected, donationsRemaining;
+
+	donationFillElement.clearQueue();
+	donationFillElement.stop();
+
+	donationTarget = this.GlobalBannerSettings.goalSum;
+	donationsCollected = this.campaignProjection.getProjectedDonationSum();
+
+	if ( donationsCollected > donationTarget ) {
+		donationsCollected = donationTarget;
+	}
+	donationsRemaining = donationTarget - donationsCollected;
+
+	donationFillElement.animate(
+		{
+			height: this.getSpaceToFill( donationsCollected, donationTarget, donationFillElement.parent().height() )
+		},
+		{
+			duration: 3000,
+			progress: function ( animation, progress ) {
+				var aCollected = donationsCollected * progress,
+					aRemaining = donationTarget - aCollected;
+				remainingValueElement.html( formatMillion( aRemaining ) );
+				donationValueElement.html( formatMillion( aCollected ) );
+			},
+			complete: function () {
+				remainingValueElement.html( formatMillion( donationsRemaining ) );
+				donationValueElement.html( formatMillion( donationsCollected ) );
+				$( '.progress_bar' ).addClass( 'progress_bar--finished' );
+			}
+		}
+	);
+};
+
+/**
+ *
+ * @param {Number} donationsCollected
+ * @param {Number} donationsTarget
+ * @param {Number} containerSpace
+ * @return {string} Min space in pixel or fill space in percent
+ */
+ProgressTree.prototype.getSpaceToFill = function ( donationsCollected, donationsTarget, containerSpace ) {
+	var spaceToFill = ( donationsCollected / donationsTarget ) * 100,
+		barFilled = containerSpace * ( donationsCollected / donationsTarget );
+	return barFilled > this.options.minSpace ? spaceToFill + '%' : this.options.minSpace + 'px';
+};
+
+function formatMillion( value ) {
+	return ( value / 1000000 ).toFixed( 1 ).replace( '.', getDecimalSeparator );
+}
+
+function getDecimalSeparator() {
+	switch ( mw.config.get( 'wgContentLanguage' ) ) {
+		case 'de':
+			return ',';
+		case 'en':
+			return '.';
+		default:
+			return ',';
+	}
+}
+
+ProgressTree.prototype.render = function () {
+	const template = require( './template.hbs' );
+
+	return template( {
+		donated: '<span class="js-donation_value">0,0</span> Mio. €',
+		missing: '<span class="js-value_remaining">1,2</span> Mio. €'
+	} );
+};
+
+module.exports = ProgressTree;
