1d5a640810b746c2844159d8226f396769a9a129
Gabriel Birke <gabriel.birke@gmail.com>
2018-08-31 23:11:14 +0200

    Split featurebox code into functions
    Group functions together:
    * Update DOM
    * Handle DOM events
    * Process server-side result
    * etc

--- wikipedia.de_featurebox/banner.js
+++ wikipedia.de_featurebox/banner.js
@@ -1,62 +1,69 @@
 require( './css/styles.pcss' );
 
 const bannerTemplate = require( './templates/banner.hbs' );
-const $bannerContainer = $( '.feature' );
+const SUBMIT_URL = 'https://spenden.wikimedia.de/contact/subscribe';
 
-$bannerContainer.html( bannerTemplate() );
+function showSuccess() {
+	$( '.call-to-action' ).hide();
+	$( '.call-to-action-confirmation' ).show();
+}
+
+function showFailure( errorMessage ) {
+	$( '.call-to-action-error' ).text( errorMessage );
+}
+
+function isEmail( email ) {
+	return email.indexOf( '@' ) > 0;
+}
+
+function createErrorMessageFromServerErrors( errors ) {
+	if ( !errors ) {
+		return 'Beim Eintragen der Adresse ist ein Fehler aufgetreten.';
+	}
+
+	if ( errors.subscription_duplicate_subscription ) {
+		return 'Die angegebene E-Mail-Adresse wurde bereits eingetragen.';
+	}
+
+	return Object.keys( errors ).reduce( ( errmsg, key ) => {
+		return errmsg + errors[ key ] + '<br />';
+	}, '' );
+}
+
+function processServerMessage( message ) {
+	if ( message.status === 'OK' ) {
+		showSuccess();
+		return;
+	}
+	showFailure( createErrorMessageFromServerErrors( message.errors ) );
+}
+
+function submitEmail( event ) {
+	const email = $.trim( $( '#call-to-action-input' ).val() );
+	if ( isEmail( email ) === false ) {
+		showFailure( 'Bitte geben Sie eine gültige E-Mail-Adresse ein.' );
+		event.preventDefault();
+		return;
+	}
+
+	$( '#call-to-action-email' ).text( email );
+
+	$.ajax( {
+		url: SUBMIT_URL,
+		data: { email: email },
+		dataType: 'jsonp',
+		success: processServerMessage
+	} );
+}
 
 $( document ).ready( function () {
-	const errorElement = $( '.call-to-action-error' );
-	registerEvents();
+	$( '.feature' ).html( bannerTemplate() );
 
-	function registerEvents() {
-		let button = $( '#call-to-action-button' );
-
-		$( 'textarea' ).keyup( function ( e ) {
-			if ( e.keyCode === 13 ) {
-				button.trigger( 'click' );
-			}
-		} );
-		button.on( 'click', function () {
-			const email = $.trim( $( '#call-to-action-input' ).val() );
-			if ( isEmail( email ) === false ) {
-				errorElement.text( 'Bitte geben Sie eine gültige E-Mail-Adresse ein.' );
-				return;
-			}
-			$( '#call-to-action-email' ).text( email );
-			$.ajax( {
-				url: 'https://spenden.wikimedia.de/contact/subscribe',
-				data: { email: email },
-				dataType: 'jsonp',
-				jsonpCallback: 'subscriptionCallback'
-			} );
-		} );
-	}
-
-	window.subscriptionCallback = function ( message ) {
-		if ( message.status === 'OK' ) {
-			$( '.call-to-action' ).hide();
-			$( '.call-to-action-confirmation' ).show();
-			return;
+	$( 'textarea' ).keyup( function ( e ) {
+		if ( e.keyCode === 13 ) {
+			submitEmail();
 		}
+	} );
 
-		if ( message.errors.subscription_duplicate_subscription ) {
-			errorElement.text( 'Die angegebene E-Mail-Adresse wurde bereits eingetragen.' );
-			return;
-		}
-
-		errorElement.text( '' );
-		if ( message.errors ) {
-			$.each( message.errors, function ( key, value ) {
-				errorElement.append( value + '</br>' );
-			} );
-			return;
-		}
-
-		errorElement.text( 'Beim Eintragen der Adresse ist ein Fehler aufgetreten.' );
-	};
-
-	function isEmail( email ) {
-		return /^(([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+)?$/.test( email );
-	}
+	$( '#call-to-action-button' ).click( submitEmail );
 } );
