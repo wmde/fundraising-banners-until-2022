b7a7f46784f162cb652d9379fd499447ce635a6e
Tim Eulitz <tim.eulitz@wikimedia.de>
2018-09-26 17:18:59 +0200

    Prepare C18_WMDE_Test_mob02 banners
    Moving slider banner into mobile directory

--- mobile/banner_var.js
+++ mobile/banner_var.js
@@ -1,31 +1,43 @@
 /* eslint no-alert: 1 */
 
 require( './css/styles_var.pcss' );
-require( './css/styles.pcss' );
-require( './css/styles_mini.pcss' );
-
-// BEGIN Banner-Specific configuration
-const bannerCloseTrackRatio = 0.01;
-const searchBoxTrackRatio = 0.01;
-const LANGUAGE = 'de';
-// END Banner-Specific configuration
 
 import EventLoggingTracker from '../shared/event_logging_tracker';
 import CampaignDays, { startOfDay, endOfDay } from '../shared/campaign_days';
 import CampaignDaySentence from '../shared/campaign_day_sentence';
 import InterruptibleTimeout from '../shared/interruptible_timeout';
 import DayName from '../shared/day_name';
+import Translations from '../shared/messages/de';
+import { Slider } from './banner_var_slider';
 
+const $ = require( 'jquery' );
 const DevGlobalBannerSettings = require( '../shared/global_banner_settings' );
 const GlobalBannerSettings = window.GlobalBannerSettings || DevGlobalBannerSettings;
-import Translations from '../shared/messages/de';
 const BannerFunctions = require( '../shared/banner_functions' )( GlobalBannerSettings, Translations );
+const formatNumber = require( 'format-number' );
+const CampaignProjection = require( '../shared/campaign_projection' );
+const bannerTemplate = require( './templates/banner_html_var.hbs' );
+
+// BEGIN Banner-Specific configuration
+const bannerClickTrackRatio = 0.01;
+const bannerCloseTrackRatio = 0.01;
+const searchBoxTrackRatio = 0.01;
+const LANGUAGE = 'de';
+const sliderAutoPlaySpeed = 2000;
+const fullscreenBannerSlideInSpeed = 1250;
+// END Banner-Specific configuration
+
+/**
+ * Slider wrapper object holding a Flickity-based slider
+ * @type {Slider}
+ */
+const bannerSlider = new Slider( sliderAutoPlaySpeed );
+
 const campaignDays = new CampaignDays(
 	startOfDay( GlobalBannerSettings[ 'campaign-start-date' ] ),
 	endOfDay( GlobalBannerSettings[ 'campaign-end-date' ] )
 );
 const campaignDaySentence = new CampaignDaySentence( campaignDays, LANGUAGE, 14 );
-const CampaignProjection = require( '../shared/campaign_projection' );
 const campaignProjection = new CampaignProjection(
 	new CampaignDays(
 		startOfDay( GlobalBannerSettings[ 'donations-date-base' ] ),
@@ -39,7 +51,6 @@ const campaignProjection = new CampaignProjection(
 	}
 );
 
-const formatNumber = require( 'format-number' );
 const donorFormatter = formatNumber( { round: 0, integerSeparator: '.' } );
 
 const dayName = new DayName( new Date() );
@@ -48,25 +59,10 @@ const weekdayPrepPhrase = dayName.isSpecialDayName() ? Translations[ 'day-name-p
 
 const animateHighlight = require( '../shared/animate_highlight' );
 
-const bannerTemplate = require( './templates/banner_html_var.hbs' );
-
-const $ = require( 'jquery' );
-
 const $bannerContainer = $( '#WMDE-Banner-Container' );
 const CampaignName = $bannerContainer.data( 'campaign-tracking' );
 const BannerName = $bannerContainer.data( 'tracking' );
-const ProgressBar = require( '../shared/progress_bar/progress_bar' );
-const numberOfDaysUntilCampaignEnd = campaignDays.getNumberOfDaysUntilCampaignEnd();
-const progressBarTextInnerLeft = [
-	Translations[ 'prefix-days-left' ],
-	numberOfDaysUntilCampaignEnd,
-	numberOfDaysUntilCampaignEnd > 1 ? Translations[ 'day-plural' ] : Translations[ 'day-singular' ],
-	Translations[ 'suffix-days-left' ]
-].join( ' ' );
-const progressBar = new ProgressBar( GlobalBannerSettings, campaignProjection, {
-	textInnerLeft: progressBarTextInnerLeft,
-	modifier: 'progress_bar--lateprogress'
-} );
+
 const bannerDisplayTimeout = new InterruptibleTimeout();
 
 $bannerContainer.html( bannerTemplate( {
@@ -76,109 +72,152 @@ $bannerContainer.html( bannerTemplate( {
 	campaignDaySentence: campaignDaySentence.getSentence(),
 	amountBannerImpressionsInMillion: GlobalBannerSettings[ 'impressions-per-day-in-million' ],
 	CampaignName: CampaignName,
-	BannerName: BannerName,
-	progressBar: progressBar.render()
+	BannerName: BannerName
 } ) );
 
 const trackingEvents = new EventLoggingTracker( BannerName );
-trackingEvents.trackClickEvent( $( '.mini-banner' ), 'mobile-mini-banner-expanded' );
-trackingEvents.trackClickEvent( $( '.mini-banner__close-button' ), 'banner-closed', bannerCloseTrackRatio );
+trackingEvents.trackClickEvent( $( '.mini-banner' ), 'mobile-mini-banner-expanded', bannerClickTrackRatio );
+trackingEvents.trackClickEvent( $( '#mini-banner-close-button' ), 'banner-closed', bannerCloseTrackRatio );
 
 // BEGIN form initialization
+
 $( '#impCount' ).val( BannerFunctions.increaseImpCount() );
 $( '#bImpCount' ).val( BannerFunctions.increaseBannerImpCount( BannerName ) );
 
-// Reset "other box" if they click a different amount
-$( '#amount1, #amount2, #amount3, #amount4, #amount5' ).click( function () {
-	$( '#input_amount_other_box' ).val( '' );
+$( '.select-group__option' ).click( function () {
+	$( '#betrag' ).val( $( 'input[name=betrag_auswahl]:checked' ).val() );
 } );
 
 // Disable Sofort-Überweisung payment option if chosen interval is not 'one time'
 $( 'input[name=interval]' ).click( function () {
+	const subPaymentButton = $( 'button[data-payment-type=SUB]' );
 	if ( $( this ).attr( 'id' ) !== 'interval_onetime' ) {
-		$( 'button[data-payment-type=SUB]' ).attr( 'disabled', true ).addClass( 'disabled' );
+		// Reset payment method selection when Sofortüberweisung was previously selected
+		if ( subPaymentButton.hasClass( 'selected-option' ) ) {
+			$( '#zahlweise' ).val( '' );
+		}
+		subPaymentButton.removeClass( 'selected-option' );
+		subPaymentButton.attr( 'disabled', true );
 	} else {
-		$( 'button[data-payment-type=SUB]' ).attr( 'disabled', false ).removeClass( 'disabled' );
+		subPaymentButton.attr( 'disabled', false );
 	}
 } );
 
-$( '.button-group__container button' ).click( function ( event ) {
-	var $checkedAmountElement = $( 'input[name=betrag_auswahl]:checked' ).val(),
-		$checkedIntervalElement = $( 'input[name=interval]:checked' ).val();
-	if ( !isValidAmount( $checkedAmountElement ) ) {
+$( '.interval-selection input' ).click( function ( event ) {
+	$( '.interval-selection .selected-option' ).removeClass( 'selected-option' );
+	$( event.target ).addClass( 'selected-option' );
+	$( '#periode' ).val( $( 'input[name=interval]:checked' ).val() );
+} );
+
+$( '.amount-selection button' ).click( function ( event ) {
+	$( '.amount-selection .selected-option' ).removeClass( 'selected-option' );
+	$( event.target ).addClass( 'selected-option' );
+	$( '#betrag' ).val( $( 'input[name=betrag_auswahl]:checked' ).val() );
+} );
+
+$( '.payment-selection button' ).click( function ( event ) {
+	event.preventDefault();
+	$( '.payment-selection .selected-option' ).removeClass( 'selected-option' );
+	$( event.target ).addClass( 'selected-option' );
+	$( '#zahlweise' ).val( $( event.target ).data( 'payment-type' ) );
+} );
+
+$( '#banner-form-submit' ).click( function () {
+	if ( !$( '#periode' ).val() || $( '.interval-selection .selected-option' ).length === 0 ) {
+		alert( 'Bitte wählen Sie ein Zahlungsinterval aus.' );
+		return false;
+	}
+	if ( !$( '#betrag' ).val() ) {
 		alert( 'Bitte wählen Sie einen Spendenbetrag aus.' );
 		return false;
-	} else if ( !isValidPeriod( $checkedIntervalElement ) ) {
-		alert( 'Bitte wählen Sie zuerst ein Zahlungsintervall.' );
-		return false;
-	} else {
-		$( '#zahlweise' ).val( $( event.target ).data( 'payment-type' ) );
-		$( '#betrag' ).val( $checkedAmountElement );
-		$( '#periode' ).val( $checkedIntervalElement );
-		return true;
 	}
+	if ( !$( '#zahlweise' ).val() ) {
+		alert( 'Bitte wählen Sie ein Zahlungsmittel aus.' );
+		return false;
+	}
+	return true;
 } );
+
 // END form initialization
-
-function isValidAmount( amount ) {
-	return amount > 0;
-}
-
-function isValidPeriod( interval ) {
-	return typeof interval !== 'undefined';
-}
 
 function displayMiniBanner() {
 
 	const miniBanner = $( '.mini-banner' );
-	const bannerHeight = miniBanner.height();
-	miniBanner.css( 'top', 0 - bannerHeight ).show();
+	const bannerHeight = miniBanner.outerHeight() + 40;
 
-	miniBanner.animate( {
-		top: 0
-	}, 1000 );
+	// Banner starts in far off screen and needs to be reset, workaround to get sliders to work
+	miniBanner.css( 'top', -bannerHeight );
+	miniBanner.animate( { top: 0 }, 1000 );
 
-	$( '#mw-mf-viewport' ).animate( {
-		marginTop: bannerHeight
-	}, 1000 );
+	$( '#mw-mf-viewport' ).animate( { marginTop: bannerHeight }, 1000 );
 
 	$( 'head' ).append( '<style>#mw-mf-viewport .overlay.media-viewer { margin-top: ' + ( 0 - bannerHeight ) + 'px }</style>' );
+
+	// Making sure automatic sliding only starts after slider is shown to the user
+	bannerSlider.enableAutoplay();
 }
 
+/**
+ * Hides mini banner and slides down full-screen banner
+ * Animation is split into two parts:
+ * First it slides to the viewport, next it pushes the viewport further down along with the fullscreen banner
+ */
 function displayFullBanner() {
+	$( '.mini-banner' ).hide();
 	window.scrollTo( 0, 0 );
-	$( '#mw-mf-viewport' ).css( { marginTop: 0 } );
-	$( '#frbanner' ).show();
-	$( '.mini-banner' ).slideToggle();
 
-	progressBar.animate();
-	window.setTimeout( function () {
-		animateHighlight( $( '#to-highlight' ), 'highlight', 10 );
-	}, 3000 );
+	const viewport = $( '#mw-mf-viewport' );
+	const viewportOffset = viewport.css( 'margin-top' ).slice( 0, -2 );
+	const fullscreenBanner = $( '.frbanner-window' );
+	const bannerHeight = fullscreenBanner.outerHeight();
+
+	// Placing fullscreen banner above of the page
+	fullscreenBanner.css( 'top', -bannerHeight );
+
+	// Calculating the percentage which the viewport was already pushed by the mini-banner
+	const viewPortOffsetFactor = viewportOffset / bannerHeight;
+
+	// Calculating the time that is needed to reach the previous mini-banner viewport offset without affecting the perceived speed of the banner
+	const initialSlideTime = fullscreenBannerSlideInSpeed * viewPortOffsetFactor;
+
+	// Pushing the fullscreen banner to where the mini banner previously was, aligning it with the viewport
+	fullscreenBanner.animate( { top: viewportOffset - bannerHeight },
+		initialSlideTime, 'linear' ).queue( function () {
+
+		// Calculating the remaining time at which the banner has to finish the slide to match the speed of the previous animation
+		const remainingSlideTime = fullscreenBannerSlideInSpeed * ( 1 - viewPortOffsetFactor );
+
+		// Now that the viewport and banner are aligned, both of them are pushed further down simultaneously
+		viewport.animate( { marginTop: bannerHeight }, remainingSlideTime, 'linear' );
+		fullscreenBanner.dequeue();
+		fullscreenBanner.animate( { top: 0 }, remainingSlideTime, 'linear' ).queue( function () {
+			// Once fullscreen banner is fully shown, the contents are animated
+			setTimeout( function () {
+				animateHighlight( $( '#to-highlight' ), 'highlight', 10 );
+			}, 500 );
+		} );
+
+	} );
 }
 
 $( document ).ready( function () {
 	$( 'body' ).prepend( $( '#centralNotice' ) );
 
-	bannerDisplayTimeout.run( displayMiniBanner, $( '#WMDE-Banner-Container' ).data( 'delay' ) || 5000 );
-
 	$( '#frbanner-close' ).click( function () {
-		// Close only the full-screen
+		$( '#mw-mf-viewport' ).css( { marginTop: 0 } );
 		$( '#frbanner' ).hide();
 	} );
 
-	$( '.mini-banner__close-button' ).click( function () {
+	$( '#mini-banner-close-button' ).click( function () {
 		$( '.mini-banner' ).hide();
 		BannerFunctions.removeBannerSpace();
-
 		if ( BannerFunctions.onMediaWiki() ) {
 			mw.centralNotice.hideBanner();
 		}
-
 		return false;
 	} );
 
-	$( '.mini-banner' ).click( displayFullBanner );
+	bannerSlider.initialize();
 
 	BannerFunctions.getSkin().addSearchObserver( function () {
 		bannerDisplayTimeout.cancel();
@@ -187,4 +226,8 @@ $( document ).ready( function () {
 		trackingEvents.createTrackHandler( 'search-box-used', searchBoxTrackRatio )();
 	} );
 
+	const bannerDelay = $( '#WMDE-Banner-Container' ).data( 'delay' ) || 5000;
+	bannerDisplayTimeout.run( displayMiniBanner, bannerDelay );
+
+	$( '.mini-banner-tab, .mini-banner .banner-headline' ).click( displayFullBanner );
 } );
