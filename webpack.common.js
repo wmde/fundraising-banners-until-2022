8bfd274dd837821ca8bc3831231d615f77ac7971
Gabriel Birke <gabriel.birke@gmail.com>
2018-11-01 19:29:31 +0100

    Add linebreaks to webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -19,7 +19,11 @@ module.exports = {
 	},
 	module: {
 		rules: [
-			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
 			{
 				test: /\.html$/,
 				use: 'html-loader'
