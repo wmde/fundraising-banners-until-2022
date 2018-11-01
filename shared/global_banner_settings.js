e206aefc5764d4124375b83986ea45913f0088eb
Gabriel Birke <gabriel.birke@gmail.com>
2018-11-01 19:30:08 +0100

    Use semi-realistic global settings

--- shared/global_banner_settings.js
+++ shared/global_banner_settings.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'goalSum': 7900000,
-	'donations-date-base': '2018-04-18',
-	'donations-collected-base': 0,
+	'donations-date-base': '2018-10-25',
+	'donations-collected-base': 10000,
 	'donators-base': 0,
 	'appr-donations-per-minute': 104,
 	'appr-donators-per-minute': 4.805,
@@ -9,7 +9,7 @@ module.exports = {
 	'impressions-per-day-in-million': 6,
 	'allBannersCookie': 'banner_impression_count',
 	'singleBannerCookie': 'single_banner_impression_count',
-	'campaign-start-date': '2018-04-01',
-	'campaign-end-date': '2018-04-31',
+	'campaign-start-date': '2018-10-01',
+	'campaign-end-date': '2018-12-31',
 	'banner-close-track-ratio': 1
 };
