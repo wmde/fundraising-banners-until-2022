f129fb0c931a8ada0c53e3986df2baf153c5b11a
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2018-11-13 13:44:43 +0100

    Remove lightbox and add external link

--- english/banner_var.js
+++ english/banner_var.js
@@ -262,20 +262,6 @@ $( '#impCount' ).val( impCount );
 var bannerImpCount = BannerFunctions.increaseBannerImpCount( BannerName );
 $( '#bImpCount' ).val( bannerImpCount );
 
-// Lightbox
-$( '#application-of-funds-link' ).wlightbox( {
-	container: $( '#mw-page-base' ),
-	right: ( $( 'body' ).width() - 750 ) / 2 + 'px',
-	top: function () {
-		return ( $( '#WMDE_Banner' ).height() + 20 ) + 'px';
-	}
-} );
-
-$( '#application-of-funds-link' ).click( function () {
-	// Lightbox position is relative to banner position
-	window.scrollTo( 0, 0 );
-} );
-
 // track lightbox link clicking and banner closing
 trackingEvents.trackClickEvent( $( '#application-of-funds-link' ), 'application-of-funds-lightbox-opened' );
 trackingEvents.trackClickEvent( $( '#link-wmf-annual-plan' ), 'wmf-annual-plan' );
