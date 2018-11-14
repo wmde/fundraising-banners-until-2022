24c319cc612ab45623990c5389b65b725001d3f4
Tim Eulitz <tim.eulitz@wikimedia.de>
2018-11-14 18:17:48 +0100

    Prepare Campaign C18_WMDE_Test_12

--- desktop/banner_var.js
+++ desktop/banner_var.js
@@ -1,5 +1,4 @@
 require( './css/styles.pcss' );
-require( './css/styles_var.pcss' );
 require( './css/icons.css' );
 require( './css/wlightbox.css' );
 
@@ -76,8 +75,6 @@ $bannerContainer.html( bannerTemplate( {
 	progressBar: progressBar.render()
 } ) );
 
-const $bannerForm = $( '#WMDE_Banner-form' );
-
 // BEGIN form init code
 
 const trackingEvents = new EventLoggingTracker( BannerName );
@@ -87,32 +84,32 @@ function setupValidationEventHandling() {
 	banner.on( 'validation:amount:ok', function () {
 		$( '#WMDE_Banner-amounts-error-text' ).hide();
 		$( '#WMDE_Banner-amounts' ).parent().removeClass( 'select-group-container--with-error' );
-		addSpaceInstantly( banner );
+		addSpaceInstantly();
 	} );
 	banner.on( 'validation:amount:error', function ( evt, text ) {
 		$( '#WMDE_Banner-amounts-error-text' ).text( text ).show();
 		$( '#WMDE_Banner-amounts' ).parent().addClass( 'select-group-container--with-error' );
-		addSpaceInstantly( banner );
+		addSpaceInstantly();
 	} );
 	banner.on( 'validation:period:ok', function () {
 		$( '#WMDE_Banner-frequency-error-text' ).hide();
 		$( '#WMDE_Banner-frequency' ).parent().removeClass( 'select-group-container--with-error' );
-		addSpaceInstantly( banner );
+		addSpaceInstantly();
 	} );
 	banner.on( 'validation:period:error', function ( evt, text ) {
 		$( '#WMDE_Banner-frequency-error-text' ).text( text ).show();
 		$( '#WMDE_Banner-frequency' ).parent().addClass( 'select-group-container--with-error' );
-		addSpaceInstantly( banner );
+		addSpaceInstantly();
 	} );
 	banner.on( 'validation:paymenttype:ok', function () {
 		$( '#WMDE_Banner-payment-type-error-text' ).hide();
 		$( '#WMDE_Banner-payment-type' ).parent().removeClass( 'select-group-container--with-error' );
-		addSpaceInstantly( banner );
+		addSpaceInstantly();
 	} );
 	banner.on( 'validation:paymenttype:error', function ( evt, text ) {
 		$( '#WMDE_Banner-payment-type-error-text' ).text( text ).show();
 		$( '#WMDE_Banner-payment-type' ).parent().addClass( 'select-group-container--with-error' );
-		addSpaceInstantly( banner );
+		addSpaceInstantly();
 	} );
 }
 function appendEuroSign( field ) {
@@ -121,28 +118,12 @@ function appendEuroSign( field ) {
 		$( field ).val( $( field ).val() + ' €' );
 	}
 }
-
-function getPaymentType() {
-	return $( 'input[name=zahlweise]:checked', document.donationForm ).val();
-}
-
-function updateFormattedAmount() {
-	const amount = BannerFunctions.getAmount();
-	if ( getPaymentType() === 'BEZ' ) {
-		$( '#betrag' ).val( amount );
-	} else if ( amount ) {
-		$( '#betrag' ).val( ( Number( amount ) ).toFixed( 2 ).replace( '.', ',' ) );
-	}
-
-}
-
 function setupAmountEventHandling() {
 	var banner = $( '#WMDE_Banner' );
 	// using delegated events with empty selector to be markup-independent and still have corrent value for event.target
 	banner.on( 'amount:selected', null, function () {
 		$( '#amount-other-input' ).val( '' );
 		$( '.select-group__custom-input' ).removeClass( 'select-group__custom-input--value-entered' );
-		updateFormattedAmount();
 		BannerFunctions.hideAmountError();
 	} );
 
@@ -151,18 +132,11 @@ function setupAmountEventHandling() {
 		var input = $( '.select-group__custom-input' );
 		input.addClass( 'select-group__custom-input--value-entered' );
 		BannerFunctions.hideAmountError();
-		updateFormattedAmount();
 		appendEuroSign( input );
 	} );
 
 	banner.on( 'paymenttype:selected', null, function () {
-		let bannerAction = $bannerForm.data( 'main-action' );
-		if ( getPaymentType() === 'BEZ' ) {
-			bannerAction = $bannerForm.data( 'fallback-action' );
-		}
-		$bannerForm.attr( 'action', bannerAction );
 		$( '#WMDE_Banner' ).trigger( 'validation:paymenttype:ok' );
-		updateFormattedAmount();
 	} );
 }
 
@@ -209,12 +183,21 @@ BannerFunctions.initializeBannerEvents();
 
 // END form init code
 
-function addSpaceInstantly( $bannerElement ) {
+function addSpace() {
+	var $bannerElement = $( 'div#WMDE_Banner' );
 	if ( !$bannerElement.is( ':visible' ) ) {
 		return;
 	}
 
-	BannerFunctions.getSkin().addSpaceInstantly( $bannerElement.height() );
+	BannerFunctions.getSkin().addSpace( $bannerElement.height() );
+}
+
+function addSpaceInstantly() {
+	if ( !$( '#WMDE_Banner' ).is( ':visible' ) ) {
+		return;
+	}
+
+	BannerFunctions.getSkin().addSpaceInstantly( $( 'div#WMDE_Banner' ).height() );
 }
 
 function removeBannerSpace() {
@@ -223,23 +206,20 @@ function removeBannerSpace() {
 
 function displayBanner() {
 	var bannerElement = $( '#WMDE_Banner' ),
-		instantBanner = $( '#WMDE_InstantBanner' ),
 		bannerHeight;
 
 	setupValidationEventHandling();
 	setupAmountEventHandling();
 
 	bannerHeight = bannerElement.height();
-	instantBanner.addClass( 'fade-out' );
 	bannerElement.css( 'top', -bannerHeight );
 	bannerElement.css( 'display', 'block' );
+	addSpace();
 	bannerElement.animate( { top: 0 }, 1000 );
-	setTimeout( function () {
-		instantBanner.hide();
-	}, 1000 );
+	setTimeout( function () { progressBar.animate(); }, 1000 );
 
 	$( window ).resize( function () {
-		addSpaceInstantly( bannerElement );
+		addSpaceInstantly();
 		calculateLightboxPosition();
 	} );
 }
@@ -256,25 +236,13 @@ $( '#impCount' ).val( impCount );
 var bannerImpCount = BannerFunctions.increaseBannerImpCount( BannerName );
 $( '#bImpCount' ).val( bannerImpCount );
 
-var applicationOfFundsLinks = $( '#application-of-funds-link, #application-of-funds-link-instant-banner' );
-
 // track lightbox link clicking and banner closing
-trackingEvents.trackClickEvent( applicationOfFundsLinks, 'application-of-funds-shown', 1 );
+trackingEvents.trackClickEvent( $( '#application-of-funds-link' ), 'application-of-funds-shown', 1 );
 trackingEvents.trackClickEvent( $( '#WMDE_Banner .close__link' ), 'banner-closed', bannerCloseTrackRatio );
 
 // BEGIN Banner close functions
 $( '#WMDE_Banner .close__link' ).click( function () {
 	$( '#WMDE_Banner' ).hide();
-	if ( BannerFunctions.onMediaWiki() ) {
-		mw.centralNotice.hideBanner();
-	}
-	removeBannerSpace();
-
-	return false;
-} );
-
-$( '#WMDE_InstantBanner .close__link' ).click( function () {
-	$( '#WMDE_InstantBanner, #WMDE_Banner' ).hide();
 	if ( BannerFunctions.onMediaWiki() ) {
 		mw.centralNotice.hideBanner();
 	}
@@ -293,8 +261,7 @@ $( '#ca-ve-edit, .mw-editsection-visualeditor' ).click( function () {
 
 // Display banner on load
 $( function () {
-	var $bannerElement = $( '#WMDE_Banner' ),
-		$instantBanner = $( '#WMDE_InstantBanner' );
+	var $bannerElement = $( '#WMDE_Banner' );
 
 	$( 'body' ).prepend( $( '#centralNotice' ) );
 
@@ -316,11 +283,7 @@ $( function () {
 			sizeIssueTrackRatio
 		);
 	} else {
-		$instantBanner.height( $bannerElement.height() );
-		$instantBanner.show();
-		addSpaceInstantly( $instantBanner );
-		setTimeout( function () { progressBar.animate(); }, 1000 );
-		bannerDisplayTimeout.run( displayBanner, 7500 );
+		bannerDisplayTimeout.run( displayBanner, $( '#WMDE-Banner-Container' ).data( 'delay' ) || 7500 );
 	}
 
 	BannerFunctions.getSkin().addSearchObserver( function () {
