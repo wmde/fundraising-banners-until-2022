e02da3a1fd1b85334001c2e0b988b011aa97d180
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-07-10 11:56:22 +0100

    Update Webpack to version 4
    This resolves the last vulnerabilities from css-loader.

--- webpack.production.js
+++ webpack.production.js
@@ -1,26 +1,6 @@
 const Merge = require( 'webpack-merge' );
 const CommonConfig = require( './webpack.common.js' );
-const webpack = require( 'webpack' );
 
 module.exports = Merge( CommonConfig, {
-	plugins: [
-		new webpack.LoaderOptionsPlugin( {
-			minimize: true,
-			debug: false
-		} ),
-		new webpack.DefinePlugin( {
-			'process.env': {
-				NODE_ENV: JSON.stringify( 'production' )
-			}
-		} ),
-		new webpack.optimize.UglifyJsPlugin( {
-			beautify: false,
-			mangle: {
-				screw_ie8: true,
-				keep_fnames: true,
-				except: [ '$', 'exports', 'require' ]
-			},
-			comments: false
-		} )
-	]
+	mode: 'production'
 } );
