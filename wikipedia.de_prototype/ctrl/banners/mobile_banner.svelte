c3b63c93dc9690f4dd890ef8f344095e3247281d
Tim Eulitz <tim.eulitz@wikimedia.de>
2019-10-08 15:09:41 +0200

    Implement svelte-based wikipedia.de mobile banner

---
+++ wikipedia.de_prototype/ctrl/banners/mobile_banner.svelte
@@ -0,0 +1,85 @@
+<div class="mini-banner">
+	<div class="mini-banner-box">
+		<div class="mini-banner-content">
+			<div class="mini-banner-headline">
+				<div>
+					<span>Die Wikimedia-Spendenkampagne</span>
+				</div>
+			</div>
+			<div id="mini-banner-close-button" class="mini-banner-close-button"></div>
+			<div class="mini-banner-carousel">
+				<div class="carousel-cell">
+					<p>Liebe Leserinnen und Leser, an diesem { currentDayName } sind Sie in Deutschland gefragt:</p>
+				</div>
+				<div class="carousel-cell">
+					<p>Über { amountBannerImpressionsInMillion } Millionen Mal wird unser Spendenaufruf täglich angezeigt, aber nur { numberOfDonors } Menschen haben bisher gespendet. Schon der Preis einer Tasse Kaffee würde genügen.</p>
+				</div>
+				<div class="carousel-cell">
+					<p class="goal-headline">Unser Spendenziel: 8,1&nbsp;Millionen&nbsp;Euro</p>
+					{@html progressBar }
+				</div>
+				<div class="carousel-cell">
+					<p>Wenn alle, die das jetzt lesen, einen kleinen Beitrag leisten, wäre unser Spendenziel bereits am heutigen { currentDayName } erreicht.</p>
+				</div>
+				<div class="carousel-cell">
+					<p>Es ist leicht, diese Nachricht zu ignorieren und die meisten werden das wohl tun.</p>
+				</div>
+				<div class="carousel-cell">
+					<p>Wenn Sie Wikipedia nützlich finden, nehmen Sie sich an diesem { currentDayName } bitte eine Minute Zeit und geben Wikipedia mit Ihrer Spende etwas zurück.</p>
+				</div>
+			</div>
+			<div class="mini-banner-tab">
+				<div class="mini-banner-tab-inner" on:click={showForm}>
+					Jetzt spenden
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<MobileFullPageBanner
+		weekdayPrepPhrase="{weekdayPrepPhrase}"
+		campaignDaySentence="{campaignDaySentence}"
+		currentDayName="{currentDayName}"
+		campaignName="{campaignName}"
+		bannerName="{bannerName}"
+		amountBannerImpressionsInMillion="{amountBannerImpressionsInMillion}"
+		numberOfDonors="{numberOfDonors}"
+		progressBar="{progressBar}"/>
+
+<script>
+	import { Slider } from '../../../shared/banner_slider';
+	import { onMount } from 'svelte';
+	import MobileFullPageBanner from './mobile_banner_fullpage.svelte';
+
+	export let weekdayPrepPhrase;
+	export let campaignDaySentence;
+	export let currentDayName;
+	export let campaignName;
+	export let bannerName;
+	export let amountBannerImpressionsInMillion;
+	export let numberOfDonors;
+	export let progressBar;
+
+	const sliderAutoPlaySpeed = 5000;
+	const fullscreenBannerSlideInSpeed = 1250;
+
+	/**
+	 * Slider wrapper object holding a Flickity-based slider
+	 * @type {Slider}
+	 */
+	const bannerSlider = new Slider( sliderAutoPlaySpeed );
+
+	onMount(async () => {
+		bannerSlider.initialize();
+		bannerSlider.enableAutoplay();
+	});
+
+	function showForm(  ) {
+		alert('TODO');
+	}
+</script>
+
+<style lang="postcss" global>
+	@import 'mobile_banner.pcss';
+</style>
---
+++ wikipedia.de_prototype/ctrl/banners/mobile_banner_fullpage.svelte
@@ -0,0 +1,44 @@
+<div id="frbanner" class="frbanner">
+	<div id="frbanner-window" class="frbanner-window">
+		<div id="modal-signup" class="cf">
+			<div class="infobox">
+				<Text	amountBannerImpressionsInMillion="{amountBannerImpressionsInMillion}"
+						 numberOfDonors="{numberOfDonors}"
+						 campaignDaySentence="{campaignDaySentence}"
+						 weekdayPrepPhrase="{weekdayPrepPhrase}"
+						 currentDayName="{currentDayName}"/>
+				<ProgressBar progressBar="{progressBar}"/>
+				<div class="call-to-action">
+					Jetzt sind Sie <span class="call-to-action__optional-text">in Deutschland</span> gefragt.
+				</div>
+				<Form campaignName="{campaignName}" bannerName={bannerName} />
+			</div>
+			<div class="smallprint" id="frbanner-smallprint">
+				<div class="smallprint">
+					<a href="https://spenden.wikimedia.de/spenden/Impressum?piwik_campaign={campaignName}&piwik_kwd={bannerName}_link&skin=0&da=0&dsn=0" target="_blank">Impressum</a> |
+					<a href="https://spenden.wikimedia.de/spenden/Datenschutz?piwik_campaign={campaignName}&piwik_kwd={bannerName}_link&skin=0&da=0&dsn=0" target="_blank">Datenschutz</a> |
+					<a href="https://spenden.wikimedia.de/use-of-funds?piwik_campaign={campaignName}&piwik_kwd={bannerName}_link&skin=0&da=0&dsn=0" target="_blank">Wohin geht meine Spende?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+	import Text from '../components/text.svelte';
+	import Form from '../components/form.svelte';
+	import ProgressBar from '../components/progressbar.svelte';
+
+	export let weekdayPrepPhrase;
+	export let campaignDaySentence;
+	export let currentDayName;
+	export let campaignName;
+	export let bannerName;
+	export let amountBannerImpressionsInMillion;
+	export let numberOfDonors;
+	export let progressBar;
+</script>
+
+<style lang="postcss" global>
+	@import 'mobile_banner_fullpage.pcss';
+</style>
