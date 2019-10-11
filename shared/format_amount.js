f536eec2dc750e682f768103bc0f7b62f618828f
Gabriel Birke <gabriel.birke@gmail.com>
2019-10-11 13:38:34 +0200

    Add form validation to svelte form
    Don't allow empty values
    Remove selected amount if user enters custom amount and vice versa
    Add Euro sign to custom amount and format it in German locale

---
+++ shared/format_amount.js
@@ -0,0 +1,21 @@
+/**
+ * Format amount string:
+ *  - commas to dots
+ *  - remove euro sign
+ *  - trim
+ *  @param {string} amountStr
+ *  @return {string} A string with 2 decimal places
+ */
+export function formatAmount( amountStr ) {
+	// Replace all commas with dots and then remove all dots except for the last one
+	amountStr = amountStr.replace( new RegExp( ',', 'g' ), '.' );
+	amountStr = amountStr.replace( /[.](?=.*[.])/g, '' );
+
+	// Remove euro sign
+	amountStr = amountStr.replace( /[€]/, '' );
+	amountStr = amountStr.trim();
+	if ( isNaN( Number( amountStr ) ) ) {
+		return '0';
+	}
+	return Number( amountStr ).toFixed( 2 );
+}
