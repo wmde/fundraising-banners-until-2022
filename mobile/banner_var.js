266c94e518637d1013f00ce4a1d85a0e6bccd7be
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2019-12-16 16:24:59 +0100

    Prepare campaign for C19_WMDE_Mobile_Test_05
    https://phabricator.wikimedia.org/T240857

--- mobile/banner_var.js
+++ mobile/banner_var.js
@@ -8,6 +8,7 @@ import DayName from '../shared/day_name';
 import animateHighlight from '../shared/animate_highlight';
 import ProgressBar from '../shared/progress_bar/progress_bar_mobile';
 import Translations from '../shared/messages/de';
+import { Slider } from './banner_slider';
 import { createCampaignParameters } from '../shared/campaign_parameters';
 import { BannerFunctions as BannerFunctionsFactory } from '../shared/banner_functions';
 import { CampaignProjection } from '../shared/campaign_projection';
@@ -30,9 +31,16 @@ const bannerTemplate = require( './templates/banner_html_var.hbs' );
 const bannerClickTrackRatio = 0.01;
 const bannerCloseTrackRatio = 0.01;
 const searchBoxTrackRatio = 0.01;
-const fullscreenBannerSlideInSpeed = 1250;
 const LANGUAGE = 'de';
+const sliderAutoPlaySpeed = 5000;
+const fullscreenBannerSlideInSpeed = 1250;
 // END Banner-Specific configuration
+
+/**
+ * Slider wrapper object holding a Flickity-based slider
+ * @type {Slider}
+ */
+const bannerSlider = new Slider( sliderAutoPlaySpeed );
 
 const campaignDays = new CampaignDays(
 	startOfDay( CampaignParameters.startDate ),
@@ -54,7 +62,6 @@ const weekdayPrepPhrase = dayName.isSpecialDayName() ? Translations[ 'day-name-p
 const $bannerContainer = $( '#WMDE-Banner-Container' );
 const CampaignName = $bannerContainer.data( 'campaign-tracking' );
 const BannerName = $bannerContainer.data( 'tracking' );
-
 const progressBar = new ProgressBar(
 	{ goalDonationSum: CampaignParameters.donationProjection.goalDonationSum },
 	campaignProjection,
@@ -74,7 +81,6 @@ $bannerContainer.html( bannerTemplate( {
 } ) );
 
 const trackingEvents = new EventLoggingTracker( BannerName );
-trackingEvents.trackClickEvent( $( '.mini-banner' ), 'mobile-mini-banner-expanded' );
 trackingEvents.trackClickEvent( $( '.mini-banner__close-button' ), 'banner-closed', bannerCloseTrackRatio );
 
 // BEGIN form initialization
@@ -96,39 +102,6 @@ $( 'input[name=interval]' ).click( function () {
 		subPaymentButton.attr( 'disabled', false );
 	}
 } );
-
-$( '#banner-form-submit' ).click( function () {
-	if ( !$( '#periode' ).val() || $( '.interval-selection .selected-option' ).length === 0 ) {
-		alert( 'Bitte wählen Sie ein Zahlungsinterval aus.' );
-		return false;
-	}
-	if ( !$( '#betrag' ).val() ) {
-		alert( 'Bitte wählen Sie einen Spendenbetrag aus.' );
-		return false;
-	}
-	if ( !$( '#zahlweise' ).val() ) {
-		alert( 'Bitte wählen Sie ein Zahlungsmittel aus.' );
-		return false;
-	}
-	return true;
-} );
-// END form initialization
-
-function displayMiniBanner() {
-	const miniBanner = $( '.mini-banner' );
-	const bannerHeight = miniBanner.height();
-	miniBanner.css( 'top', 0 - bannerHeight ).show();
-
-	miniBanner.animate( {
-		top: 0
-	}, 1000 );
-
-	$( '#mw-mf-viewport' ).animate( {
-		marginTop: bannerHeight
-	}, 1000 );
-
-	$( 'head' ).append( '<style>#mw-mf-viewport .overlay.media-viewer { margin-top: ' + ( 0 - bannerHeight ) + 'px }</style>' );
-}
 
 function setupAmountEventHandling() {
 	var otherInput = $( '#amount-other-input' );
@@ -182,6 +155,26 @@ $( '#banner-form-submit' ).click( function () {
 	}
 	return true;
 } );
+
+// END form initialization
+
+function displayMiniBanner() {
+
+	const miniBanner = $( '.mini-banner' );
+	const bannerHeight = miniBanner.outerHeight() + 40;
+
+	// Banner starts in far off screen and needs to be reset, workaround to get sliders to work
+	miniBanner.css( 'top', -bannerHeight );
+	miniBanner.animate( { top: 0 }, 1000 );
+
+	$( '#mw-mf-viewport' ).animate( { marginTop: bannerHeight }, 1000 );
+
+	$( 'head' ).append( '<style>#mw-mf-viewport .overlay.media-viewer { margin-top: ' + ( 0 - bannerHeight ) + 'px }</style>' );
+	// Making sure automatic sliding only starts after slider is shown to the user
+	bannerSlider.enableAutoplay();
+	progressBar.animate();
+}
+
 /**
  * Hides mini banner and slides down full-screen banner
  * Animation is split into two parts:
@@ -190,8 +183,8 @@ $( '#banner-form-submit' ).click( function () {
 function displayFullBanner() {
 	trackingEvents.trackBannerEvent(
 		'mobile-mini-banner-expanded',
-		0,
-		0,
+		bannerSlider.getViewedSlides(),
+		bannerSlider.getCurrentSlide(),
 		bannerClickTrackRatio
 	);
 
@@ -226,9 +219,7 @@ function displayFullBanner() {
 		fullscreenBanner.dequeue();
 		fullscreenBanner.animate( { top: 0 }, remainingSlideTime, 'linear' ).queue( function () {
 			// Once fullscreen banner is fully shown, the contents are animated
-			setTimeout( function () {
-				animateHighlight( $( '#to-highlight' ), 'highlight', 10 );
-			}, 500 );
+			setTimeout( function () { animateHighlight( $( '#to-highlight' ), 'highlight', 10 ); }, 500 );
 		} );
 
 	} );
@@ -251,6 +242,8 @@ $( document ).ready( function () {
 		return false;
 	} );
 
+	bannerSlider.initialize();
+
 	BannerFunctions.getSkin().addSearchObserver( function () {
 		bannerDisplayTimeout.cancel();
 		$( '.mini-banner' ).hide();
@@ -261,7 +254,8 @@ $( document ).ready( function () {
 	const bannerDelay = $( '#WMDE-Banner-Container' ).data( 'delay' ) || 5000;
 	bannerDisplayTimeout.run( displayMiniBanner, bannerDelay );
 
-	$( '.mini-banner' ).click( displayFullBanner );
+	const clickableBannerArea = $( '.mini-banner-tab, .mini-banner .banner-headline' );
 	trackingEvents.trackClickEvent( $( '#application-of-funds-link' ), 'application-of-funds-shown', 1 );
-	progressBar.animate();
+
+	clickableBannerArea.click( displayFullBanner );
 } );
