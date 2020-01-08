93db782195388ab34810028aecbe8fd682cad08b
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-01-08 18:20:47 +0100

    Extract form elements from donationform
    The select groups were extracted into a generic SelectGroup component.
    Any necessary properties are being passed to them.

--- desktop_preact/components/BannerText.jsx
+++ desktop_preact/components/BannerText.jsx
@@ -15,8 +15,7 @@ export default function BannerText( props ) {
 				className="optional-text text-m">, um Wikipedias Unabhängigkeit zu sichern</span>:
 		</p>
 		<p className="text__paragraph">
-			<span>{ campaignDaySentence }
-			Wikipedia wird durch Spenden von durchschnittlich 23,83 € finanziert, aber 99 % der Leserinnen und Leser
+			<span>{ campaignDaySentence } Wikipedia wird durch Spenden von durchschnittlich 23,83 € finanziert, aber 99 % der Leserinnen und Leser
 				spenden nicht. </span>
 
 			<span className="text__paragraph--bold">
@@ -26,17 +25,17 @@ export default function BannerText( props ) {
 			Mal wird unser Spendenaufruf täglich angezeigt,
 			aber nur  { numberOfDonors } Menschen haben bisher gespendet. </span>
 			<span className="optional-text text-m">
-				<span>Wenn Wikipedia eine kommerzielle Seite sein würde, wäre das ein riesiger Verlust für die Welt. </span>
-			Wikipedia ist ein Ort des Lernens – Werbung hat darin keinen Platz.
+				Wenn Wikipedia eine kommerzielle Seite sein würde, wäre das ein riesiger Verlust für die Welt. </span>
+			<span> Wikipedia ist ein Ort des Lernens – Werbung hat darin keinen Platz.
 			Sicher könnten wir mit Werbung eine Menge Geld verdienen. Aber dann wäre Wikipedia komplett anders. Wir
 				könnten ihr nicht vertrauen. </span>
 
 			<span className="optional-text text-xl">
         Das Herzstück von Wikipedia ist die Gemeinschaft von Menschen, die uns unbegrenzten Zugang zu verlässlichen und neutralen Informationen geben. </span>
 
-			<span>Es ist leicht, diese Nachricht zu ignorieren und die meisten werden das wohl tun.
-				{/* eslint-disable-next-line max-len */}
-			Wenn Sie Wikipedia nützlich finden, nehmen Sie sich { weekdayPrepPhrase } { currentDayName } bitte eine Minute Zeit und geben Wikipedia mit Ihrer Spende etwas zurück</span>
+			<span>Es ist leicht, diese Nachricht zu ignorieren und die meisten werden das wohl tun. Wenn Sie Wikipedia
+				nützlich finden, nehmen Sie sich { weekdayPrepPhrase } { currentDayName } bitte eine
+				Minute Zeit und geben Wikipedia mit Ihrer Spende etwas zurück</span>
 			<span className="optional-text text-l">, damit Wikipedia weiter wachsen kann</span>.
 
 			<span className="text__paragraph--italic"> Vielen Dank!</span>
