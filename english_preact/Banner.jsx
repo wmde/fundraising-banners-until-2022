0aa9191aba72fab10a859f72562ed81b4eb3fd18
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-01-13 17:42:31 +0100

    Implement english desktop preact banner

---
+++ english_preact/Banner.jsx
@@ -0,0 +1,117 @@
+// eslint-disable-next-line no-unused-vars
+import { Component, h, createRef } from 'preact';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import BannerTransition from '../shared/components/BannerTransition';
+import ProgressBar from '../shared/components/ui/ProgressBar';
+import DonationForm from '../shared/components/ui/form/DonationForm';
+import Footer from '../shared/components/ui/Footer';
+import Infobox from '../shared/components/ui/Infobox';
+import FundsModal from '../shared/components/ui/FundsModal';
+import TranslationContext from '../shared/components/TranslationContext';
+
+const PENDING = 0;
+const VISIBLE = 1;
+const CLOSED = 2;
+
+export default class Banner extends Component {
+
+	static propTypes = {
+		/** callback when banner closes */
+		onClose: PropTypes.func,
+		/** */
+		registerDisplayBanner: PropTypes.func.isRequired
+	}
+
+	ref = createRef();
+
+	constructor( props ) {
+		super( props );
+		this.state = {
+			displayState: PENDING,
+			isFundsModalVisible: false
+		};
+		this.slideInBanner = () => {};
+	}
+
+	componentDidMount() {
+		this.props.registerDisplayBanner(
+			() => {
+				this.setState( { displayState: VISIBLE } );
+				this.slideInBanner();
+			}
+		);
+	}
+
+	closeBanner = e => {
+		this.props.trackingData.tracker.trackBannerEvent( 'banner-closed', 0, 0, this.props.trackingData.bannerCloseTrackRatio );
+		e.preventDefault();
+		this.setState( { displayState: CLOSED } );
+		this.props.onClose();
+	};
+
+	registerBannerTransition = ( cb ) => {
+		this.slideInBanner = cb;
+	}
+
+	registerStartProgressbar = ( startPb ) => {
+		this.startProgressbar = startPb;
+	};
+
+	toggleFundsModal = () => {
+		this.props.trackingData.tracker.trackBannerEvent( 'application-of-funds-shown', 0, 0, this.props.trackingData.bannerClickTrackRatio );
+		this.setState( { isFundsModalVisible: !this.state.isFundsModalVisible } );
+	};
+
+	// eslint-disable-next-line no-unused-vars
+	render( props, state, context ) {
+		const campaignProjection = props.campaignProjection;
+		return <div
+			className={classNames(
+				'wmde-banner',
+				state.displayState === CLOSED ? 'wmde-banner--hidden' : '',
+				state.displayState === VISIBLE ? 'wmde-banner--visible' : ''
+			)}
+			ref={this.ref}>
+			<BannerTransition registerDisplayBanner={ this.registerBannerTransition } onFinish={ this.startProgressbar }>
+				<TranslationContext.Provider value={props.translations}>
+					<div className="banner__wrapper">
+						<div className="banner__content">
+							<div className="banner__infobox">
+								<Infobox
+									formatters={props.formatters}
+									campaignParameters={props.campaignParameters}
+									campaignProjection={props.campaignProjection}
+									bannerText={props.bannerText}/>
+								<ProgressBar
+									formatters={props.formatters}
+									daysLeft={campaignProjection.getRemainingDays()}
+									donationAmount={campaignProjection.getProjectedDonationSum()}
+									goalDonationSum={campaignProjection.goalDonationSum}
+									missingAmount={campaignProjection.getProjectedRemainingDonationSum()}
+									setStartAnimation={this.registerStartProgressbar}/>
+							</div>
+							<DonationForm
+								formItems={props.formItems}
+								bannerName={props.bannerName}
+								campaignName={props.campaignName}
+								formatters={props.formatters}
+								impressionCounts={props.impressionCounts}
+							/>
+						</div>
+						<div className="close">
+							<a className="close__link" onClick={this.closeBanner}>&#x2715;</a>
+						</div>
+						<Footer showFundsModal={ this.toggleFundsModal }/>
+					</div>
+				</TranslationContext.Provider>
+			</BannerTransition>
+			<FundsModal
+				fundsModalData={props.fundsModalData}
+				toggleFundsModal={ this.toggleFundsModal }
+				isFundsModalVisible={ this.state.isFundsModalVisible }
+				locale='de'/>
+		</div>;
+	}
+
+}
