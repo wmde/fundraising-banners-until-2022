7d81cb3fe9ae295565587f4286658f03167ad63e
Abban Dunne <abban.dunne@wikimedia.de>
2020-03-06 13:33:29 +0100

    Update of styles for desktop custom amount
    Changes Made:
    * Added wrapper around euro symbol and text input on SelectCustomAmount component
    * Updated styles on all banners to reflect new structure
    * Changed fieldname of SelectCustomAmount radio button to match other amount selectors
    * Fixed bug where the custom amount radio input was not selected on custom field focus

--- shared/components/ui/form/SelectCustomAmount.jsx
+++ shared/components/ui/form/SelectCustomAmount.jsx
@@ -1,53 +1,67 @@
-import { Component } from 'preact/compat';
+import { Component, createRef } from 'preact/compat';
 // eslint-disable-next-line no-unused-vars
 import { h } from 'preact';
 import classNames from 'classnames';
 
 export default class SelectCustomAmount extends Component {
+	radioInput = createRef();
 
 	constructor( props ) {
 		super( props );
 		this.state = {
+			focused: false,
 			showEuro: false
 		};
 	}
+
 	onFocus = ( e ) => {
-		this.setState( { showEuro: true } );
+		this.setState( { showEuro: true, focused: true } );
+		this.radioInput.current.click();
 		if ( this.props.value !== '' ) {
 			e.target.select();
 		}
 	};
+
+	onInput = ( e ) => {
+		if ( !this.radioInput.current.checked ) {
+			this.radioInput.current.click();
+		}
+		this.props.onInput( e );
+	};
+
 	onBlur = ( e ) => {
+		this.setState( { focused: false } );
 		this.props.onBlur( e );
 		if ( this.props.value === '' ) {
 			this.setState( { showEuro: false } );
 		}
 	};
+
 	render( props, state ) {
 		return <label className="select-group__option select-group__option--amount-other-input">
-			<input type="radio" name="amount" className="select-group__input" value=""/>
+			<input type="radio" name="select-amount" className="select-group__input" value="" ref={this.radioInput}/>
 
-			{ state.showEuro && props.language === 'en' ? <span className={ classNames(
-				'select-group__custom-input--euro-symbol',
-				{ 'select-group__custom-input--euro-symbol--value-entered': props.value } ) }
-			>&euro;</span> : null }
+			<div className={ classNames(
+				'select-group__custom-input',
+				{
+					'select-group__custom-input--value-entered': props.value,
+					'select-group__custom-input--focused': state.focused
+				} ) }
+			>
 
-			<input type="text"
-				value={ props.value }
-				onInput={ props.onInput }
-				size="3"
-				maxLength="8"
-				onFocus={ this.onFocus }
-				onBlur={ this.onBlur }
-				autoComplete="off"
-				placeholder={ props.placeholder }
-				className={ classNames( 'select-group__custom-input', { 'select-group__custom-input--value-entered': props.value } ) } />
+				{ state.showEuro ? <span className="select-group__custom-input--euro-symbol">&euro;</span> : null }
 
-			{ state.showEuro && props.language === 'de' ? <span className={ classNames(
-				'select-group__custom-input--euro-symbol',
-				{ 'select-group__custom-input--euro-symbol--value-entered': props.value } ) }
-			>&euro;</span> : null }
-
+				<input type="text"
+					value={ props.value }
+					onInput={ this.onInput }
+					size="3"
+					maxLength="8"
+					onFocus={ this.onFocus }
+					onBlur={ this.onBlur }
+					autoComplete="off"
+					placeholder={ props.placeholder }
+					className="select-group__custom-input--input" />
+			</div>
 		</label>;
 	}
 
