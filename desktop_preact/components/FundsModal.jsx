ed27082c32b50b75a59c9628e6d04115eadfe6e1
Gabriel Birke <gabriel.birke@gmail.com>
2020-07-07 18:54:23 +0200

    Add list with icons

--- desktop_preact/components/FundsModal.jsx
+++ desktop_preact/components/FundsModal.jsx
@@ -39,8 +39,56 @@ export default class FundsModal extends Component {
 						</div>
 						<FundsDistributionInfo />
 					</div>
-					<div className="banner_modal__section" style="background:grey">Section 2 (2 cols)</div>
-					<div className="banner_modal__section">Section 3</div>
+					<div className="banner_modal__section banner_modal__section--two-cols">
+						<div>
+							<div className="banner_modal__column_info">
+								Einen genauen Einblick in die internationalen Aktivitäten finden Sie hier:<br/>
+								<a href="#">Jahresplan Wikimedia Foundation</a>
+							</div>
+							<div className="banner_modal__benefits_list">
+								<h2>Was Ihre Spende bewirkt</h2>
+								<ul className="banner_modal__icon-list">
+									<li className='banner_modal__icon-list_item--hand'>
+										Ihre Spende ist ein wichtiger Beitrag, um allen Menschen überall auf der Welt
+										und zu jedem Zeitpunkt freien Zugang zu Wissen zu ermöglichen.
+									</li>
+
+									<li className='banner_modal__icon-list_item--smartphone'>
+										Ihre Spende trägt dazu bei, die sichere, stabile Infrastruktur und einfache
+										Bedienbarkeit von Wikipedia auch in Zukunft zu erhalten.
+									</li>
+
+									<li className='banner_modal__icon-list_item--world'>
+										Ihre Spende unterstützt unsere Bemühungen, weltweit die Bildung regionaler
+										Wikipedia-Communities zu fördern.
+									</li>
+
+									<li className='banner_modal__icon-list_item--megaphone'>
+										Ihre Spende ist die wichtigste Einnahmequelle, die wir haben. Sie stellt sicher,
+										dass Wikipedia auch weiterhin von Werbung, kommerziellen Interessen oder
+										Drittmitteln unabhängig bleibt.
+									</li>
+
+									<li className='banner_modal__icon-list_item--twentyfourseven'>
+										Ihre Spende ist nicht zuletzt der Beitrag, den Sie leisten, damit Wikipedia
+										immer und überall für Sie persönlich da ist und in einzigartiger Breite Ihre
+										Fragen beantworten kann.
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div>
+							<div className="banner_modal__column_info">
+								Einen genauen Einblick in die Aktivitäten in Deutschland finden Sie hier:<br/>
+								<a href="#">Jahresplan Wikimedia Deutschland</a>
+							</div>
+
+						</div>
+					</div>
+					<div className="banner_modal__section">
+						<h2>An wen gehen die Spenden für Wikipedia</h2>
+						<div>TODO</div>
+					</div>
 				</div>
 			</div>
 		</div>;
