6efd6e5123b75983bec9d6f1cc5714bf5cba2c1c
Abban Dunne <abban.dunne@wikimedia.de>
2020-07-16 11:42:52 +0200

    Fix copy in CTRL/VAR for C20_WMDE_Test_01

--- desktop_preact/components/BannerText.jsx
+++ desktop_preact/components/BannerText.jsx
@@ -9,7 +9,8 @@ export default function BannerText( props ) {
 			<div className="banner-text-ctrl">
 				<p>
 					Liebe Leserinnen und Leser, bitte verzeihen Sie die Störung. Es ist ein bisschen unangenehm, daher
-					kommen wir gleich zur Sache. { capitalizeFirstLetter( weekdayPrepPhrase ) } { currentDayName } sind Sie in Deutschland gefragt:
+					kommen wir gleich zur Sache. { capitalizeFirstLetter( weekdayPrepPhrase ) } { currentDayName } sind Sie in Deutschland gefragt
+					<span className="optional-text text-m">, um Wikipedias Unabhängigkeit zu sichern</span>:
 				</p>
 				<p>
 					{ campaignDaySentence + ' ' }<strong>Wikipedia wird durch Spenden von durchschnittlich 22,81 € finanziert. Wenn alle, die das jetzt
@@ -17,16 +18,20 @@ export default function BannerText( props ) {
 						erreicht. Millionen Menschen nutzen Wikipedia, aber 99 % der Leserinnen und Leser spenden nicht. </strong>
 					<span className="optional-text text-m">Wenn Wikipedia eine kommerzielle Seite sein würde, wäre das ein riesiger Verlust für die Welt. </span>
 					Sicher könnten wir mit Werbung eine Menge Geld verdienen. Aber dann wäre Wikipedia komplett anders.
-					Wir könnten ihr nicht vertrauen. Es ist leicht, diese Nachricht zu ignorieren und die meisten werden
+					Wir könnten ihr nicht vertrauen.
+					<span className="optional-text text-xl"> Das Herzstück von Wikipedia ist die Gemeinschaft von Menschen,
+						die uns unbegrenzten Zugang zu verlässlichen und neutralen Informationen geben. </span>
+					Es ist leicht, diese Nachricht zu ignorieren und die meisten werden
 					das wohl tun. <strong>Schon der Preis einer Tasse Kaffee würde genügen.</strong> Wenn Sie Wikipedia nützlich finden,
 					nehmen Sie sich an diesem { currentDayName } bitte eine Minute Zeit und geben Wikipedia mit Ihrer Spende
-					etwas zurück. <em>Vielen Dank!</em>
+					etwas zurück<span className="optional-text text-l">, damit Wikipedia weiter wachsen kann</span>. <em>Vielen Dank!</em>
 				</p>
 			</div>
 			<div className="banner-text-var">
 				<p>
 					Liebe Leserinnen und Leser, bitte verzeihen Sie die Störung. Es ist ein bisschen unangenehm, daher
-					kommen wir gleich zur Sache. { capitalizeFirstLetter( weekdayPrepPhrase ) } { currentDayName } sind Sie in Deutschland gefragt:
+					kommen wir gleich zur Sache. { capitalizeFirstLetter( weekdayPrepPhrase ) } { currentDayName } sind Sie in Deutschland gefragt
+					<span className="optional-text text-m">, um Wikipedias Unabhängigkeit zu sichern</span>:
 				</p>
 
 				<ul>
@@ -45,10 +50,13 @@ export default function BannerText( props ) {
 				<p>
 					<span className="optional-text text-m">Wenn Wikipedia eine kommerzielle Seite sein würde, wäre das ein riesiger Verlust für die Welt. </span>
 					Sicher könnten wir mit Werbung eine Menge Geld verdienen. Aber dann wäre Wikipedia komplett anders.
-					Wir könnten ihr nicht vertrauen. Es ist leicht, diese Nachricht zu ignorieren und die meisten werden
+					Wir könnten ihr nicht vertrauen.
+					<span className="optional-text text-xl"> Das Herzstück von Wikipedia ist die Gemeinschaft von Menschen,
+						die uns unbegrenzten Zugang zu verlässlichen und neutralen Informationen geben. </span>
+					Es ist leicht, diese Nachricht zu ignorieren und die meisten werden
 					das wohl tun. <strong>Schon der Preis einer Tasse Kaffee würde genügen.</strong> Wenn Sie Wikipedia nützlich finden,
 					nehmen Sie sich an diesem { currentDayName } bitte eine Minute Zeit und geben Wikipedia mit Ihrer Spende
-					etwas zurück. <em>Vielen Dank!</em>
+					etwas zurück<span className="optional-text text-l">, damit Wikipedia weiter wachsen kann</span>. <em>Vielen Dank!</em>
 				</p>
 			</div>
 		</div>
