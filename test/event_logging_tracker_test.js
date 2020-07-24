d7dbb78304747562053433d63388f2f54789fac4
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-07-24 10:32:18 +0200

    Add tests to check for correct tracker naming
    .. and unify the naming: make prefixes instead of suffixes/prefixes

--- test/event_logging_tracker_test.js
+++ test/event_logging_tracker_test.js
@@ -2,6 +2,8 @@ import assert from 'assert';
 import sinon from 'sinon';
 import { EVENT_SCHEMA, BANNER_SIZE_SCHEMA, ALWAYS_TRACK, EventLoggingTracker } from '../shared/event_logging_tracker';
 
+import { VIEWPORT_TRACKING_CLOSED_EVENT_IDENTIFIER, VIEWPORT_TRACKING_SUBMITTED_EVENT_IDENTIFIER, VIEWPORT_TRACKING_IDENTIFIER } from '../shared/event_logging_tracker';
+
 const BANNER_NAME = 'TestTracker';
 const DIMENSION_DATA = {
 	window: {
@@ -53,7 +55,7 @@ describe( 'EventLoggingTracker', function () {
 
 	it( 'sends correct data on viewport event', () => {
 		const eventData = {
-			bannerName: BANNER_NAME + '-viewport_tracking',
+			bannerName: VIEWPORT_TRACKING_IDENTIFIER + '-' + BANNER_NAME,
 			viewportWidth: DIMENSION_DATA.window.width,
 			viewportHeight: DIMENSION_DATA.window.height,
 			bannerHeight: DIMENSION_DATA.bannerHeight,
@@ -62,7 +64,7 @@ describe( 'EventLoggingTracker', function () {
 		const eventSpy = { track: sinon.spy() };
 		const tracker = new EventLoggingTracker( BANNER_NAME, eventSpy, RANDOM_ALWAYS_ZERO );
 
-		tracker.trackViewPortDimensions( 'viewport_tracking', DIMENSION_DATA, eventData.eventRate );
+		tracker.trackViewPortDimensions( VIEWPORT_TRACKING_IDENTIFIER, DIMENSION_DATA, eventData.eventRate );
 
 		assert.deepEqual( eventSpy.track.firstCall.args[ 0 ], BANNER_SIZE_SCHEMA );
 		assert.deepEqual( eventSpy.track.firstCall.args[ 1 ], eventData );
@@ -101,5 +103,35 @@ describe( 'EventLoggingTracker', function () {
 			tracker.trackBannerEventWithViewport( 'test-action', 1, 1, testItem.ratio, DIMENSION_DATA );
 			assert.equal( eventSpy.track.calledTwice, testItem.shouldFire );
 		} );
+	} );
+
+	it( 'Prepends "vtc" string on banner name for close tracking', () => {
+		const eventData = {
+			bannerName: VIEWPORT_TRACKING_CLOSED_EVENT_IDENTIFIER + '-' + BANNER_NAME,
+			viewportWidth: DIMENSION_DATA.window.width,
+			viewportHeight: DIMENSION_DATA.window.height,
+			bannerHeight: DIMENSION_DATA.bannerHeight,
+			eventRate: ALWAYS_TRACK
+		};
+		const eventSpy = { track: sinon.spy() };
+		const tracker = new EventLoggingTracker( BANNER_NAME, eventSpy, RANDOM_ALWAYS_ZERO );
+
+		tracker.trackBannerEventWithViewport( 'irrelevantActionName', 0, 0, 1, DIMENSION_DATA );
+		assert.deepEqual( eventSpy.track.secondCall.args[ 1 ], eventData );
+	} );
+
+	it( 'Prepends "submit" string on banner name for submit tracking', () => {
+		const eventData = {
+			bannerName: VIEWPORT_TRACKING_SUBMITTED_EVENT_IDENTIFIER + '-' + BANNER_NAME,
+			viewportWidth: DIMENSION_DATA.window.width,
+			viewportHeight: DIMENSION_DATA.window.height,
+			bannerHeight: DIMENSION_DATA.bannerHeight,
+			eventRate: ALWAYS_TRACK
+		};
+		const eventSpy = { track: sinon.spy() };
+		const tracker = new EventLoggingTracker( BANNER_NAME, eventSpy, RANDOM_ALWAYS_ZERO );
+
+		tracker.trackViewPortDimensions( 'submit', DIMENSION_DATA, 1 );
+		assert.deepEqual( eventSpy.track.firstCall.args[ 1 ], eventData );
 	} );
 } );
