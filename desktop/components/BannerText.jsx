bd389bd8e9d72d76a2bc7895c7217e55d4241535
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-11-10 17:35:35 +0100

    Prepare campaign for C20_WMDE_Test_12
    -based on desktop DE test 10
    - changes 5€ into 10€ in bannertext
    
    https://phabricator.wikimedia.org/T267596

--- desktop/components/BannerText.jsx
+++ desktop/components/BannerText.jsx
@@ -2,12 +2,7 @@
 import { h } from 'preact';
 
 export default function BannerText( props ) {
-	const { currentDayName, campaignDaySentence, overallImpressionCount, visitorsVsDonorsSentence } = props;
-
-	const firstImpressionSentence = <span>Zum ersten Mal seit langem möchten wir Sie an diesem { currentDayName } bescheiden
-		darum bitten, die Unabhängigkeit von Wikipedia zu verteidigen.</span>;
-	const subsequentImpressionSentence = <span>Dies ist der { overallImpressionCount }. Spendenaufruf, den wir Ihnen zeigen.
-		Am heutigen { currentDayName } bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia zu verteidigen.</span>;
+	const { currentDayName, campaignDaySentence, visitorsVsDonorsSentence } = props;
 
 	return <div className="banner-text">
 
@@ -20,18 +15,16 @@ export default function BannerText( props ) {
 						<strong> An alle unsere Leserinnen und Leser in Deutschland. </strong>
 					</span>
 					Vielleicht kommen wir gerade ungelegen, aber dennoch:
-					Bitte klicken Sie jetzt nicht weg! { campaignDaySentence }{ ' ' }
-
-					{ overallImpressionCount <= 1 ? firstImpressionSentence : subsequentImpressionSentence }{ ' ' }
-
+					Bitte klicken Sie jetzt nicht weg! { campaignDaySentence } Zum ersten Mal seit langem möchten wir Sie an
+					diesem { currentDayName } bescheiden darum bitten, die Unabhängigkeit von Wikipedia zu verteidigen.
 					Insgesamt spenden 99% unserer Leserinnen und Leser nichts – sie übergehen diesen Aufruf. Sollten
 					Sie zu dem kleinen Kreis gehören, die bereits gespendet haben, danken wir Ihnen sehr herzlich.
 					Wikipedia wird durch Spenden von durchschnittlich 22,81 € finanziert. Doch schon mit einer Spende
 					von 5 € kann Wikipedia sich auch in Zukunft erfolgreich entwickeln. { visitorsVsDonorsSentence } Die meisten Menschen spenden,
 					weil Sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert von 5 €
-					geschenkt? Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück. <span className="optional-text text-l">Zeigen
+					geschenkt? Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück. Zeigen
 					Sie den Freiwilligen, die Ihnen verlässliche und neutrale Informationen zur Verfügung stellen,
-					dass Sie ihre Arbeit wertschätzen. </span>Vielen Dank!
+					dass Sie ihre Arbeit wertschätzen. Vielen Dank!
 				</p>
 			</div>
 
@@ -42,18 +35,16 @@ export default function BannerText( props ) {
 						<strong> An alle unsere Leserinnen und Leser in Deutschland. </strong>
 					</span>
 					Vielleicht kommen wir gerade ungelegen, aber dennoch:
-					Bitte klicken Sie jetzt nicht weg! { campaignDaySentence }{ ' ' }
-
-					{ overallImpressionCount <= 1 ? firstImpressionSentence : subsequentImpressionSentence }{ ' ' }
-
-					Die operativen Kosten und Investitionen in die Weiterentwicklung von Wikipedia werden durch Spenden von durchschnittlich 22,81 € finanziert.
-					Insgesamt spenden aber 99% unserer Leserinnen und Leser nichts – sie übergehen diesen Aufruf. Sollten
-					Sie zu dem kleinen Kreis gehören, die bereits gespendet haben, danken wir Ihnen sehr herzlich. Doch schon mit einer Spende
+					Bitte klicken Sie jetzt nicht weg! { campaignDaySentence } Zum ersten Mal seit langem möchten wir Sie an
+					diesem { currentDayName } bescheiden darum bitten, die Unabhängigkeit von Wikipedia zu verteidigen.
+					Insgesamt spenden 99% unserer Leserinnen und Leser nichts – sie übergehen diesen Aufruf. Sollten
+					Sie zu dem kleinen Kreis gehören, die bereits gespendet haben, danken wir Ihnen sehr herzlich.
+					Wikipedia wird durch Spenden von durchschnittlich 22,81 € finanziert. Doch schon mit einer Spende
 					von 5 € kann Wikipedia sich auch in Zukunft erfolgreich entwickeln. { visitorsVsDonorsSentence } Die meisten Menschen spenden,
 					weil Sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert von 5 €
-					geschenkt? Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück. <span className="optional-text text-l">Zeigen
+					geschenkt? Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück. Zeigen
 					Sie den Freiwilligen, die Ihnen verlässliche und neutrale Informationen zur Verfügung stellen,
-					dass Sie ihre Arbeit wertschätzen. </span>Vielen Dank!
+					dass Sie ihre Arbeit wertschätzen. Vielen Dank!
 				</p>
 			</div>
 
