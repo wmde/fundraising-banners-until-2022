a169aa6cc2c01adbf4e81c6f11b40160ccfbe0a3
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-11-11 11:36:18 +0100

    Implement VAR for C20_WMDE_Test_12
    - based on Test 10 ctrl
    - changes 5 € to 10 € in the bannertext
    
    https://phabricator.wikimedia.org/T267596

--- desktop/components/BannerText.jsx
+++ desktop/components/BannerText.jsx
@@ -19,10 +19,10 @@ export default function BannerText( props ) {
 					diesem { currentDayName } bescheiden darum bitten, die Unabhängigkeit von Wikipedia zu verteidigen.
 					Insgesamt spenden 99% unserer Leserinnen und Leser nichts – sie übergehen diesen Aufruf. Sollten
 					Sie zu dem kleinen Kreis gehören, die bereits gespendet haben, danken wir Ihnen sehr herzlich.
-					Wikipedia wird durch Spenden von durchschnittlich 22,81 € finanziert. Doch schon mit einer Spende
-					von 5 € kann Wikipedia sich auch in Zukunft erfolgreich entwickeln. { visitorsVsDonorsSentence } Die meisten Menschen spenden,
-					weil Sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert von 5 €
-					geschenkt? Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück. Zeigen
+					Wikipedia wird durch Spenden von durchschnittlich 22,81&nbsp;€ finanziert. Doch schon mit einer Spende
+					von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich entwickeln. { visitorsVsDonorsSentence } Die meisten Menschen spenden,
+					weil Sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert von 5&nbsp;€ geschenkt?
+					Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück. Zeigen
 					Sie den Freiwilligen, die Ihnen verlässliche und neutrale Informationen zur Verfügung stellen,
 					dass Sie ihre Arbeit wertschätzen. Vielen Dank!
 				</p>
@@ -39,10 +39,10 @@ export default function BannerText( props ) {
 					diesem { currentDayName } bescheiden darum bitten, die Unabhängigkeit von Wikipedia zu verteidigen.
 					Insgesamt spenden 99% unserer Leserinnen und Leser nichts – sie übergehen diesen Aufruf. Sollten
 					Sie zu dem kleinen Kreis gehören, die bereits gespendet haben, danken wir Ihnen sehr herzlich.
-					Wikipedia wird durch Spenden von durchschnittlich 22,81 € finanziert. Doch schon mit einer Spende
-					von 5 € kann Wikipedia sich auch in Zukunft erfolgreich entwickeln. { visitorsVsDonorsSentence } Die meisten Menschen spenden,
-					weil Sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert von 5 €
-					geschenkt? Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück. Zeigen
+					Wikipedia wird durch Spenden von durchschnittlich 22,81&nbsp;€ finanziert. Doch schon mit einer Spende
+					von 10&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich entwickeln. { visitorsVsDonorsSentence } Die meisten Menschen spenden,
+					weil Sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert von 10&nbsp;€ geschenkt?
+					Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück. Zeigen
 					Sie den Freiwilligen, die Ihnen verlässliche und neutrale Informationen zur Verfügung stellen,
 					dass Sie ihre Arbeit wertschätzen. Vielen Dank!
 				</p>
