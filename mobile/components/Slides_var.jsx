9ce93bb7e8f3966e7649732e7cdce9334f11967b
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-11-18 14:22:05 +0100

    Implement VAR for C20_WMDE_Mobile_Test_06
    - changes var slides + bannertext + highlighting
    
    https://phabricator.wikimedia.org/T268106

---
+++ mobile/components/Slides_var.jsx
@@ -0,0 +1,36 @@
+// eslint-disable-next-line no-unused-vars
+import { h } from 'preact';
+
+export default function Slides( { formattedGoalDonationSumNumeric, campaignDaySentence, currentDayName, progressBar, visitorsVsDonorsSentence } ) {
+	return <div><div className="mini-banner-carousel">
+		<div className="carousel-cell">
+			<p className="goal-headline">Unser Spendenziel: { formattedGoalDonationSumNumeric } Millionen Euro</p>
+			{ progressBar }
+		</div>
+		<div className="carousel-cell">
+			<p>An alle unsere Leserinnen und Leser in Deutschland. Vielleicht kommen wir gerade ungelegen,
+				aber dennoch: Bitte klicken Sie jetzt nicht weg! { campaignDaySentence }</p>
+		</div>
+		<div className="carousel-cell">
+			<p style={'position:absolute'}>Zum ersten Mal seit langem möchten wir Sie an diesem { currentDayName } bescheiden darum bitten,
+				die Unabhängigkeit von Wikipedia zu verteidigen. <span className="text-highlight">{ visitorsVsDonorsSentence }</span></p>
+			<p className="space-placeholder">
+				<p>Liebe Leserinnen und Leser:</p>
+				<p>{ visitorsVsDonorsSentence } Wenn alle, die das
+					jetzt lesen, einen kleinen Beitrag leisten, wäre unsere Spendenkampagne am heutigen { currentDayName } vorbei.
+				</p>
+			</p>
+		</div>
+		<div className="carousel-cell">
+			<p>Wikipedia wird durch Spenden von durchschnittlich 22,81&nbsp;€ finanziert. <span className="text-highlight">Doch schon mit einer
+				Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich entwickeln.</span></p>
+		</div>
+		<div className="carousel-cell">
+			<p>Insgesamt spenden 99% unserer Leserinnen und Leser nichts – sie
+				übergehen diesen Aufruf. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert von 5 € geschenkt?
+			<span className="text-highlight"> Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück.</span>
+			</p>
+		</div>
+	</div>
+	</div>;
+}
