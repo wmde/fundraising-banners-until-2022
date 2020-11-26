ad1108278a28c89589b3561a76537a48e066eae6
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-11-26 20:29:04 +0100

    Prepare campaign for C20_WPDE_02
    https://phabricator.wikimedia.org/T268816

--- wikipedia.de/desktop/components/BannerText.jsx
+++ wikipedia.de/desktop/components/BannerText.jsx
@@ -28,7 +28,8 @@ export default function BannerText( props ) {
 						</TextHighlight>
 					</span>
 					<span className="text__paragraph--bold">
-						{' '}Wenn alle, die das jetzt lesen, einen kleinen Beitrag leisten, wäre unser Spendenziel bereits am heutigen { currentDayName } erreicht.{' '}
+						{' '}Wenn alle, die das jetzt lesen, einen kleinen Beitrag leisten, wäre
+						unser Spendenziel bereits am heutigen { currentDayName } erreicht.{' '}
 					</span>
 
 					<span className='ab-test-text'>Menschen spenden aus einem einfachen Grund – weil Wikipedia nützlich ist. </span>
@@ -40,12 +41,7 @@ export default function BannerText( props ) {
 
 					<span>
 						Sicher könnten wir mit Werbung eine Menge Geld verdienen. Aber dann wäre Wikipedia komplett anders.
-						Wir könnten ihr nicht vertrauen. </span>
-
-					<span className="optional-text text-xxl">
-						Das Herzstück von Wikipedia ist die Gemeinschaft von Menschen, die uns unbegrenzten Zugang zu verlässlichen und neutralen Informationen geben. </span>
-
-					<span>
+						Wir könnten ihr nicht vertrauen.
 						Es ist leicht, diese Nachricht zu ignorieren und die meisten werden das wohl tun. Wenn Sie Wikipedia nützlich
 						finden, nehmen Sie sich an diesem Donnerstag bitte eine Minute Zeit und geben Wikipedia mit Ihrer Spende etwas zurück. </span>
 
@@ -56,22 +52,45 @@ export default function BannerText( props ) {
 				</p>
 			</div>
 			<div className="banner-text-var">
-				<p>
+				<p className="text__headline">
 					<img className="info-icon" src="https://upload.wikimedia.org/wikipedia/donate/9/99/RedInfoI.svg" alt="info_icon" width="16" height="16" />
-					<strong> An alle unsere Leserinnen und Leser in Deutschland.</strong>
+					<span className="text__headline--bold"> Liebe Leserinnen und Leser,</span>
+					<span> bitte verzeihen Sie die Störung. Es ist ein bisschen unangenehm, daher kommen wir gleich zur Sache. </span>
+					{capitalizeFirstLetter( weekdayPrepPhrase )} {currentDayName} sind Sie in Deutschland
+					gefragt:
 				</p>
-				<p>
-					Vielleicht kommen wir gerade ungelegen, aber dennoch: Bitte klicken Sie jetzt nicht weg! {campaignDaySentence} Zum ersten
-					Mal seit langem möchten wir Sie an diesem { currentDayName } bescheiden
-					darum bitten, die Unabhängigkeit von Wikipedia zu verteidigen. Insgesamt spenden 99% unserer Leserinnen und Leser
-					nichts – sie übergehen diesen Aufruf. Sollten Sie zu dem kleinen Kreis gehören, die bereits
-					gespendet haben, danken wir Ihnen sehr herzlich. Wikipedia wird durch Spenden von durchschnittlich
-					22,81 € finanziert. Doch schon mit einer Spende von 5 € kann Wikipedia sich auch in Zukunft
-					erfolgreich entwickeln. { visitorsVsDonorsSentence } Die meisten
-					Menschen spenden, weil sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in
-					diesem Jahr Wissen im Wert von 5 € geschenkt? Dann nehmen Sie sich doch bitte eine Minute Zeit und
-					geben Sie etwas zurück. Zeigen Sie den Freiwilligen, die Ihnen verlässliche und neutrale
-					Informationen zur Verfügung stellen, dass Sie ihre Arbeit wertschätzen. Vielen Dank!
+
+				<p className="text__paragraph">
+					{' '}{campaignDaySentence} Wikipedia wird durch Spenden von durchschnittlich 22,81&nbsp;€ finanziert, aber
+					99&nbsp;% der Leserinnen und Leser spenden nicht.
+
+					<span className="optional-text-highlight">
+						<TextHighlight registerStartAnimation={ props.registerStartHighlight }>
+							{' '}Wenn alle, die das jetzt lesen, einen kleinen Beitrag leisten, wäre unser Spendenziel bereits am heutigen { currentDayName } erreicht.{' '}
+						</TextHighlight>
+					</span>
+					<span className="text__paragraph--bold">
+						{' '}Wenn alle, die das jetzt lesen, einen kleinen Beitrag leisten, wäre
+						unser Spendenziel bereits am heutigen { currentDayName } erreicht.{' '}
+					</span>
+
+					<span className='ab-test-text'>Menschen spenden aus einem einfachen Grund – weil Wikipedia nützlich ist. </span>
+
+					<span>Schon der Preis einer Tasse Kaffee würde genügen. { visitorsVsDonorsSentence } </span>
+
+					<span className="optional-text text-l">
+						Wenn Wikipedia eine kommerzielle Seite sein würde, wäre das ein riesiger Verlust für die Welt. </span>
+
+					<span>
+						Sicher könnten wir mit Werbung eine Menge Geld verdienen. Aber dann wäre Wikipedia komplett anders.
+						Wir könnten ihr nicht vertrauen.
+						Es ist leicht, diese Nachricht zu ignorieren und die meisten werden das wohl tun. Wenn Sie Wikipedia nützlich
+						finden, nehmen Sie sich an diesem Donnerstag bitte eine Minute Zeit und geben Wikipedia mit Ihrer Spende etwas zurück. </span>
+
+					<span className="text__paragraph--italic">
+						{' '}Vielen Dank! </span>
+
+					<span className='ab-test-text hidden'>Menschen spenden aus einem einfachen Grund – weil Wikipedia nützlich ist.</span>
 				</p>
 			</div>
 		</div>
