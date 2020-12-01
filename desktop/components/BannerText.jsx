cba707a49d22fd730fc11333397fb31d14f57e78
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-12-01 15:11:07 +0100

    Implement VAR for C20_WMDE_Test_20
    - the real new test 20 (changes copy text)
    
    https://phabricator.wikimedia.org/T269103

--- desktop/components/BannerText.jsx
+++ desktop/components/BannerText.jsx
@@ -38,7 +38,7 @@ export default function BannerText( props ) {
 					Sie zu dem kleinen Kreis gehören, die bereits gespendet haben, danken wir Ihnen sehr herzlich.
 					Wikipedia wird durch Spenden von durchschnittlich 22,81&nbsp;€ finanziert. Doch schon mit einer Spende
 					von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich entwickeln. { visitorsVsDonorsSentence } Die meisten Menschen spenden,
-					weil sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert von 5&nbsp;€ geschenkt?
+					weil sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt?
 					Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück. <span className="optional-text text-l">Zeigen
 			Sie den Freiwilligen, die Ihnen verlässliche und neutrale Informationen zur Verfügung stellen,
 			dass Sie ihre Arbeit wertschätzen.</span> Vielen Dank!
