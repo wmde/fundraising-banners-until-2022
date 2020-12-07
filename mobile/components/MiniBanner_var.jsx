d87d59ff2539ac1afe1fdafee92ea2b8c4cc497b
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-12-07 19:29:10 +0100

    Implement VAR for C20_WMDE_Mobile_Test_09
    - based on mobile test 07 VAR
    - introduces use of funds link to minibanner
    
    https://phabricator.wikimedia.org/T269571

---
+++ mobile/components/MiniBanner_var.jsx
@@ -0,0 +1,64 @@
+// eslint-disable-next-line no-unused-vars
+import { h } from 'preact';
+import Infobox from '../../shared/components/ui/Infobox';
+import ProgressBar, { AmountToShowOnRight } from '../../shared/components/ui/ProgressBar';
+import * as PropTypes from 'prop-types';
+
+export default function MiniBanner( props ) {
+	const campaignProjection = props.campaignProjection;
+	return <div className="mini-banner">
+		<div className="mini-banner__box">
+			<div className="mini-banner__content">
+				<header className="headline">
+					<div className="headline__container">
+						<span className="headline__content">{ props.sliderHeading }</span>
+					</div>
+				</header>
+				<div className="close-button" onClick={props.onClose}/>
+
+				<Infobox
+					campaignParameters={props.campaignParameters}
+					campaignProjection={campaignProjection}
+					formatters={props.formatters}
+					bannerText={props.slides}
+					propsForText={{
+						formattedGoalDonationSumNumeric: props.formatters.millionFormatterNumeric( campaignProjection.goalDonationSum ),
+						progressBar: ( <ProgressBar
+							formatters={props.formatters}
+							daysLeft={campaignProjection.getRemainingDays()}
+							donationAmount={campaignProjection.getProjectedDonationSum()}
+							goalDonationSum={campaignProjection.goalDonationSum}
+							missingAmount={campaignProjection.getProjectedRemainingDonationSum()}
+							setStartAnimation={props.setStartAnimation}
+							animate={true}
+							amountToShowOnRight={AmountToShowOnRight.MISSING}
+						/> )
+					}}
+				/>
+
+				<div className="mini-banner__tab">
+					<button className="mini-banner__tab-inner mini-banner__button" onClick={props.onExpandFullpage}>
+						Jetzt spenden
+					</button>
+				</div>
+			</div>
+			<div className="smallprint-mini">
+				<span>
+					<a className="application-of-funds-link"
+						href={`https://spenden.wikimedia.de/use-of-funds?${ props.trackingParams }`}
+						onClick={ props.toggleFundsModal } >Wohin geht meine Spende?</a>
+				</span>
+			</div>
+		</div>
+	</div>;
+}
+
+MiniBanner.propTypes = {
+	slides: PropTypes.element,
+	onClose: PropTypes.func,
+	formatters: PropTypes.object,
+	campaignProjection: PropTypes.any,
+	campaignParameters: PropTypes.object,
+	startAnimation: PropTypes.func,
+	onExpandFullpage: PropTypes.func
+};
