e837bc7fed2ec498dc5ec8b55482660fd950d3ee
Abban Dunne <abban.dunne@wikimedia.de>
2021-09-03 11:36:40 +0200

    Implement VAR for C21_WMDE_Test_05
    Ticket: https://phabricator.wikimedia.org/T289459

--- desktop/banner_var.js
+++ desktop/banner_var.js
@@ -1,23 +1,22 @@
 // eslint-disable-next-line no-unused-vars
-import style from './styles/var/styles.pcss';
+import style from './styles/styles_var.pcss';
 
 import * as formatters from '../shared/number_formatter/de';
 import { createCampaignParameters } from '../shared/campaign_parameters';
 import { createTrackingData } from '../shared/tracking_data';
 import { getTrackingIds } from '../shared/tracking_ids';
 
-import { Banner } from './components/Banner_var';
+import Banner from './components/Banner_var';
 import { BannerType } from './BannerType';
 import BannerPresenter from '../shared/banner_presenter';
 import Translations from '../shared/messages/de';
-import LocalTranslations from './translations';
-import DonationForm from './components/ui/form/DonationForm';
 import Footer from '../shared/components/ui/EasySelectFooter';
-import BannerText from './components/BannerText_var';
 import useOfFundsText from '../node_modules/fundraising-frontend-content/i18n/de_DE/data/use_of_funds_content.json';
 import { createCampaignProjection } from '../shared/campaign_projection';
 import { createFormItems } from './form_items';
 import { LocalImpressionCount } from '../shared/local_impression_count';
+import DonationForm from '../shared/components/ui/form/DonationForm';
+import LocalTranslations from './translations';
 
 const bannerContainer = document.getElementById( 'WMDE-Banner-Container' );
 const campaignParameters = createCampaignParameters();
@@ -27,8 +26,7 @@ const trackingData = createTrackingData( trackingIds.bannerName );
 const bannerPresenter = new BannerPresenter(
 	trackingData,
 	bannerContainer.dataset.delay || 7500,
-	new LocalImpressionCount( trackingIds.bannerName ),
-	mw.centralNotice.internal.hide.setHideWithCloseButtonCookies
+	new LocalImpressionCount( trackingIds.bannerName )
 );
 
 bannerPresenter.present(
@@ -40,11 +38,10 @@ bannerPresenter.present(
 		campaignProjection,
 		formatters,
 		useOfFundsText,
-		donationForm: DonationForm,
 		footer: Footer,
-		bannerText: BannerText,
+		donationForm: DonationForm,
 		translations: Object.assign( Translations, LocalTranslations ),
 		formItems: createFormItems( Translations, formatters.amountInputFormatter ),
-		bannerType: BannerType.VAR
+		bannerType: BannerType.CTRL
 	}
 );
