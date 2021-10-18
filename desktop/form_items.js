afc376ccb17ff657c1f9efb7723be736c6e6d744
Abban Dunne <abban.dunne@wikimedia.de>
2021-10-18 14:31:12 +0200

    Prepare campaign for C21_WMDE_Test_07
    Added missing linting fixes from #683

--- desktop/form_items.js
+++ desktop/form_items.js
@@ -1,11 +1,10 @@
-import FormItemsBuilder, { Intervals, PaymentMethods } from '../shared/components/ui/form/FormItemsBuilder';
+import FormItemsBuilder, { Intervals, PaymentMethods, AddressType } from './components/ui/form/FormItemsBuilder';
 
 export function createFormItems( translations, amountFormatter ) {
 	const builder = new FormItemsBuilder( translations, amountFormatter );
 	builder.setIntervals(
 		Intervals.ONCE,
 		Intervals.MONTHLY,
-		Intervals.QUARTERLY,
 		Intervals.YEARLY
 	);
 	builder.setAmounts( 5, 10, 20, 25, 50, 100 );
@@ -14,6 +13,11 @@ export function createFormItems( translations, amountFormatter ) {
 		PaymentMethods.BANK_TRANSFER,
 		PaymentMethods.CREDIT_CARD,
 		PaymentMethods.PAYPAL
+	);
+	builder.setAddressType(
+		AddressType.FULL,
+		AddressType.EMAIL,
+		AddressType.NO
 	);
 	return builder.getItems();
 }
