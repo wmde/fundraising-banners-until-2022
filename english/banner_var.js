51bfb75478b770dfdc63ab24f78df1870429c25b
Abban Dunne <abban.dunne@wikimedia.de>
2021-10-27 15:15:00 +0200

    Prepare campaign for C21_WMDE_Test_EN_02

--- english/banner_var.js
+++ english/banner_var.js
@@ -6,7 +6,7 @@ import { createCampaignParameters } from '../shared/campaign_parameters';
 import { createTrackingData } from '../shared/tracking_data';
 import { getTrackingIds } from '../shared/tracking_ids';
 
-import Banner, { BannerType } from './components/Banner_var';
+import Banner, { BannerType } from './components/Banner';
 import BannerPresenter from '../shared/banner_presenter';
 import Translations from '../shared/messages/en';
 import DonationForm from './components/ui/form/DonationForm';
