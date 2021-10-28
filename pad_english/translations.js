faa509f857b3c132025849660d4e9d994c8f8f90
Gabriel Birke <gabriel.birke@posteo.net>
2021-10-28 18:28:56 +0200

    Implement VAR for C21_WMDE_ipad_EN_Test_01
    VAR is the blue banner from C21_WMDE_EN_Test_02
    
    Shrink CTRL banner to match heights
    
    Banner Task: https://phabricator.wikimedia.org/T294100
    
    Track *all* size issues, as requested by Task
    https://phabricator.wikimedia.org/T294553
    
    Don't track slide number, as requested by
    https://phabricator.wikimedia.org/T294551
    This removes slide tracking from ALL banners, because the close handler
    and tracking is part of `BannerPresenter`.

---
+++ pad_english/translations.js
@@ -0,0 +1,11 @@
+const Translations = {
+	'intervals-header': 'I will donate:',
+	'amounts-header': 'Amount:',
+	'payments-header': 'Using:',
+	'next-button': 'Support now',
+	'back-button': 'Back',
+	'address-type-notice-full': '(For postal donation receipt and email confirmation)',
+	'address-type-notice-email': '(For email confirmation)'
+};
+
+export default Translations;
