59af048dd1eb952e2be4a9efa146edf911211788
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2021-10-29 16:53:46 +0200

    Implement VAR for C21_WPDE_mob01
    https://phabricator.wikimedia.org/T294104

---
+++ wikipedia.de/mobile/components/FullpageBanner_var.jsx
@@ -0,0 +1,42 @@
+import { h, Component } from 'preact';
+import classNames from 'classnames';
+
+import Infobox from '../../../shared/components/ui/Infobox';
+
+export default class FullpageBanner extends Component {
+	render( props ) {
+		const DonationForm = props.donationForm;
+		const trackingParams = `piwik_campaign=${props.campaignName}&piwik_kwd=${props.bannerName}_link`;
+
+		return <div className={ classNames( 'fullpage-banner', { visible: props.isFullPageVisible && props.bannerVisible } ) }>
+
+			<div className="fullpage-banner__close" onClick={ props.onClose }/>
+			<div className="fullpage-banner__info">
+				<div className="fullpage-banner__heading">
+					Jetzt spenden
+				</div>
+				<Infobox
+					formatters={props.formatters}
+					campaignParameters={props.campaignParameters}
+					campaignProjection={props.campaignProjection}
+					bannerText={props.bannerText}
+					propsForText={ { overallImpressionCount: props.impressionCounts.getOverallCount() } }
+				/>
+			</div>
+			<div className="call-to-action">
+				Jetzt sind Sie <span className="call-to-action__optional-text">in Deutschland</span> gefragt.
+			</div>
+			<DonationForm
+				formItems={props.formItems}
+				bannerName={props.bannerName}
+				campaignName={props.campaignName}
+				formatters={props.formatters}
+				impressionCounts={props.impressionCounts}
+				customAmountPlaceholder={ props.translations[ 'custom-amount-placeholder-short' ] }
+				onSubmit={props.onSubmit}
+				trackingParams={trackingParams}
+				toggleFundsModal={props.toggleFundsModal}
+			/>
+		</div>;
+	}
+}
