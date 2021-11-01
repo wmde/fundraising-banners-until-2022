95af9a0ef147c92d8b350374630bb7b227d13552
Abban Dunne <abban.dunne@wikimedia.de>
2021-11-01 16:23:10 +0100

    Fix text and other small bugs

--- mobile/components/BannerText.jsx
+++ mobile/components/BannerText.jsx
@@ -1,27 +1,20 @@
 import { h } from 'preact';
 
 export default function BannerText( props ) {
-	const { currentDayName, campaignDaySentence, visitorsVsDonorsSentence } = props.dynamicCampaignText;
+	const { currentDayName, campaignDaySentence, visitorsVsDonorsSentence, overallImpressionCount } = props.dynamicCampaignText;
 	return <div className="banner-text">
-		<p className="text__headline">
-			<span>An alle unsere Leserinnen und Leser in Deutschland. Vielleicht kommen wir
-				gerade ungelegen, aber dennoch: Bitte klicken Sie jetzt nicht weg! { campaignDaySentence }</span>
-		</p>
 		<p>
-			Zum ersten Mal seit langem möchten wir Sie an diesem { currentDayName } bescheiden
-			darum bitten, die Unabhängigkeit von Wikipedia zu verteidigen.
-			Insgesamt spenden 99% unserer Leserinnen und Leser nichts – sie übergehen
-			diesen Aufruf. Sollten Sie zu dem kleinen Kreis gehören, die bereits gespendet haben,
-			danken wir Ihnen sehr herzlich. Wikipedia wird durch Spenden von durchschnittlich 21,60&nbsp;€ finanziert.
-			Doch schon mit einer Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich entwickeln.
-			{' '}{ visitorsVsDonorsSentence }
-
-			Die meisten Menschen spenden, weil sie Wikipedia nützlich finden.
-			Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt?
-			Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück. Zeigen Sie den
-			Freiwilligen, die Ihnen verlässliche und neutrale Informationen
-			zur Verfügung stellen, dass Sie ihre Arbeit wertschätzen.
-			<span className="text--italic"> Vielen Dank!</span>
+			<strong>An alle, die Wikipedia in Deutschland nutzen</strong>
+		</p>
+		<p>Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! { campaignDaySentence }</p>
+		<p>
+			Zum { overallImpressionCount }. Mal seit langem möchten wir Sie an diesem { currentDayName } bescheiden darum bitten,
+			die Unabhängigkeit von Wikipedia zu sichern. Insgesamt spenden 99% nichts - sie übergehen diesen Aufruf. Sollten Sie
+			zu dem kleinen Kreis gehören, die bereits gespendet haben, danken wir Ihnen sehr herzlich. Wikipedia wird durch Spenden
+			von durchschnittlich 21,60&nbsp;€ finanziert. Schon mit einer Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft
+			erfolgreich entwickeln. { visitorsVsDonorsSentence } Die meisten Menschen spenden, weil sie Wikipedia nützlich finden.
+			Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? Dann nehmen Sie sich doch bitte eine
+			Minute Zeit und geben Sie etwas zurück. <em>Vielen Dank!</em>
 		</p>
 	</div>;
 }
