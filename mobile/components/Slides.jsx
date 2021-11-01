4de6cdaa0ee226a438580a3f968f730457710fa8
Abban Dunne <abban.dunne@wikimedia.de>
2021-11-01 16:23:10 +0100

    Fix text and other small bugs

--- mobile/components/Slides.jsx
+++ mobile/components/Slides.jsx
@@ -1,37 +1,41 @@
 import { h } from 'preact';
 
 export default function Slides( dynamicCampaignText, progressBar ) {
-	const { currentDayName, visitorsVsDonorsSentence } = dynamicCampaignText;
+	const { currentDayName, campaignDaySentence, visitorsVsDonorsSentence, overallImpressionCount } = dynamicCampaignText;
 
 	return [
 		{
 			content: <div className="carousel-cell-first">
-				<p>Liebe Leserinnen und Leser in Deutschland: Zum ersten Mal seit langem möchten wir Sie an diesem{ ' ' }
-					{ currentDayName } bescheiden darum bitten, die Unabhängigkeit von Wikipedia zu sichern.</p>
+				<p>Unser Spendenziel: 9,2 Millionen Euro</p>
 				{ progressBar }
 			</div>
 		},
 		{
 			content: <div>
-				<p>Vielleicht kommen wir gerade ungelegen, aber dennoch: Bitte klicken Sie jetzt nicht weg!</p>
+				<p>An alle, die Wikipedia in Deutschland nutzen: Zum { overallImpressionCount }. Mal seit langem möchten wir Sie an
+					diesem { currentDayName } bescheiden darum bitten, die Unabhängigkeit von Wikipedia zu sichern.</p>
 			</div>
 		},
 		{
 			content: <div>
-				<p>Insgesamt spenden 99% unserer Leserinnen und Leser nichts - sie übergehen diesen Aufruf. <span className="text-highlight">Doch
-					schon mit einer Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich entwickeln.</span></p>
+				<p>Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! { campaignDaySentence }</p>
 			</div>
 		},
 		{
 			content: <div>
-				<p>Wikipedia wird durch Spenden von durchschnittlich 21,60&nbsp;€ finanziert.{ ' ' }
-					<span className="text-highlight">{ visitorsVsDonorsSentence }</span></p>
+				<p>Insgesamt spenden 99% nichts - sie übergehen diesen Aufruf. <strong>Schon mit einer Spende von 5&nbsp;€ kann
+					Wikipedia sich auch in Zukunft erfolgreich entwickeln.</strong></p>
 			</div>
 		},
 		{
 			content: <div>
-				<p>Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt?
-					<span className="text-highlight"> Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück.</span></p>
+				<p>{ visitorsVsDonorsSentence } Wikipedia wird durch Spenden von durchschnittlich 21,60&nbsp;€ finanziert.</p>
+			</div>
+		},
+		{
+			content: <div>
+				<p>Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? <strong>Dann nehmen Sie sich
+					doch bitte eine Minute Zeit und geben Sie etwas zurück.</strong> Vielen Dank.</p>
 			</div>
 		}
 	];
--- mobile/create_dynamic_campaign_text.js
+++ mobile/create_dynamic_campaign_text.js
@@ -3,7 +3,7 @@ import CampaignDays, { endOfDay, startOfDay } from '../shared/campaign_days';
 import CampaignDaySentence from '../shared/campaign_day_sentence';
 import VisitorsVsDonorsSentence from '../shared/visitors_vs_donors_sentence';
 
-export default function createDynamicCampaignText( campaignParameters, campaignProjection, formatters, translations ) {
+export default function createDynamicCampaignText( campaignParameters, campaignProjection, impressionCounts, formatters, translations ) {
 	const dayName = new DayName( new Date() );
 	const currentDayName = translations[ dayName.getDayNameMessageKey() ];
 	const campaignDays = new CampaignDays(
@@ -21,6 +21,7 @@ export default function createDynamicCampaignText( campaignParameters, campaignP
 	return {
 		currentDayName,
 		campaignDaySentence: campaignDaySentence.getSentence(),
-		visitorsVsDonorsSentence: visitorsVsDonorsSentence.getSentence()
+		visitorsVsDonorsSentence: visitorsVsDonorsSentence.getSentence(),
+		overallImpressionCount: impressionCounts.getOverallCount() + 1
 	};
 }
