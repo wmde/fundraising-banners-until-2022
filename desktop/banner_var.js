08b475a3dace89e4dcb01fb03a28d7d23795f89d
Abban Dunne <abban.dunne@wikimedia.de>
2021-11-12 10:07:52 +0100

    Implement VAR for C21_WMDE_Test_10
    Same as ctrl but with maybe later functionality
    
    Ticket: https://phabricator.wikimedia.org/T295573

--- desktop/banner_var.js
+++ desktop/banner_var.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
-import style from './styles/styles.pcss';
+import style from './styles/styles_var.pcss';
 
 import * as formatters from '../shared/number_formatter/de';
 import { createCampaignParameters } from '../shared/campaign_parameters';
 import { createTrackingData } from '../shared/tracking_data';
 import { getTrackingIds } from '../shared/tracking_ids';
 
-import { Banner } from './components/Banner';
+import { Banner } from './components/Banner_var';
 import { BannerType } from './BannerType';
 import BannerPresenter from '../shared/banner_presenter';
 import Translations from '../shared/messages/de';
@@ -18,12 +18,14 @@ import useOfFundsText from '../node_modules/fundraising-frontend-content/i18n/de
 import { createCampaignProjection } from '../shared/campaign_projection';
 import { createFormItems } from './form_items';
 import { LocalImpressionCount } from '../shared/local_impression_count';
+import { createMinimisedPersistence } from './minimised_persistence';
 
 const bannerContainer = document.getElementById( 'WMDE-Banner-Container' );
 const campaignParameters = createCampaignParameters();
 const campaignProjection = createCampaignProjection( campaignParameters );
 const trackingIds = getTrackingIds( bannerContainer );
 const trackingData = createTrackingData( trackingIds.bannerName );
+const minimisedPersistence = createMinimisedPersistence( trackingIds.bannerName );
 const bannerPresenter = new BannerPresenter(
 	trackingData,
 	bannerContainer.dataset.delay || 7500,
@@ -45,6 +47,7 @@ bannerPresenter.present(
 		bannerText: BannerText,
 		translations: Object.assign( Translations, LocalTranslations ),
 		formItems: createFormItems( Translations, formatters.amountInputFormatter ),
-		bannerType: BannerType.VAR
+		bannerType: BannerType.VAR,
+		minimisedPersistence
 	}
 );
