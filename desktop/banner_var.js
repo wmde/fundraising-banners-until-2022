e350e1b6f9495ed0c8bc0ecb75959330d8490707
Abban Dunne <abban.dunne@wikimedia.de>
2021-11-26 05:23:07 +0100

    Prepare campaign for C21_WMDE_Test_15
    There is no VAR banner in this campaign, this
    test is on a new address type selector on Spenden
    
    Ticket: https://phabricator.wikimedia.org/T296390

--- desktop/banner_var.js
+++ desktop/banner_var.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
-import style from './styles_var/styles.pcss';
+import style from './styles/styles.pcss';
 
 import * as formatters from '../shared/number_formatter/de';
 import { createCampaignParameters } from '../shared/campaign_parameters';
 import { createTrackingData } from '../shared/tracking_data';
 import { getTrackingIds } from '../shared/tracking_ids';
 
-import { Banner } from './components/Banner_var';
+import { Banner } from './components/Banner';
 import { BannerType } from './BannerType';
 import BannerPresenter from '../shared/banner_presenter';
 import Translations from '../shared/messages/de';
@@ -18,14 +18,12 @@ import useOfFundsText from '../node_modules/fundraising-frontend-content/i18n/de
 import { createCampaignProjection } from '../shared/campaign_projection';
 import { createFormItems } from './form_items';
 import { LocalImpressionCount } from '../shared/local_impression_count';
-import { createMinimisedPersistence } from './minimised_persistence';
 
 const bannerContainer = document.getElementById( 'WMDE-Banner-Container' );
 const campaignParameters = createCampaignParameters();
 const campaignProjection = createCampaignProjection( campaignParameters );
 const trackingIds = getTrackingIds( bannerContainer );
 const trackingData = createTrackingData( trackingIds.bannerName );
-const minimisedPersistence = createMinimisedPersistence( trackingIds.bannerName );
 const bannerPresenter = new BannerPresenter(
 	trackingData,
 	bannerContainer.dataset.delay || 7500,
@@ -48,7 +46,6 @@ bannerPresenter.present(
 		translations: Object.assign( Translations, LocalTranslations ),
 		formItems: createFormItems( Translations, formatters.amountInputFormatter ),
 		bannerType: BannerType.VAR,
-		minimisedPersistence,
-		showCookieBanner: '0'
+		formActionProps: { ocn: '0', ast: '3' }
 	}
 );
