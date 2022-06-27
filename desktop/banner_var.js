1c445260368ee6ee4dd51302378dd44ec5bc4361
Abban Dunne <abban.dunne@wikimedia.de>
2022-06-27 08:54:19 +0200

    Implement VAR for C22_WMDE_Test_01
    * Remove transition.
    * Make banner appear immediately.
    * Adjust presenter and skins.
    
    Ticket: https://phabricator.wikimedia.org/T310985

--- desktop/banner_var.js
+++ desktop/banner_var.js
@@ -1,14 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-import style from './styles/styles.pcss';
+import style from './styles/styles_var.pcss';
 
 import * as formatters from '../shared/number_formatter/de';
 import { createCampaignParameters } from '../shared/campaign_parameters';
 import { createTrackingData } from '../shared/tracking_data';
 import { getTrackingIds } from '../shared/tracking_ids';
 
-import { Banner } from './components/Banner';
+import { Banner } from './components/Banner_var';
 import { BannerType } from './BannerType';
-import BannerPresenter from '../shared/banner_presenter';
+import BannerPresenter from '../shared/banner_presenter_immediate';
 import Translations from '../shared/messages/de';
 import LocalTranslations from './translations';
 import DonationForm from '../shared/components/ui/form/DonationForm';
@@ -26,7 +26,6 @@ const trackingIds = getTrackingIds( bannerContainer );
 const trackingData = createTrackingData( trackingIds.bannerName );
 const bannerPresenter = new BannerPresenter(
 	trackingData,
-	bannerContainer.dataset.delay || 7500,
 	new LocalImpressionCount( trackingIds.bannerName ),
 	mw.centralNotice.internal.hide.setHideWithCloseButtonCookies
 );
@@ -46,6 +45,7 @@ bannerPresenter.present(
 		translations: Object.assign( Translations, LocalTranslations ),
 		formItems: createFormItems( Translations, formatters.amountInputFormatter ),
 		bannerType: BannerType.VAR,
-		showCookieBanner: '0'
+		showCookieBanner: '0',
+		initialBannerWidth: window.innerWidth
 	}
 );
