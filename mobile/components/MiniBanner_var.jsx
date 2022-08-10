55fd453a6030668bc4b1f32d6b2a2e4c94aaa4ae
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2022-08-10 18:05:02 +0200

    Implement VAR for C22_WMDE_Mobile_Test_03
    https://phabricator.wikimedia.org/T313719

---
+++ mobile/components/MiniBanner_var.jsx
@@ -0,0 +1,48 @@
+import { h } from 'preact';
+import * as PropTypes from 'prop-types';
+import Slider from '../../shared/components/Slider';
+import Slides from './Slides';
+
+export default function MiniBanner( props ) {
+	return <div className="mini-banner">
+		<div className="mini-banner__box">
+			<div className="mini-banner__content">
+				<div className="banner__close">
+					<button className="close-button" onClick={props.onClose}>schließen</button>
+				</div>
+
+				<header className="headline">
+					<div className="headline__container">
+						<span className="headline__content">Ist Ihnen Wikipedia 5&nbsp;€ wert?</span>
+					</div>
+				</header>
+
+				<div className="banner__slideshow">
+					<Slider
+						slides={ Slides( props.dynamicCampaignText ) }
+						onSlideChange={ props.onSlideChange }
+						registerAutoplay={ props.registerSliderAutoplayCallbacks }
+						interval={ props.sliderAutoPlaySpeed }
+						sliderOptions={ { loop: false } }
+					/>
+				</div>
+
+				<div className="mini-banner__tab">
+					<button className="mini-banner__tab-inner mini-banner__button" onClick={props.onExpandFullpage}>
+						Jetzt spenden
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>;
+}
+
+MiniBanner.propTypes = {
+	slides: PropTypes.element,
+	onClose: PropTypes.func,
+	formatters: PropTypes.object,
+	campaignProjection: PropTypes.any,
+	campaignParameters: PropTypes.object,
+	startAnimation: PropTypes.func,
+	onExpandFullpage: PropTypes.func
+};
