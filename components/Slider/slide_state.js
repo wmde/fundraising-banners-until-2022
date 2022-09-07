917d33f02926ef0b2e812b4ee6337c1605e15452
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-09-07 12:44:41 +0200

    Refactor Slides

---
+++ components/Slider/slide_state.js
@@ -0,0 +1,16 @@
+/**
+ * Data structure for keeping track of shown slides in a banner.
+ *
+ * The data is used in event tracking
+ */
+export default class SlideState {
+	constructor() {
+		this.currentSlide = 0;
+		this.slidesShown = 0;
+	}
+
+	onSlideChange( newSlideIndex ) {
+		this.slidesShown++;
+		this.currentSlide = newSlideIndex;
+	}
+}
