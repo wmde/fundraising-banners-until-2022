479b086f22c2a384bbbb0dc84d5877af156e1b76
Abban Dunne <abban.dunne@wikimedia.de>
2022-09-07 16:20:03 +0200

    Move DonationForm

---
+++ components/DonationForm/SelectionInput.jsx
@@ -0,0 +1,41 @@
+import { Component, createRef, h } from 'preact';
+import classNames from 'classnames';
+
+export default class SelectionInput extends Component {
+
+	ref = createRef();
+
+	constructor( props ) {
+		super( props );
+		this.state = {
+			focused: false
+		};
+	}
+
+	handleFocus = e => {
+		e.preventDefault();
+		setTimeout( () => this.ref.current.setSelectionRange( 0, 9999 ), 1 );
+		this.setState( { focused: true } );
+	};
+
+	handleBlur = () => {
+		this.setState( { focused: false } );
+	};
+
+	// eslint-disable-next-line no-unused-vars
+	render( props, state, context ) {
+		return <div className={ classNames( 'selection-input', { focused: state.focused } ) }>
+			<span className="selection-input-text">{ props.value }</span>
+			<input
+				className="selection-input-input"
+				type="text"
+				readOnly={ true }
+				value={ props.focusedValue || props.value }
+				onFocus={ this.handleFocus }
+				onBlur={ this.handleBlur }
+				ref={ this.ref }
+			/>
+		</div>;
+	}
+
+}
---
+++ components/DonationForm/SubmitValues.jsx
@@ -0,0 +1,19 @@
+import { h, Fragment } from 'preact';
+import PropTypes from 'prop-types';
+
+export default function SubmitValues( props ) {
+	let addressType = props.addressType || '';
+	return <Fragment>
+		<input type="hidden" name="addressType" value={ addressType } />
+		<input type="hidden" name="amount" value={ props.amount } />
+		<input type="hidden" name="interval" value={ props.interval } />
+		<input type="hidden" name="paymentType" value={ props.paymentType } />
+	</Fragment>;
+}
+
+SubmitValues.propTypes = {
+	addressType: PropTypes.string,
+	amount: PropTypes.string.isRequired,
+	interval: PropTypes.string.isRequired,
+	paymentType: PropTypes.string.isRequired
+};
