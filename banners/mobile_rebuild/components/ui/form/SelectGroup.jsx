98845ba437000d7949cf0b3285667d78434efa60
Abban Dunne <abban.dunne@wikimedia.de>
2022-09-09 13:20:25 +0200

    Move mobile components to new structure

---
+++ banners/mobile_rebuild/components/ui/form/SelectGroup.jsx
@@ -0,0 +1,62 @@
+import { h } from 'preact';
+import classNames from 'classnames';
+
+export const ErrorPosition = Object.freeze( {
+	TOP: Symbol( 'top' ),
+	BOTTOM: Symbol( 'bottom' )
+} );
+
+export function SelectGroup( props ) {
+
+	const errorPosition = props.errorPosition || ErrorPosition.BOTTOM;
+
+	const error = <span className="select-group__errormessage">
+		<span className="select-group__erroricon">
+			{ props.errorMessage }
+		</span>
+	</span>;
+
+	return <div
+		className={ classNames(
+			`select-group-container--${ props.fieldname }`,
+			{
+				'select-group-container': true,
+				'select-group-container--with-error': !props.isValid
+			}
+		) }>
+
+		{ errorPosition === ErrorPosition.TOP ? error : null }
+
+		<div className="select-group">
+			{ props.selectionItems.map( ( { value, label, notice } ) => (
+				<div key={ value } className={ classNames(
+					'select-group__option',
+					`select-group__option--${ props.fieldname }-${value.replace( ' ', '-' )}`,
+					{ 'select-group__disabled': props.disabledOptions.indexOf( value ) > -1 }
+				) }>
+					<label>
+						<input
+							type="radio"
+							onClick={ props.onSelected }
+							checked={ value === props.currentValue }
+							name={ props.fieldname }
+							value={ value }
+							disabled={ props.disabledOptions.indexOf( value ) > -1 }
+							className="select-group__input"/>
+						<span className="select-group__state">{ label || value }</span>
+					</label>
+					{ notice ? <span className={ classNames(
+						'select-group__notice',
+						{
+							'select-group__notice--selected': value === props.currentValue
+						}
+					) }>{ notice }</span> : null }
+				</div>
+			) ) }
+			{ props.children }
+		</div>
+
+		{ errorPosition === ErrorPosition.BOTTOM ? error : null }
+
+	</div>;
+}
