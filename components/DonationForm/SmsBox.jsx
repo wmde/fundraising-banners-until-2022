f7654097b76d8842792e54142e6410c1214abebc
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-09-09 18:13:01 +0200

    Style SMS payment
    Convert class names
    Hide in goku (Desktop) theme
    Style in gohan theme
    
    Add comments in themes to help our future selves keep styles in sync

--- components/DonationForm/SmsBox.jsx
+++ components/DonationForm/SmsBox.jsx
@@ -4,10 +4,10 @@ import TranslationContext from '../../shared/components/TranslationContext';
 
 export default function SmsBox() {
 	const Translations = useContext( TranslationContext );
-	return <div className="sms-box">
-		<label className="select-group__option">
-			<a href="sms:81190;?&body=WIKI" className="select-group__state">{ Translations[ 'sms-payment-message' ] }</a>
+	return <div className="wmde-banner-sms-box">
+		<label>
+			<a href="sms:81190;?&body=WIKI" className="wmde-banner-sms-box-label">{ Translations[ 'sms-payment-message' ] }</a>
 		</label>
-		<span className="sms-box__hint">{ Translations[ 'sms-info-message' ] }</span>
+		<span className="wmde-banner-sms-box-notice">{ Translations[ 'sms-info-message' ] }</span>
 	</div>;
 }
