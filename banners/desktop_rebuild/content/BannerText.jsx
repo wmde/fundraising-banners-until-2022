f54f27fcd71c87168c5220d78616ec514fa47821
Abban Dunne <abban.dunne@wikimedia.de>
2022-09-14 07:04:01 +0200

    Port test 05 to new banner

--- banners/desktop_rebuild/content/BannerText.jsx
+++ banners/desktop_rebuild/content/BannerText.jsx
@@ -11,11 +11,10 @@ export default function BannerText( props ) {
 		<p>
 			Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg!
 			Am heutigen { currentDayName } bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia
-			zu sichern. {donorsNeededSentence} Sollten Sie zu dem kleinen Kreis
-			derjenigen gehören, die bereits gespendet haben, danken wir Ihnen sehr herzlich.
-			Wikipedia wird durch Spenden von durchschnittlich 22,66&nbsp;€ finanziert. Doch schon mit einer
+			zu sichern. Wikipedia wird durch Spenden von
+			durchschnittlich 22,66&nbsp;€ finanziert. Doch schon mit einer
 			Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich
-			entwickeln. <span className="wmde-banner-text-animated-highlight">Millionen Menschen nutzen Wikipedia,
+			entwickeln. <span className="text-animated-highlight">Millionen Menschen nutzen Wikipedia,
 			aber 99 % spenden nicht – sie übergehen diesen Aufruf.</span> Die meisten Menschen spenden, weil
 			sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse
 			Kaffee geschenkt? Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie
