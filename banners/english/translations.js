e56672f1486e2435605f7bf57e9dc50a31402188
Abban Dunne <abban.dunne@wikimedia.de>
2022-10-07 16:01:48 +0200

    Prepare campaign for C22_WMDE_Test_EN_01
    Update config and text
    
    Ticket: https://phabricator.wikimedia.org/T318651

--- banners/english/translations.js
+++ banners/english/translations.js
@@ -7,14 +7,13 @@ const Translations = {
 	'show-more': 'Show More',
 	'show-less': 'Show Less',
 	'remaining-donors-needed-sentence': 'Noch benötigen wir die Unterstützung von weiteren {{donorsNeeded}} Menschen.',
-	'form-step-2-header': 'Könnten Sie auch {{amount}} € jährlich spenden?',
-	'form-step-2-copy': 'Jedes Jahr sind wir auf die Unterstützung von Menschen wie Ihnen angewiesen.' +
-		' Jährliche Spenden helfen uns nachhaltig und ermöglichen langfristige Weiterentwicklungen.',
-	'form-step-2-no': 'Nein, danke! Ich möchte eine einmalige Spende von {{amount}} € machen.',
-	'form-step-2-yes': 'Ja, ich möchte {{amount}} € jährlich spenden.',
-	'form-step-2-link': 'Ja, ich möchte jährlich spenden, aber einen anderen Betrag.',
-	'form-step-2-button': 'Weiter, um Spende abzuschließen',
-	'form-step-2-error': 'Bitte wählen Sie eine der Optionen aus.'
+	'form-step-2-header': 'Can you make it €{{amount}} yearly?',
+	'form-step-2-copy': 'Every year we are dependent on the support of people like you. Yearly donations help sustainably and enable long term development.',
+	'form-step-2-no': 'No, thanks! I\'ll make a one-time donation of €{{amount}}',
+	'form-step-2-yes': 'Yes, I\'ll donate €{{amount}} each year',
+	'form-step-2-link': 'Yes, I\'ll donate yearly, but for a different amount',
+	'form-step-2-button': 'Proceed with the donation',
+	'form-step-2-error': 'Please choose one of the above options.'
 };
 
 export default Translations;
