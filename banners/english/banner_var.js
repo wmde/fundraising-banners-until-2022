79a1daf0494ac7e8e205851d118a932be9b0744e
Abban Dunne <abban.dunne@wikimedia.de>
2022-10-07 15:59:30 +0200

    Convert English banner to new structure
    Used desktop as the base and added the progress
    bar and English localisations.
    
    Ticket: https://phabricator.wikimedia.org/T318651

--- banners/english/banner_var.js
+++ banners/english/banner_var.js
@@ -7,17 +7,18 @@ import { createTrackingData } from '../../shared/tracking_data';
 import { getTrackingIds } from '../../shared/tracking_ids';
 
 import { Banner } from './components/Banner';
+import { BannerType } from '../../shared/BannerType';
 import BannerPresenter from '../../shared/banner_presenter';
 import Translations from '../../shared/messages/en';
 import LocalTranslations from './translations';
-import DonationForm from './components/ui/form/DonationForm_var';
-import BannerText from './components/BannerText';
-import Footer from '../../shared/components/ui/EasySelectFooter';
+import DonationForm from '../../components/DonationForm/DonationForm';
+import Footer from '../../components/Footer/Footer';
+import BannerText from './content/BannerText';
+import Slides from './content/Slides';
 import useOfFundsText from 'fundraising-frontend-content/i18n/en_GB/data/use_of_funds_content.json';
 import { createCampaignProjection } from '../../shared/campaign_projection';
-import { createFormItems } from './form_items_var';
+import { createFormItems } from './form_items';
 import { LocalImpressionCount } from '../../shared/local_impression_count';
-import { BannerType } from '../../shared/BannerType';
 
 const bannerContainer = document.getElementById( 'WMDE-Banner-Container' );
 const campaignParameters = createCampaignParameters();
@@ -27,7 +28,8 @@ const trackingData = createTrackingData( trackingIds.bannerName );
 const bannerPresenter = new BannerPresenter(
 	trackingData,
 	bannerContainer.dataset.delay || 7500,
-	new LocalImpressionCount( trackingIds.bannerName )
+	new LocalImpressionCount( trackingIds.bannerName ),
+	mw.centralNotice.internal.hide.setHideWithCloseButtonCookies
 );
 
 bannerPresenter.present(
@@ -42,9 +44,11 @@ bannerPresenter.present(
 		donationForm: DonationForm,
 		footer: Footer,
 		bannerText: BannerText,
+		slides: Slides,
 		translations: Object.assign( Translations, LocalTranslations ),
 		formItems: createFormItems( Translations, formatters.amountInputFormatter ),
 		bannerType: BannerType.VAR,
-		formProps: { ast: 2, locale: 'en_GB' }
+		showCookieBanner: '0',
+		initialBannerWidth: window.innerWidth
 	}
 );
