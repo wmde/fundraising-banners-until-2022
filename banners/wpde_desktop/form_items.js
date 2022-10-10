9b63093716a47f02a124efe5c45a1c4bf59cb35a
Abban Dunne <abban.dunne@wikimedia.de>
2022-10-10 10:58:56 +0200

    Move WPDE banners out of subfolder
    Each channel should have it's own top
    level folder.

---
+++ banners/wpde_desktop/form_items.js
@@ -0,0 +1,20 @@
+import FormItemsBuilder, { Intervals, PaymentMethods } from '../../shared/components/ui/form/FormItemsBuilder';
+
+export function createFormItems( translations, amountFormatter ) {
+	const builder = new FormItemsBuilder( translations, amountFormatter );
+	builder.setIntervals(
+		Intervals.ONCE,
+		Intervals.MONTHLY,
+		Intervals.QUARTERLY,
+		Intervals.BIANNUAL,
+		Intervals.YEARLY
+	);
+	builder.setAmounts( 5, 10, 20, 25, 50, 100 );
+	builder.setPaymentMethods(
+		PaymentMethods.DIRECT_DEBIT,
+		PaymentMethods.BANK_TRANSFER,
+		PaymentMethods.CREDIT_CARD,
+		PaymentMethods.PAYPAL
+	);
+	return builder.getItems();
+}
---
+++ banners/wpde_mobile/components/FullpageBanner.jsx
@@ -0,0 +1,62 @@
+import { h, Component } from 'preact';
+import classNames from 'classnames';
+
+import ProgressBar from '../../../shared/components/ui/ProgressBar';
+import BannerText from './BannerText';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class FullpageBanner extends Component {
+	render( props ) {
+		const campaignProjection = props.campaignProjection;
+		const DonationForm = props.donationForm;
+		const trackingParams = `piwik_campaign=${props.campaignName}&piwik_kwd=${props.bannerName}_link`;
+
+		return <div className={ classNames( 'fullpage-banner', { visible: props.isFullPageVisible && props.bannerVisible } ) }>
+			<div className="fullpage-banner__info">
+				<div className="close" onClick={ props.onClose }>
+					{ props.setCookie ? <img src="https://bruce.wikipedia.de/close-banner?c=fundraising" alt="" height="0" width="0"/> : '' }
+				</div>
+				<div className="infobox">
+					<BannerText dynamicCampaignText={ props.dynamicCampaignText }/>
+				</div>
+				<ProgressBar
+					formatters={props.formatters}
+					daysLeft={campaignProjection.getRemainingDays()}
+					donationAmount={campaignProjection.getProjectedDonationSum()}
+					goalDonationSum={campaignProjection.goalDonationSum}
+					missingAmount={campaignProjection.getProjectedRemainingDonationSum()}
+					setStartAnimation={props.setStartAnimation}
+					animate={true}
+				/>
+
+				<div className="call-to-action">
+					Jetzt sind Sie <span className="call-to-action__optional-text">in Deutschland</span> gefragt.
+				</div>
+			</div>
+			<DonationForm
+				formItems={props.formItems}
+				bannerName={props.bannerName}
+				campaignName={props.campaignName}
+				formatters={props.formatters}
+				impressionCounts={props.impressionCounts}
+				customAmountPlaceholder={ props.translations[ 'custom-amount-placeholder-short' ] }
+				onSubmit={props.onSubmit}
+			/>
+			<div className="smallprint">
+				<span>
+					<a href={`https://spenden.wikimedia.de/spenden/Impressum?${ trackingParams }`} target="_blank">Impressum</a>
+				</span>
+				<span className="separator"> | </span>
+				<span>
+					<a href={`https://spenden.wikimedia.de/spenden/Datenschutz?${ trackingParams }`} target="_blank">Datenschutz</a>
+				</span>
+				<span className="separator"> | </span>
+				<span>
+					<a className="application-of-funds-link"
+						href={`https://spenden.wikimedia.de/use-of-funds?${ trackingParams }`}
+						onClick={ props.toggleFundsModal } >Wohin geht meine Spende?</a>
+				</span>
+			</div>
+		</div>;
+	}
+}
