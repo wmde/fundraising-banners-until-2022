380421d2205d8d7f85f823306e777321acf826cd
Tanuja Doriya <tanuja.doriya@wikimedia.de>
2022-10-10 17:56:07 +0200

    Style Countdown bar

--- components/SoftClose/SoftClose.jsx
+++ components/SoftClose/SoftClose.jsx
@@ -1,8 +1,14 @@
 import { h } from 'preact';
+import { useState } from 'preact/hooks';
+import classNames from 'classnames';
 
 export default function SoftClose() {
-	return <div className="wmde-banner-soft-close">
-		<div className="wmde-banner-soft-close-progress-bar"></div>
+	const [ isProgressing, setProgress ] = useState( false );
+	return <div className={ classNames( {
+		'wmde-banner-soft-close': true,
+		'countdown-bar--progressing': isProgressing
+	} )}>
+		<div className="wmde-banner-soft-close-countdown-bar"></div>
 		<div className="wmde-banner-soft-close-columns">
 			<div className="wmde-banner-soft-close-column">
 				<span className="wmde-banner-soft-close-prompt">Vielleicht möchten Sie Wikipedia später unterstützen?</span>
