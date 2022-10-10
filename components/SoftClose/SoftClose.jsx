7e03d93da6e5d61c8e68630a359ff7974c690376
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-10-10 17:01:31 +0200

    Style SoftClose component

--- components/SoftClose/SoftClose.jsx
+++ components/SoftClose/SoftClose.jsx
@@ -5,12 +5,12 @@ export default function SoftClose() {
 		<div className="wmde-banner-soft-close-progress-bar"></div>
 		<div className="wmde-banner-soft-close-columns">
 			<div className="wmde-banner-soft-close-column">
-				Vielleicht möchten Sie Wikipedia später unterstützen?
+				<span className="wmde-banner-soft-close-prompt">Vielleicht möchten Sie Wikipedia später unterstützen?</span>
 				<button className="wmde-banner-soft-close-button">Ja</button>
 				<button className="wmde-banner-soft-close-button">Erstmal nicht</button>
 			</div>
-			<div className="wmde-banner-soft-close-column">
-				Diese Mitteilung wird automatisch in .... Sekunden ausgeblendet.
+			<div className="wmde-banner-soft-close-column wmde-banner-soft-close-countdown-text">
+				Diese Mitteilung wird automatisch in <strong> .... Sekunden</strong> ausgeblendet.
 			</div>
 		</div>
 	</div>;
