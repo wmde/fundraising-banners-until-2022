d4c42963a9b4a41cd92a7778d65b86b9937de15e
Abban Dunne <abban.dunne@wikimedia.de>
2022-10-10 08:15:54 +0200

    Implement VAR for C22_WMDE_Test_EN_01
    Use new beg form for var and add styles to
    handle it to the Goku theme
    
    Ticket: https://phabricator.wikimedia.org/T318651

--- banners/english/content/BannerText.jsx
+++ banners/english/content/BannerText.jsx
@@ -1,9 +1,8 @@
 import { h } from 'preact';
-import getEnglishOrdinalSuffixOf from '../../../shared/english_ordinal';
 import InfoIcon from '../../../components/Icons/InfoIcon';
 
 export default function BannerText( props ) {
-	const { currentDayName, campaignDaySentence, visitorsVsDonorsSentence, overallImpressionCount } = props.dynamicCampaignText;
+	const { currentDayName, campaignDaySentence, visitorsVsDonorsSentence } = props.dynamicCampaignText;
 	const animatedVisitorsVsDonorsSentence = visitorsVsDonorsSentence ? <span className="wmde-banner-text-animated-highlight">{ visitorsVsDonorsSentence }</span> : '';
 
 	return <div>
@@ -11,13 +10,11 @@ export default function BannerText( props ) {
 			<strong><InfoIcon/> To all our readers in Germany, </strong>
 		</p>
 		<p>
-			It might be a little awkward, so we'll get straight to the point. { campaignDaySentence } This { currentDayName }, for
-			the { getEnglishOrdinalSuffixOf( overallImpressionCount ) } time recently, we humbly ask you to defend Wikipedia's
-			independence. 99% of our readers don't give; they simply look the other way. We depend on donations averaging about
-			€21.60. { animatedVisitorsVsDonorsSentence } If you donate just €5,
-			Wikipedia could keep thriving for years. Most people donate because Wikipedia is useful. If Wikipedia has
-			given you €5 worth of knowledge this year, take a minute to donate. Show the volunteers who bring you reliable,
-			neutral information that their work matters. Thank you.
+			It might be a little awkward, so we'll get straight to the point. This { currentDayName } we humbly ask
+			you to defend Wikipedia's independence. { campaignDaySentence } 99% of our readers don't give; they simply
+			look the other way. We depend on donations averaging about €22.66. { animatedVisitorsVsDonorsSentence } If
+			you donate just €5, Wikipedia could keep thriving for years. If Wikipedia has given you €5 worth of
+			knowledge this year, take a minute to donate. Thank you.
 		</p>
 	</div>;
 }
