ea93cbeeb698a75d8317b768f37c5a35386a86e7
Abban Dunne <abban.dunne@wikimedia.de>
2022-10-10 15:06:52 +0200

    Add SoftClose component

---
+++ banners/desktop_soft_close/content/BannerText_var.jsx
@@ -0,0 +1,26 @@
+import { h } from 'preact';
+import InfoIcon from '../../../components/Icons/InfoIcon';
+
+export default function BannerText( props ) {
+	const { currentDayName } = props.dynamicCampaignText;
+
+	return <div>
+		<p>
+			<strong><InfoIcon/> An alle, die Wikipedia in Deutschland nutzen </strong>
+		</p>
+		<p>
+			Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg!
+			Am heutigen { currentDayName } bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia
+			zu sichern. Wikipedia wird durch Spenden von
+			durchschnittlich 22,66&nbsp;€ finanziert. Doch schon mit einer
+			Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich
+			entwickeln. <span className="wmde-banner-text-animated-highlight">Millionen Menschen nutzen Wikipedia,
+			aber 99 % spenden nicht – sie übergehen diesen Aufruf.</span>
+			Wenn alle, die das jetzt lesen, einen kleinen Beitrag leisten, wäre unser Spendenziel bereits in wenigen Stunden erreicht.
+			Die meisten Menschen spenden, weil
+			sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse
+			Kaffee geschenkt? Dann nehmen Sie sich doch bitte eine Minute Zeit und geben Sie
+			etwas zurück. Vielen Dank!
+		</p>
+	</div>;
+}
---
+++ banners/desktop_soft_close/translations.js
@@ -0,0 +1,16 @@
+const Translations = {
+	'intervals-header': 'Ich spende:',
+	'amounts-header': 'Betrag:',
+	'payments-header': 'Zahlweise:',
+	'remaining-donors-needed-sentence': 'Noch benötigen wir die Unterstützung von weiteren {{donorsNeeded}} Menschen.',
+	'form-step-2-header': 'Könnten Sie auch {{amount}} € jährlich spenden?',
+	'form-step-2-copy': 'Jedes Jahr sind wir auf die Unterstützung von Menschen wie Ihnen angewiesen.' +
+		' Jährliche Spenden helfen uns nachhaltig und ermöglichen langfristige Weiterentwicklungen.',
+	'form-step-2-no': 'Nein, danke! Ich möchte eine einmalige Spende von {{amount}} € machen.',
+	'form-step-2-yes': 'Ja, ich möchte {{amount}} € jährlich spenden.',
+	'form-step-2-link': 'Ja, ich möchte jährlich spenden, aber einen anderen Betrag.',
+	'form-step-2-button': 'Weiter, um Spende abzuschließen',
+	'form-step-2-error': 'Bitte wählen Sie eine der Optionen aus.'
+};
+
+export default Translations;
