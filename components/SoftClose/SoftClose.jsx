50ffcfb3291c1fc97f71cac442941cf0c921b408
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-10-18 20:20:56 +0200

    Add documentation for soft-close

--- components/SoftClose/SoftClose.jsx
+++ components/SoftClose/SoftClose.jsx
@@ -1,6 +1,36 @@
 import { Component, h } from 'preact';
+import PropTypes from 'prop-types';
 
+/**
+ * A "mini banner" that displays when the user clicks on the regular "close" button.
+ * It gives the user the option to perform the regular "close" action (setting cookie)
+ * or choosing a "maybe later" action (which does not set the cookie that prevents further banners).
+ *
+ * The component also has a 15 seconds countdown timer. When the user takes no action,
+ * the component triggers the regular "close" action.
+ *
+ * The parent banner is responsible for creating and destroying this component
+ * and passing in the right event handlers.
+ */
 export default class SoftClose extends Component {
+
+	static propTypes = {
+		/**
+		 * Callback when the user clicks the "I really want to close the banner" option
+		 */
+		onCloseBanner: PropTypes.func,
+
+		/**
+		 * Callback when the user clicks the "Maybe later" option
+		 */
+		onMaybeLater: PropTypes.func,
+
+		/**
+		 * Callback when the timer runs out without user choice
+		 */
+		onTimeOutClose: PropTypes.func
+	};
+
 	constructor( props ) {
 		super( props );
 		this.state = {
