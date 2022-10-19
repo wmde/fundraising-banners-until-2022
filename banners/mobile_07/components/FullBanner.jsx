e863857dcd08de8696885198a0dd41a32049d770
Abban Dunne <abban.dunne@wikimedia.de>
2022-10-19 10:01:05 +0200

    Implement mobile soft close

---
+++ banners/mobile_07/components/FullBanner.jsx
@@ -0,0 +1,49 @@
+import { h } from 'preact';
+import classNames from 'classnames';
+
+import CloseIconMobile from '../../../components/Icons/CloseIconMobile';
+import Footer from '../../../components/Footer/Footer';
+
+export default function FullBanner( props ) {
+	const DonationForm = props.donationForm;
+	const BannerText = props.bannerText;
+
+	const scrollToFirstError = e => {
+		e.preventDefault();
+		document.getElementsByClassName( 'wmde-banner-select-group-container--with-error' )[ 0 ]?.scrollIntoView(
+			{ behavior: 'smooth', block: 'center', inline: 'nearest' }
+		);
+	};
+
+	return <div className={ classNames( 'wmde-banner-full', { visible: props.isFullPageVisible } ) }>
+		<button className="wmde-banner-full-close" onClick={ props.onClose }><CloseIconMobile/></button>
+		<div className="wmde-banner-full-info">
+			<BannerText dynamicCampaignText={ props.dynamicCampaignText }/>
+		</div>
+		<div className="wmde-banner-full-call-to-action">
+			Jetzt sind Sie <span className="wmde-banner-full-call-to-action-optional-text">in Deutschland</span> gefragt.
+		</div>
+		<DonationForm
+			formItems={props.formItems}
+			bannerName={props.bannerName}
+			campaignName={props.campaignName}
+			formatters={props.formatters}
+			impressionCounts={props.impressionCounts}
+			customAmountPlaceholder={ props.translations[ 'custom-amount-placeholder-short' ] }
+			onSubmit={props.onSubmit}
+			toggleFundsModal={props.toggleFundsModal}
+			scrollToFirstError={ scrollToFirstError }
+			preselectedAmount={ props.preselectedAmount }
+		/>
+		<div className="wmde-banner-full-small-print">
+			<span>
+				<a className="application-of-funds-link"
+					href={`https://spenden.wikimedia.de/use-of-funds?${ props.trackingParams }`}
+					onClick={ props.toggleFundsModal } >Wohin geht meine Spende?</a>
+			</span>
+		</div>
+
+		<Footer/>
+
+	</div>;
+}
---
+++ banners/mobile_07/content/BannerText.jsx
@@ -0,0 +1,20 @@
+import { h } from 'preact';
+
+export default function BannerText( props ) {
+	const { currentDayName } = props.dynamicCampaignText;
+	return <div className="banner-text">
+		<p className="banner-text-title">
+			<strong>An alle, die Wikipedia in Deutschland nutzen:</strong>
+		</p>
+		<p>Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! Am
+			heutigen { currentDayName } bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia zu
+			sichern. Wikipedia wird durch Spenden von durchschnittlich 22,66&nbsp;€
+			finanziert. Doch schon mit einer Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft
+			erfolgreich entwickeln. <strong>Millionen Menschen nutzen Wikipedia, aber 99&nbsp;% spenden
+			nicht – sie übergehen diesen Aufruf.</strong> Die meisten Menschen spenden, weil sie
+			Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse
+			Kaffee geschenkt? Dann nehmen Sie sich doch bitte eine Minute Zeit und geben
+			Sie etwas zurück. <em>Vielen Dank!</em>
+		</p>
+	</div>;
+}
