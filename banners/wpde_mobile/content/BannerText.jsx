09ae138addd05d5673c1c32d89fb6b6639edcc44
Abban Dunne <abban.dunne@wikimedia.de>
2022-10-26 08:29:57 +0200

    Apply banner text
    Ticket: https://phabricator.wikimedia.org/T320469

--- banners/wpde_mobile/content/BannerText.jsx
+++ banners/wpde_mobile/content/BannerText.jsx
@@ -1,20 +1,23 @@
 import { h } from 'preact';
 
 export default function BannerText( props ) {
-	const { currentDayName } = props.dynamicCampaignText;
+	const { currentDayName, campaignDaySentence, visitorsVsDonorsSentence } = props.dynamicCampaignText;
+	const animatedVisitorsVsDonorsSentence = visitorsVsDonorsSentence ? <span className="wmde-banner-text-animated-highlight">{ visitorsVsDonorsSentence }</span> : '';
+
 	return <div className="banner-text">
 		<p className="banner-text-title">
 			<strong>An alle, die Wikipedia in Deutschland nutzen:</strong>
 		</p>
 		<p>Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! Am
 			heutigen { currentDayName } bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia zu
-			sichern. Wikipedia wird durch Spenden von durchschnittlich 22,66&nbsp;€
-			finanziert. Doch schon mit einer Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft
-			erfolgreich entwickeln. <span className="wmde-banner-text-animated-highlight">Millionen Menschen nutzen
-			Wikipedia, aber 99&nbsp;% spenden nicht – sie übergehen diesen Aufruf.</span> Die meisten
+			sichern. { campaignDaySentence } Insgesamt spenden 99% nichts – sie übergehen diesen Aufruf.
+			Wikipedia wird durch Spenden von durchschnittlich 22,66&nbsp;€ finanziert. Doch schon mit
+			einer Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich
+			entwickeln. { animatedVisitorsVsDonorsSentence } Wenn alle, die das jetzt lesen, einen kleinen
+			Beitrag leisten, wäre unser Spendenziel bereits in wenigen Stunden erreicht. Die meisten
 			Menschen spenden, weil sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr
-			Wissen im Wert einer Tasse Kaffee geschenkt? Dann nehmen Sie sich doch bitte eine Minute Zeit und geben
-			Sie etwas zurück. <em>Vielen Dank!</em>
+			Wissen im Wert einer Tasse Kaffee geschenkt? Dann nehmen Sie sich doch bitte eine Minute
+			Zeit und geben Sie etwas zurück. <em>Vielen Dank!</em>
 		</p>
 	</div>;
 }
--- banners/wpde_mobile/content/Slides.jsx
+++ banners/wpde_mobile/content/Slides.jsx
@@ -1,36 +1,41 @@
 import { h } from 'preact';
 
 export default function Slides( dynamicCampaignText, progressBar ) {
-	const { currentDayName } = dynamicCampaignText;
+	const { currentDayName, goalDonationSum, campaignDaySentence, visitorsVsDonorsSentence } = dynamicCampaignText;
+	const animatedVisitorsVsDonorsSentence = visitorsVsDonorsSentence ? <span className="wmde-banner-slider-text-animated-highlight">{ visitorsVsDonorsSentence }</span> : '';
 
 	return [
 		{
 			content: <div>
-				<p>Unser Spendenziel: n Millionen Euro</p>
+				<p>Unser Spendenziel: { goalDonationSum } Millionen Euro</p>
 				{ progressBar }
 			</div>
 		},
 		{
 			content: <div>
-				<p>Am heutigen { currentDayName } bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia zu sichern.</p>
+				<p>An alle, die Wikipedia in Deutschland nutzen: Vielleicht kommen wir gerade ungelegen,
+					aber dennoch: Klicken Sie jetzt bitte nicht weg! Am heutigen { currentDayName } bitten
+					wir Sie bescheiden, die Unabhängigkeit von Wikipedia zu sichern.</p>
 			</div>
 		},
 		{
 			content: <div>
-				<p>Wikipedia wird durch Spenden von durchschnittlich 22,66&nbsp;€ finanziert. <strong>Doch schon mit einer
-					Spende von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich entwickeln.</strong></p>
+				<p>{ campaignDaySentence } Insgesamt spenden 99% nichts – sie übergehen diesen Aufruf. Wikipedia
+					wird durch Spenden von durchschnittlich 22,66&nbsp;€ finanziert. Doch schon mit einer Spende
+					von 5&nbsp;€ kann Wikipedia sich auch in Zukunft erfolgreich entwickeln.</p>
 			</div>
 		},
 		{
 			content: <div>
-				<p><span className="wmde-banner-slider-text-animated-highlight">Millionen Menschen nutzen Wikipedia, aber 99&nbsp;% spenden nicht – sie übergehen diesen
-					Aufruf.</span> Die meisten Menschen spenden, weil sie Wikipedia nützlich finden.</p>
+				<p>{ animatedVisitorsVsDonorsSentence } Wenn alle, die das jetzt lesen, einen kleinen Beitrag
+					leisten, wäre unser Spendenziel bereits in wenigen Stunden erreicht.</p>
 			</div>
 		},
 		{
 			content: <div>
-				<p>Hat Wikipedia Ihnen in diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? <strong>Dann nehmen
-					Sie sich doch bitte eine Minute Zeit und geben Sie etwas zurück. Vielen Dank!</strong></p>
+				<p>Die meisten Menschen spenden, weil sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in
+					diesem Jahr Wissen im Wert einer Tasse Kaffee geschenkt? <strong>Dann nehmen Sie sich doch
+						bitte eine Minute Zeit und geben Sie etwas zurück. Vielen Dank!</strong></p>
 			</div>
 		}
 	];
