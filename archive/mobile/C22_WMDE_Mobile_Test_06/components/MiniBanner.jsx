aa937d72e75af148693aa653c1e9fa5925e80f26
Abban Dunne <abban.dunne@wikimedia.de>
2022-10-28 08:45:13 +0200

    Archive C22_WMDE_Mobile_Test_06
    Ticket: https://phabricator.wikimedia.org/T318669

---
+++ archive/mobile/C22_WMDE_Mobile_Test_06/components/MiniBanner.jsx
@@ -0,0 +1,44 @@
+import { h } from 'preact';
+import * as PropTypes from 'prop-types';
+import CloseIconMobile from '../../../components/Icons/CloseIconMobile';
+import Slider from '../../../components/Slider/Slider';
+
+export default function MiniBanner( props ) {
+	const Slides = props.slides;
+
+	return <div className="wmde-banner-mini">
+		<div className="wmde-banner-mini-close">
+			<button className="wmde-banner-mini-close-button" onClick={ props.onClose }><CloseIconMobile/></button>
+		</div>
+
+		<header className="wmde-banner-mini-headline">
+			<div className="wmde-banner-mini-headline-background">
+				<span className="wmde-banner-mini-headline-content">Ist Ihnen Wikipedia 5&nbsp;€ wert?</span>
+			</div>
+		</header>
+
+		<div className="wmde-banner-mini-banner-slideshow">
+			<Slider
+				slides={ Slides( props.dynamicCampaignText ) }
+				onSlideChange={ props.onSlideChange }
+				registerAutoplay={ props.registerSliderAutoplayCallbacks }
+				interval={ props.sliderAutoPlaySpeed }
+				sliderOptions={ { loop: false } }
+			/>
+		</div>
+
+		<button className="wmde-banner-mini-button" onClick={ props.onExpandFullpage }>
+			Jetzt spenden
+		</button>
+	</div>;
+}
+
+MiniBanner.propTypes = {
+	slides: PropTypes.element,
+	onClose: PropTypes.func,
+	formatters: PropTypes.object,
+	campaignProjection: PropTypes.any,
+	campaignParameters: PropTypes.object,
+	startAnimation: PropTypes.func,
+	onExpandFullpage: PropTypes.func
+};
---
+++ archive/mobile/C22_WMDE_Mobile_Test_06/translations.js
@@ -0,0 +1,21 @@
+const Translations = {
+	'intervals-header': 'Häufigkeit wählen',
+	'amounts-header': 'Betrag wählen',
+	'payments-header': 'Zahlweise wählen',
+	'minimise-button': 'Vielleicht später',
+	'global-error': 'Bitte oben die Formularfelder ausfüllen.',
+	'address-type-notice-full': 'Nur so können wir Ihnen eine Spendenquittung per Post zusenden. Außerdem erhalten Sie eine Bestätigung per E-Mail.',
+	'address-type-notice-email': '',
+	'address-type-notice-none': 'Sie verzichten sowohl auf eine Spendenquittung als auch auf eine Bestätigung per E-Mail. ' +
+		'Sie erhalten von uns keine Information, wenn Wikipedia wieder Hilfe braucht.',
+	'form-step-2-header': 'Könnten Sie auch {{amount}} € jährlich spenden?',
+	'form-step-2-copy': 'Jedes Jahr sind wir auf die Unterstützung von Menschen wie Ihnen angewiesen.' +
+		' Jährliche Spenden helfen uns nachhaltig und ermöglichen langfristige Weiterentwicklungen.',
+	'form-step-2-no': 'Nein, danke! Ich möchte eine einmalige Spende von {{amount}} € machen.',
+	'form-step-2-yes': 'Ja, ich möchte {{amount}} € jährlich spenden.',
+	'form-step-2-link': 'Ja, ich möchte jährlich spenden, aber einen anderen Betrag.',
+	'form-step-2-button': 'Weiter, um Spende abzuschließen',
+	'form-step-2-error': 'Bitte wählen Sie eine der Optionen aus.'
+};
+
+export default Translations;
