84af0a8f8e18509f650a51e404606040d5fe7154
Abban Dunne <abban.dunne@wikimedia.de>
2022-10-31 12:32:39 +0100

    Update UoF config on C22_WPDE_Test_Mob01

--- banners/wpde_mobile/components/Banner_var.jsx
+++ banners/wpde_mobile/components/Banner_var.jsx
@@ -278,6 +278,7 @@ export default class Banner extends Component {
 				isFundsModalVisible={ this.state.isFundsModalVisible }
 				onCallToAction={ this.fundsModalDonate }
 				useOfFundsText={ props.useOfFundsText }
+				figuresAreProvisional={ props.campaignParameters.useOfFundsProvisional }
 				locale='de'>
 				<FundsDistributionAccordion
 					applicationOfFundsData={ props.useOfFundsText.applicationOfFundsData }
