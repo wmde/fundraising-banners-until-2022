0d9ed0c7b45101125285033817532d116e604478
Abban Dunne <abban.dunne@wikimedia.de>
2022-11-09 14:12:28 +0100

    Implement VAR for C22_WMDE_Test_EN_02
    VAR banner uses the new soft close functionality
    
    Ticket: https://phabricator.wikimedia.org/T322731

--- banners/english/banner_var.js
+++ banners/english/banner_var.js
@@ -1,16 +1,17 @@
 // eslint-disable-next-line no-unused-vars
-import style from './styles/styles.pcss';
+import style from './styles/styles_var.pcss';
 
 import * as formatters from '../../shared/number_formatter/en';
 import { createCampaignParameters } from '../../shared/campaign_parameters';
 import { createTrackingData } from '../../shared/tracking_data';
 import { getTrackingIds } from '../../shared/tracking_ids';
 
-import { Banner } from './components/Banner';
+import { Banner } from './components/Banner_var';
 import { BannerType } from '../../shared/BannerType';
 import BannerPresenter from '../../shared/banner_presenter';
 import Translations from '../../shared/messages/en';
 import LocalTranslations from './translations';
+import TranslationsSoftClose from './translations_soft_close';
 import DonationForm from '../../components/DonationForm/BegYearlyRecurringDonationForm';
 import DonationFormStep2 from '../../components/DonationForm/BegYearlyRecurringDonationFormStep2';
 import Footer from '../../components/Footer/Footer';
@@ -48,7 +49,7 @@ bannerPresenter.present(
 		footer: Footer,
 		bannerText: BannerText,
 		slides: Slides,
-		translations: Object.assign( Translations, LocalTranslations ),
+		translations: Object.assign( Translations, LocalTranslations, TranslationsSoftClose ),
 		formItems: createFormItems( Translations, formatters.amountInputFormatter ),
 		bannerType: BannerType.VAR,
 		showCookieBanner: '0',
