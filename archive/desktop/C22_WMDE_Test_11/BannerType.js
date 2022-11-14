7557d89818b639f783af6cbd4df9eec001663555
Abban Dunne <abban.dunne@wikimedia.de>
2022-11-14 11:44:26 +0100

    Archive C22_WMDE_Test_11
    Ticket: https://phabricator.wikimedia.org/T323021

---
+++ archive/desktop/C22_WMDE_Test_11/BannerType.js
@@ -0,0 +1,4 @@
+export const BannerType = Object.freeze( {
+	CTRL: Symbol( 'ctrl ' ),
+	VAR: Symbol( 'var' )
+} );
---
+++ archive/desktop/C22_WMDE_Test_11/translations_soft_close.js
@@ -0,0 +1,8 @@
+const Translations = {
+	'soft-close-prompt': 'Vielleicht möchten Sie Wikipedia später unterstützen?',
+	'soft-close-button-1': 'Ja',
+	'soft-close-button-2': 'Erstmal nicht',
+	'soft-close-countdown-text': 'Diese Mitteilung wird automatisch in <strong> {{seconds}} Sekunden</strong> ausgeblendet.'
+};
+
+export default Translations;
