b99736c0d0d5d518226d2200d5d664a73ea1d9a3
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2022-11-22 16:42:39 +0100

    Archive C22_WMDE_Test_13
    https://phabricator.wikimedia.org/T323389

---
+++ archive/desktop/C22_WMDE_Test_13/translations.js
@@ -0,0 +1,21 @@
+const Translations = {
+	'intervals-header': 'Ich spende:',
+	'amounts-header': 'Betrag:',
+	'payments-header': 'Zahlweise:',
+	'remaining-donors-needed-sentence': 'Noch benötigen wir die Unterstützung von weiteren {{donorsNeeded}} Menschen.',
+	'form-step-2-header': 'Könnten Sie auch {{amount}} € jährlich spenden?',
+	'form-step-2-copy': 'Jedes Jahr sind wir auf die Unterstützung von Menschen wie Ihnen angewiesen.' +
+		' Jährliche Spenden helfen uns nachhaltig und ermöglichen langfristige Weiterentwicklungen.',
+	'form-step-2-no': 'Nein, danke! Ich möchte eine einmalige Spende von {{amount}} € machen.',
+	'form-step-2-yes': 'Ja, ich möchte {{amount}} € jährlich spenden.',
+	'form-step-2-link': 'Ja, ich möchte jährlich spenden, aber einen anderen Betrag.',
+	'form-step-2-button': 'Weiter, um Spende abzuschließen',
+	'form-step-2-error': 'Bitte wählen Sie eine der Optionen aus.',
+	'form-step-3-header': 'Vielen Dank für Ihre Unterstützung!',
+	'form-step-3-copy': 'Geben Sie Ihren jährlichen Spendenbetrag an.',
+	'form-step-3-button-blank': 'Jährlich spenden',
+	'form-step-3-button': 'Jährlich {{amount}} € spenden',
+	'form-step-3-error': 'Bitte wählen Sie einen Betrag aus.'
+};
+
+export default Translations;
