93618d9bb5320cbedbb9c4542a0abcede068ca09
Abban Dunne <abban.dunne@wikimedia.de>
2022-11-23 13:06:15 +0100

    Hide form legends with CSS
    The form legends were empty and being used as
    visual separators. This isn't very semantic,
    so include the text and hide them with CSS.
    
    Ticket: https://phabricator.wikimedia.org/T318655

--- banners/mobile_english/translations.js
+++ banners/mobile_english/translations.js
@@ -1,8 +1,7 @@
 const Translations = {
-	'intervals-header': '',
-	'amounts-header': '',
-	'payments-header': '',
-	'address-type-header': '',
+	'intervals-header': 'I will donate:',
+	'amounts-header': 'Amount:',
+	'payments-header': 'Using:',
 	'address-type-notice-full': 'Only with address data we are able to send you a donation receipt by mail. You will' +
 		' also receive an e-mail confirmation of your donation.',
 	'address-type-notice-email': 'You waive the donation receipt, but receive an e-mail confirmation of your donation.',
