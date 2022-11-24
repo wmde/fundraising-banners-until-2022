1e8bd47502c9b1605fc72ab735d60eda2660143a
Abban Dunne <abban.dunne@wikimedia.de>
2022-11-24 11:53:54 +0100

    Archive C22_WMDE_Test_EN_03
    Ticket: https://phabricator.wikimedia.org/T323745

---
+++ archive/desktop/C22_WMDE_Test_EN_03/content/Slides.jsx
@@ -0,0 +1,31 @@
+import { h } from 'preact';
+import InfoIcon from '../../../components/Icons/InfoIcon';
+
+export default function Slides( dynamicCampaignText ) {
+	const { currentDayName, campaignDaySentence, visitorsVsDonorsSentence } = dynamicCampaignText;
+	const animatedVisitorsVsDonorsSentence = visitorsVsDonorsSentence ? <span className="wmde-banner-slider-text-animated-highlight">{ visitorsVsDonorsSentence }</span> : '';
+
+	return [
+		{
+			content: <div>
+				<p className="headline"><InfoIcon/> <strong> To all our readers in Germany, </strong></p>
+				<p>It might be a little awkward, so we'll get straight to the point. This { currentDayName } we
+					humbly ask you to defend Wikipedia's independence.</p>
+			</div>
+		},
+		{
+			content: <div>
+				<p>
+					{ campaignDaySentence } 99% of our readers don't give; they simply look the other way. We
+					depend on donations averaging about €22.66. { animatedVisitorsVsDonorsSentence }
+				</p>
+			</div>
+		},
+		{
+			content: <div>
+				<p>If you donate just €5, Wikipedia could keep thriving for years. If Wikipedia has given you
+					€5 worth of knowledge this year, take a minute to donate. Thank you.</p>
+			</div>
+		}
+	];
+}
