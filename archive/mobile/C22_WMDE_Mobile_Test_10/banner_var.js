9ac9daf0d0f236eaa46137e5ede76f7e504d164a
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2022-11-26 12:55:42 +0100

    Archive C22_WMDE_Mobile_Test_10

---
+++ archive/mobile/C22_WMDE_Mobile_Test_10/banner_var.js
@@ -0,0 +1,56 @@
+// eslint-disable-next-line no-unused-vars
+import style from './styles/styles_var.pcss';
+
+import * as formatters from '../../shared/number_formatter/de';
+import { createCampaignParameters } from '../../shared/campaign_parameters';
+import { createTrackingData } from '../../shared/tracking_data';
+import { getTrackingIds } from '../../shared/tracking_ids';
+import BannerPresenter from '../../shared/banner_presenter';
+import Translations from '../../shared/messages/de';
+import LocalTranslations from './translations';
+import TranslationsSoftClose from './translations_soft_close';
+import TranslationsSubscriptionForm from '../../components/SubscriptionForm/translations/de';
+import useOfFundsText from '../../node_modules/fundraising-frontend-content/i18n/de_DE/data/use_of_funds_content.json';
+import DonationForm from '../../components/DonationForm/BegYearlyRecurringDonationFormCompact';
+import FormStep2 from '../../components/DonationForm/BegYearlyRecurringDonationFormStep2MobileCompact';
+import BannerText from './content/BannerText';
+import Slides from './content/Slides';
+
+import Banner from './components/Banner_var';
+
+import { createCampaignProjection } from '../../shared/campaign_projection';
+import { createFormItems } from './form_items';
+import { LocalImpressionCount } from '../../shared/local_impression_count';
+import { BannerType } from '../../shared/BannerType';
+
+const bannerContainer = document.getElementById( 'WMDE-Banner-Container' );
+const campaignParameters = createCampaignParameters();
+const campaignProjection = createCampaignProjection( campaignParameters );
+const trackingIds = getTrackingIds( bannerContainer );
+const trackingData = createTrackingData( trackingIds.bannerName );
+const bannerPresenter = new BannerPresenter(
+	trackingData,
+	bannerContainer.dataset.delay || 5000,
+	new LocalImpressionCount( trackingIds.bannerName )
+);
+
+bannerPresenter.present(
+	Banner,
+	bannerContainer,
+	{
+		...trackingIds,
+		campaignParameters,
+		campaignProjection,
+		formatters,
+		useOfFundsText,
+		sliderAutoPlaySpeed: 5000,
+		translations: Object.assign( Translations, TranslationsSoftClose, TranslationsSubscriptionForm, LocalTranslations ),
+		formItems: createFormItems( Translations, formatters.amountInputFormatter ),
+		bannerType: BannerType.VAR,
+		donationForm: DonationForm,
+		donationFormStep2: FormStep2,
+		bannerText: BannerText,
+		slides: Slides
+	},
+	0
+);
