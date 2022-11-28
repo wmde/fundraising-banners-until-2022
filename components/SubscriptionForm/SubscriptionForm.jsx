543d845c6b918da329981a39f2ad35b11e79e7e9
Abban Dunne <abban.dunne@wikimedia.de>
2022-11-28 08:26:30 +0100

    Update mobile subscription form
    * Add an overlay element to the SubscriptionForm component
    * Fix selectors in Gohan subscription form styles
    * Update styles to handle altered layout
    
    Ticket: https://phabricator.wikimedia.org/T323831

--- components/SubscriptionForm/SubscriptionForm.jsx
+++ components/SubscriptionForm/SubscriptionForm.jsx
@@ -76,37 +76,39 @@ export default class SubscriptionForm extends Component {
 			<a href="#" onClick={ this.toggleModal } className="wmde-banner-subscription-form-link">
 				{ Translations[ 'subscription-form-toggle-link' ] }
 			</a>
-			<form
-				method="post"
-				action={ `https://spenden.wikimedia.de/contact/subscribe?piwik_kwd=${ props.bannerName }&piwik_campaign=${ props.campaignName }` }
-				target="_blank"
-				className="wmde-banner-subscription-form-modal"
-				onSubmit={ this.submitForm }
-			>
-				<button className="wmde-banner-subscription-form-modal-close" onClick={ this.hideModal }><CloseIcon/></button>
-				<div className="wmde-banner-subscription-form-modal-content">
-					<label className="wmde-banner-subscription-form-input">
-						{ Translations[ 'subscription-form-input-label' ] }
-						<input type="text" name="email" onChange={ this.setEmail } placeholder={ Translations[ 'subscription-form-input-placeholder' ] }/>
-					</label>
-					{ state.formWasSubmitted && !state.emailIsValid && ( <div className="wmde-banner-subscription-form-error">
-						{ Translations[ 'subscription-form-error-email' ] }
-					</div> ) }
-					<label className="wmde-banner-subscription-form-checkbox">
-						<input type="checkbox" name="subscription-agree" value="consent-given" onChange={ this.setConsentGiven }/>
-						{ Translations[ 'subscription-form-checkbox-label' ] }
-					</label>
-					{ state.formWasSubmitted && !state.consentGiven && ( <div className="wmde-banner-subscription-form-error">
-						{ Translations[ 'subscription-form-error-privacy' ] }
-					</div> ) }
-					<button className="wmde-banner-subscription-form-submit" type="submit">
-						{ Translations[ 'subscription-form-button' ] }
-					</button>
-					<a className="wmde-banner-subscription-form-privacy" href="https://spenden.wikimedia.de/page/Datenschutz" target="_blank">
-						{ Translations[ 'subscription-form-privacy' ] }
-					</a>
-				</div>
-			</form>
+			<div className="wmde-banner-subscription-form-modal-overlay">
+				<form
+					method="post"
+					action={ `https://spenden.wikimedia.de/contact/subscribe?piwik_kwd=${ props.bannerName }&piwik_campaign=${ props.campaignName }` }
+					target="_blank"
+					className="wmde-banner-subscription-form-modal"
+					onSubmit={ this.submitForm }
+				>
+					<button className="wmde-banner-subscription-form-modal-close" onClick={ this.hideModal }><CloseIcon/></button>
+					<div className="wmde-banner-subscription-form-modal-content">
+						<label className="wmde-banner-subscription-form-input">
+							{ Translations[ 'subscription-form-input-label' ] }
+							<input type="text" name="email" onChange={ this.setEmail } placeholder={ Translations[ 'subscription-form-input-placeholder' ] }/>
+						</label>
+						{ state.formWasSubmitted && !state.emailIsValid && ( <div className="wmde-banner-subscription-form-error">
+							{ Translations[ 'subscription-form-error-email' ] }
+						</div> ) }
+						<label className="wmde-banner-subscription-form-checkbox">
+							<input type="checkbox" name="subscription-agree" value="consent-given" onChange={ this.setConsentGiven }/>
+							{ Translations[ 'subscription-form-checkbox-label' ] }
+						</label>
+						{ state.formWasSubmitted && !state.consentGiven && ( <div className="wmde-banner-subscription-form-error">
+							{ Translations[ 'subscription-form-error-privacy' ] }
+						</div> ) }
+						<button className="wmde-banner-subscription-form-submit" type="submit">
+							{ Translations[ 'subscription-form-button' ] }
+						</button>
+						<a className="wmde-banner-subscription-form-privacy" href="https://spenden.wikimedia.de/page/Datenschutz" target="_blank">
+							{ Translations[ 'subscription-form-privacy' ] }
+						</a>
+					</div>
+				</form>
+			</div>
 		</div>;
 	}
 }
