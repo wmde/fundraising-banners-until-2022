c527b3da456ee15f34d9944c1e58e0c40cd61d89
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2022-12-07 16:01:05 +0100

    Add table structure in dashboard component

--- dashboard/components/Dashboard.jsx
+++ dashboard/components/Dashboard.jsx
@@ -4,5 +4,25 @@ export default function Dashboard( props ) {
 	return <div>
 		<h1>FUN Banner Dashboard</h1>
 		<p>We have { props.campaigns?.length } banners</p>
+
+		<table>
+			{ Object.entries( props.campaigns ).map( ( [ campaignName, campaign ] ) => (
+				<tr key={campaignName}>
+					<td>
+						{ /* TODO put icon here maybe */ }
+						{ campaignName }
+					</td>
+					<td>
+						{ campaign.banners.ctrl.pagename }
+					</td>
+					<td>
+						{ campaign.banners.var?.pagename }
+					</td>
+				</tr>
+			)
+			)}
+
+		</table>
+
 	</div>;
 }
