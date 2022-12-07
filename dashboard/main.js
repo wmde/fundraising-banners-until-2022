cc01e561e2a61eaefa1c7fb578e659fd7cf044a4
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-12-07 17:04:23 +0100

    Add more links and a stylesheet

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,6 +1,8 @@
 import { render, createElement } from "preact";
 import Dashboard from "./components/Dashboard";
 
+import './styles.pcss';
+
 const dashboardContainer = document.getElementById('dashboard');
 
 console.log("campaigns is ", CAMPAIGNS)
