7ad18bf120cba6b3b57f82038250a5120b8729c5
Abban Dunne <abban.dunne@wikimedia.de>
2022-12-08 10:16:21 +0100

    Update dashboard layout

--- dashboard/components/Dashboard.jsx
+++ dashboard/components/Dashboard.jsx
@@ -1,5 +1,8 @@
 import { h } from 'preact';
 import BannerActions from './BannerActions';
+import IconShutterbug from './IconShutterbug';
+import IconGit from './IconGit';
+import IconRefresh from './IconRefresh';
 
 export default function Dashboard( props ) {
 	const gitFailurePrefix = /^UNKNOWN -/;
@@ -12,45 +15,60 @@ export default function Dashboard( props ) {
 			} );
 	};
 
-	let branchInfo = (
-		<div>
-			Current branch {props.gitBranch}
-			<a className='do-screenshots' title="Copy SSH command for screenshots to clipboard" onClick={onDoScreenshots} href="#">📷 </a>
-		</div>
-	);
+	let branchInfo = props.gitBranch;
 	if ( props.gitBranch.match( gitFailurePrefix ) ) {
-		branchInfo = ( <div className="error">Could not get git branch: {props.gitBranch.replace( gitFailurePrefix, '' ) } </div> );
+		branchInfo = props.gitBranch.replace( gitFailurePrefix, '' );
 	}
 
-	return <div>
-		<h1>FUN Forge</h1>
-		{ branchInfo }
-		<table>
-			{ Object.entries( props.campaigns ).map( ( [ campaignName, campaign ] ) => (
-				<tr key={campaignName}>
-					<td className='campaign-column'>
-						{ /* TODO put icon here maybe */ }
-						<a
-							className='shutterbug-preview'
-							title="Show images in Shutterbug"
-							target='_blank'
-							href={`https://shutterbug.wikimedia.de/#/slides/${campaign.campaign_tracking}`}
-						>📷 </a>
-						{ campaignName }
-					</td>
-					<td className='banner-column'>
-						<BannerActions campaign={campaign} banner={ campaign.banners.ctrl } />
-					</td>
-					<td className='banner-column'>
-						{ campaign.banners.var ?
-							<BannerActions campaign={campaign} banner={ campaign.banners.var } /> :
-							'' }
-					</td>
-				</tr>
-			)
-			)}
+	const refresh = e => {
+		e.preventDefault();
+		window.location.reload();
+	};
 
-		</table>
+	return <div>
+		<header className="header">
+			<div className="header-left">
+				<h1>FUN Forge</h1> <a className="header-link header-git" target="_blank" href="https://github.com/wmde/fundraising-banners"><IconGit/> { branchInfo }</a>
+			</div>
+			<div className="header-right">
+				<a className="header-link" title="Copy SSH command for screenshots to clipboard" onClick={onDoScreenshots} href="#">Shutterbug Command</a>
+				<a href="https://meta.wikimedia.org/w/index.php?title=Special:CentralNotice" className="header-link">CN</a>
+				<a href="https://meta.wikimedia.org/wiki/Special:CentralNoticeBanners" className="header-link">CN Banners</a>
+				<a href="#" onClick={ refresh } className="header-link header-link-refresh"><IconRefresh/></a>
+			</div>
+		</header>
+
+		<section className="content">
+			<div className="campaigns">
+				{ Object.entries( props.campaigns ).map( ( [ campaignName, campaign ], index ) => (
+					<div key={ campaignName } className="campaign" style={ '--index: ' + index }>
+						<div className="campaign-title">
+							<a href={`https://shutterbug.wikimedia.de/#/slides/${campaign.campaign_tracking}`}
+								className="link-icon link-icon-large"
+								data-tooltip="View in Shutterbug">
+								<IconShutterbug/>
+							</a>
+							<span>{ campaignName }</span>
+						</div>
+						<div className="campaign-banners">
+							<div className="campaign-banner">
+								<BannerActions campaign={campaign} banner={ campaign.banners.ctrl } />
+							</div>
+							<div className="campaign-banner">
+								<BannerActions campaign={campaign} banner={ campaign.banners.var } />
+							</div>
+						</div>
+					</div>
+				) ) }
+			</div>
+		</section>
+
+		<footer className="footer">
+			<div className="footer-left">Welcome to the Fun Forge, we got fun campaigns!</div>
+			<div className="footer-right">
+				<a href="#" className="footer-link">Docs</a>
+			</div>
+		</footer>
 
 	</div>;
 }
