7a45b0552b160ad31f410b5021945b23b24a6a65
Abban Dunne <abban.dunne@wikimedia.de>
2022-12-09 12:45:51 +0100

    Archive C22_WMDE_ipad_en_Test_01
    Ticket: https://phabricator.wikimedia.org/T324821

---
+++ archive/iPad/C22_WMDE_ipad_en_Test_01/banner_var.js
@@ -0,0 +1,49 @@
+// eslint-disable-next-line no-unused-vars
+import style from './styles/styles_var.pcss';
+
+import * as formatters from '../../shared/number_formatter/en';
+import { createCampaignParameters } from '../../shared/campaign_parameters';
+import { createTrackingData } from '../../shared/tracking_data';
+import { getTrackingIds } from '../../shared/tracking_ids';
+
+import Banner from './components/Banner_var';
+import { BannerType } from '../../shared/BannerType';
+import BannerPresenter from '../../shared/banner_presenter';
+import Translations from '../../shared/messages/en';
+import TranslationsSoftClose from './translations_soft_close';
+import useOfFundsText from 'fundraising-frontend-content/i18n/en_GB/data/use_of_funds_content.json';
+import { createCampaignProjection } from '../../shared/campaign_projection';
+import { createFormItems } from './form_items';
+import { LocalImpressionCount } from '../../shared/local_impression_count';
+import DonationForm from '../../components/DonationForm/DonationForm';
+import Slides from './content/Slides';
+import getBannerDelay from '../../shared/banner_delay';
+
+const bannerContainer = document.getElementById( 'WMDE-Banner-Container' );
+const campaignParameters = createCampaignParameters();
+const campaignProjection = createCampaignProjection( campaignParameters );
+const trackingIds = getTrackingIds( bannerContainer );
+const trackingData = createTrackingData( trackingIds.bannerName );
+const bannerPresenter = new BannerPresenter(
+	trackingData,
+	getBannerDelay( 7500 ),
+	new LocalImpressionCount( trackingIds.bannerName )
+);
+
+bannerPresenter.present(
+	Banner,
+	bannerContainer,
+	{
+		...trackingIds,
+		campaignParameters,
+		campaignProjection,
+		formatters,
+		useOfFundsText,
+		donationForm: DonationForm,
+		translations: Object.assign( Translations, TranslationsSoftClose ),
+		formItems: createFormItems( Translations, formatters.amountInputFormatter ),
+		slides: Slides,
+		bannerType: BannerType.VAR,
+		formActionProps: { locale: 'en_GB' }
+	}
+);
