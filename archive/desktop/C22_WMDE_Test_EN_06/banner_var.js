fcf5b81780fcdbf8588c819e1666fb58cf2f717e
Abban Dunne <abban.dunne@wikimedia.de>
2022-12-14 15:20:28 +0100

    Archive C22_WMDE_Test_EN_06
    Ticket: https://phabricator.wikimedia.org/T325149

---
+++ archive/desktop/C22_WMDE_Test_EN_06/banner_var.js
@@ -0,0 +1,62 @@
+// eslint-disable-next-line no-unused-vars
+import style from './styles/styles.pcss';
+
+import * as formatters from '../../shared/number_formatter/en';
+import { createCampaignParameters } from '../../shared/campaign_parameters';
+import { createTrackingData } from '../../shared/tracking_data';
+import { getTrackingIds } from '../../shared/tracking_ids';
+
+import { Banner } from './components/Banner';
+import { BannerType } from '../../shared/BannerType';
+import BannerPresenter from '../../shared/banner_presenter';
+import Translations from '../../shared/messages/en';
+import LocalTranslations from './translations';
+import TranslationsSoftClose from './translations_soft_close';
+import DonationForm from '../../components/DonationForm/BegYearlyRecurringDonation3StepForm';
+import DonationFormStep2 from '../../components/DonationForm/BegYearlyRecurringDonationFormStep2';
+import DonationFormStep3 from '../../components/DonationForm/BegYearlyRecurringDonationFormStep3CustomAmount';
+import Footer from '../../components/Footer/Footer';
+import BannerText from './content/BannerText_var';
+import Slides from './content/Slides_var';
+import useOfFundsText from 'fundraising-frontend-content/i18n/en_GB/data/use_of_funds_content.json';
+import { createCampaignProjection } from '../../shared/campaign_projection';
+import { createFormItems } from './form_items';
+import { LocalImpressionCount } from '../../shared/local_impression_count';
+import overrideUseOfFundsFigures from '../../shared/override_use_of_funds_figures';
+import getBannerDelay from '../../shared/banner_delay';
+
+const bannerContainer = document.getElementById( 'WMDE-Banner-Container' );
+const campaignParameters = createCampaignParameters();
+const campaignProjection = createCampaignProjection( campaignParameters );
+const trackingIds = getTrackingIds( bannerContainer );
+const trackingData = createTrackingData( trackingIds.bannerName );
+const bannerPresenter = new BannerPresenter(
+	trackingData,
+	getBannerDelay( 7500 ),
+	new LocalImpressionCount( trackingIds.bannerName ),
+	mw.centralNotice.internal.hide.setHideWithCloseButtonCookies
+);
+
+bannerPresenter.present(
+	Banner,
+	bannerContainer,
+	{
+		...trackingIds,
+		campaignParameters,
+		campaignProjection,
+		formatters,
+		useOfFundsText: overrideUseOfFundsFigures( useOfFundsText, campaignParameters.useOfFundsFigures ),
+		donationForm: DonationForm,
+		donationFormStep2: DonationFormStep2,
+		donationFormStep3: DonationFormStep3,
+		footer: Footer,
+		bannerText: BannerText,
+		slides: Slides,
+		translations: Object.assign( Translations, LocalTranslations, TranslationsSoftClose ),
+		formItems: createFormItems( Translations, formatters.amountInputFormatter ),
+		bannerType: BannerType.VAR,
+		showCookieBanner: '0',
+		initialBannerWidth: window.innerWidth,
+		formActionProps: { locale: 'en_GB' }
+	}
+);
---
+++ archive/desktop/C22_WMDE_Test_EN_06/translations.js
@@ -0,0 +1,25 @@
+const Translations = {
+	'intervals-header': 'I will donate:',
+	'amounts-header': 'Amount:',
+	'payments-header': 'Using:',
+	'next-button': 'Continue',
+	'back-button': 'Back',
+	'show-more': 'Show More',
+	'show-less': 'Show Less',
+	'remaining-donors-needed-sentence': 'Noch benötigen wir die Unterstützung von weiteren {{donorsNeeded}} Menschen.',
+	'form-step-2-header': 'Can you make it €{{amount}} yearly?',
+	'form-step-2-copy': 'Every year we are dependent on the support of people like you. Yearly donations help sustainably and enable long term development.',
+	'form-step-2-no': 'No, thanks! I\'ll make a one-time donation of €{{amount}}.',
+	'form-step-2-yes': 'Yes, I\'ll donate €{{amount}} each year.',
+	'form-step-2-link': 'Yes, I\'ll donate yearly, but for a different amount.',
+	'form-step-2-button': 'Proceed with the donation',
+	'form-step-2-error': 'Please choose one of the above options.',
+
+	'form-step-3-header': 'Thank you for your support!',
+	'form-step-3-copy': 'Enter your annual donation amount.',
+	'form-step-3-button-blank': 'Donate annually',
+	'form-step-3-button': 'Donate €{{amount}} annually',
+	'form-step-3-error': 'Please choose an amount.'
+};
+
+export default Translations;
