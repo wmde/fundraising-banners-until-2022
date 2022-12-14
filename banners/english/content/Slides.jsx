604f393c9bd1cb71a43d2bd956482b4a34b94445
Abban Dunne <abban.dunne@wikimedia.de>
2022-12-14 15:24:14 +0100

    Prepare campaign for C22_WMDE_Test_EN_07
    Uses CTRL from test 06
    
    Ticket: https://phabricator.wikimedia.org/T325149

--- banners/english/content/Slides.jsx
+++ banners/english/content/Slides.jsx
@@ -2,32 +2,33 @@ import { h } from 'preact';
 import InfoIcon from '../../../components/Icons/InfoIcon';
 
 export default function Slides( dynamicCampaignText ) {
-	const { currentDayName } = dynamicCampaignText;
+	const { currentDayName, currentDate } = dynamicCampaignText;
 
 	return [
 		{
 			content: <div>
 				<p className="headline"><InfoIcon/> <strong> To all our readers in Germany, </strong></p>
-				<p>This { currentDayName } your support is requested by the nonprofit that collects donations
-					on Wikipedia to support free knowledge. If Wikipedia has given you €5 worth of knowledge
-					this year, please join the readers who donate.</p>
+				<p>This { currentDayName } { currentDate }, with only a few days before 2022 comes to an end, we
+					humbly ask for your support. We are the nonprofit that collects donations on Wikipedia to
+					support free knowledge. If you can, please join the 1% of readers who give.</p>
 			</div>
 		},
 		{
 			content: <div>
 				<p>
-					These donations support the technology that makes our projects possible, and help us provide
-					resources to the groups who build local communities of contributors to create our millions of
-					articles and images. They also help us advocate for public policy to advance the cause of free
-					knowledge worldwide and defend information access in countries struggling with censorship.
+					<span className="wmde-banner-slider-text-animated-highlight">If everyone reading this right now
+						gave just €5, we'd hit our annual goal in a couple of hours.</span> The price of a cup of
+					coffee is all we ask. It's easy to ignore our messages, but we hope you'll take one minute to
+					think about how useful Wikipedia is in your life.
 				</p>
 			</div>
 		},
 		{
 			content: <div>
-				<p><span className="wmde-banner-slider-text-animated-highlight">Today, we invite you to donate €5 or
-					whatever seems right to you.</span> Show the world that free access to independent and unbiased
-					information matters to you. Thank you for your help — <i>Wikimedia Fördergesellschaft</i></p>
+				<p>Online, Wikipedia is the closest thing we have to a public park or a library where everyone can
+					go to learn. If Wikipedia has given you €5 worth of knowledge this year, please support the
+					technology that makes our projects possible and advance the cause of free knowledge
+					worldwide. — <i>Wikimedia Fördergesellschaft</i></p>
 			</div>
 		}
 	];
