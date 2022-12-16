ab5079ec65c7622a0ecfabb5315033750cfd8264
Abban Dunne <abban.dunne@wikimedia.de>
2022-12-16 15:38:02 +0100

    Archive C22_WMDE_Test_21
    Ticket: https://phabricator.wikimedia.org/T325356

---
+++ archive/desktop/C22_WMDE_Test_21/FormController_var.js
@@ -0,0 +1,107 @@
+import { Intervals, PaymentMethods } from '../../shared/components/ui/form/FormItemsBuilder';
+import { Alternatives } from '../../components/DonationForm/hooks/use_alternative';
+
+let formModel = {};
+let submitCallback = () => {};
+let nextCallback = () => {};
+let backCallback = () => {};
+let goToStepCallback = () => {};
+
+let thirdPageAmount = '';
+
+const submitStep = ( step, extraData ) => {
+	const [ paymentInterval, setPaymentInterval, , ] = formModel.interval;
+	const [ paymentMethod, , , ] = formModel.paymentMethod;
+
+	switch ( step ) {
+		case 1:
+			if ( paymentInterval !== Intervals.ONCE.value || paymentMethod === PaymentMethods.SOFORT.value ) {
+				goToStepCallback( 4 );
+				return;
+			}
+			nextCallback();
+			break;
+		case 2:
+			if ( extraData.upgradeToYearly === Alternatives.YES ) {
+				setPaymentInterval( Intervals.YEARLY.value );
+			}
+			goToStepCallback( 4 );
+			break;
+		case 3:
+			thirdPageAmount = extraData.numericThirdPageAmount;
+			setPaymentInterval( Intervals.YEARLY.value );
+			goToStepCallback( 4 );
+			break;
+		case 4:
+			submitCallback();
+			break;
+	}
+};
+
+const goNext = () => {
+	nextCallback();
+};
+
+const goBack = ( step ) => {
+	const [ , setPaymentInterval, , ] = formModel.interval;
+	switch ( step ) {
+		case 2:
+			setPaymentInterval( Intervals.ONCE.value );
+			break;
+		case 4:
+			// Going back to step 1 is probably the best for all 3 possible user flows
+			goToStepCallback( 1 );
+			return;
+	}
+	backCallback();
+};
+
+const goToStep = ( step ) => {
+	goToStepCallback( step );
+};
+
+const getSubmitValues = () => {
+	const [ addressType, , , ] = formModel.addressType;
+	const [ { numericAmount } ] = formModel.amount;
+	const [ paymentInterval, , , ] = formModel.interval;
+	const [ paymentMethod, , , ] = formModel.paymentMethod;
+
+	return {
+		addressType: addressType,
+		amount: thirdPageAmount === '' ? numericAmount : thirdPageAmount,
+		interval: paymentInterval,
+		paymentMethod: paymentMethod
+	};
+};
+
+const onNext = callback => {
+	nextCallback = callback;
+};
+
+const onBack = callback => {
+	backCallback = callback;
+};
+
+const onSubmit = callback => {
+	submitCallback = callback;
+};
+
+const onGoToStep = callback => {
+	goToStepCallback = callback;
+};
+
+export default function createFormController( withFormModel ) {
+	formModel = withFormModel;
+
+	return {
+		onNext,
+		onBack,
+		onSubmit,
+		onGoToStep,
+		submitStep,
+		goNext,
+		goBack,
+		goToStep,
+		getSubmitValues
+	};
+}
---
+++ archive/desktop/C22_WMDE_Test_21/banner_var.js
@@ -0,0 +1,66 @@
+// eslint-disable-next-line no-unused-vars
+import style from './styles/styles_var.pcss';
+
+import * as formatters from '../../shared/number_formatter/de';
+import { createCampaignParameters } from '../../shared/campaign_parameters';
+import { createTrackingData } from '../../shared/tracking_data';
+import { getTrackingIds } from '../../shared/tracking_ids';
+
+import { Banner } from './components/Banner';
+import { BannerType } from '../../shared/BannerType';
+import BannerPresenter from '../../shared/banner_presenter';
+import Translations from '../../shared/messages/de';
+import LocalTranslations from './translations_var';
+import TranslationsSoftClose from '../../components/SoftClose/translations/de';
+import TranslationsAlreadyDonated from '../../components/AlreadyDonatedModal/translations/de';
+import DonationForm from '../../components/MultistepDonationForm/MultistepDonationForm';
+import Donation from '../../components/MultistepDonationForm/forms/Donation';
+import UpgradeToYearly from '../../components/MultistepDonationForm/forms/UpgradeToYearly';
+import CustomAmount from '../../components/MultistepDonationForm/forms/CustomAmount';
+import AddressType from '../../components/MultistepDonationForm/forms/AddressType';
+import Footer from '../../components/Footer/FooterAlreadyDonated';
+import BannerText from './content/BannerText';
+import Slides from './content/Slides';
+import AlreadyDonated from './content/AlreadyDonated';
+import useOfFundsText from 'fundraising-frontend-content/i18n/de_DE/data/use_of_funds_content.json';
+import { createCampaignProjection } from '../../shared/campaign_projection';
+import { createFormItems } from './form_items_var';
+import { LocalImpressionCount } from '../../shared/local_impression_count';
+import createFormController from './FormController_var';
+import getBannerDelay from '../../shared/banner_delay';
+
+const bannerContainer = document.getElementById( 'WMDE-Banner-Container' );
+const campaignParameters = createCampaignParameters();
+const campaignProjection = createCampaignProjection( campaignParameters );
+const trackingIds = getTrackingIds( bannerContainer );
+const trackingData = createTrackingData( trackingIds.bannerName );
+const bannerPresenter = new BannerPresenter(
+	trackingData,
+	getBannerDelay( 7500 ),
+	new LocalImpressionCount( trackingIds.bannerName ),
+	mw.centralNotice.internal.hide.setHideWithCloseButtonCookies
+);
+
+bannerPresenter.present(
+	Banner,
+	bannerContainer,
+	{
+		...trackingIds,
+		campaignParameters,
+		campaignProjection,
+		formatters,
+		useOfFundsText,
+		donationForm: DonationForm,
+		donationForms: [ Donation, UpgradeToYearly, CustomAmount, AddressType ],
+		createFormController: createFormController,
+		footer: Footer,
+		bannerText: BannerText,
+		slides: Slides,
+		alreadyDonatedContent: AlreadyDonated,
+		translations: Object.assign( Translations, TranslationsSoftClose, TranslationsAlreadyDonated, LocalTranslations ),
+		formItems: createFormItems( Translations, formatters.amountInputFormatter ),
+		bannerType: BannerType.VAR,
+		showCookieBanner: '0',
+		initialBannerWidth: window.innerWidth
+	}
+);
