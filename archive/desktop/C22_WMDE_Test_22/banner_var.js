3733c58fdb86ffa92a907807a8a9c83eebe7ff33
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2022-12-20 15:49:01 +0100

    Archive C22_WMDE_Test_22
    https://phabricator.wikimedia.org/T325619

---
+++ archive/desktop/C22_WMDE_Test_22/banner_var.js
@@ -0,0 +1,67 @@
+// eslint-disable-next-line no-unused-vars
+import style from './styles/styles_var.pcss';
+
+import * as formatters from '../../shared/number_formatter/de';
+import { createCampaignParameters } from '../../shared/campaign_parameters';
+import { createTrackingData } from '../../shared/tracking_data';
+import { getTrackingIds } from '../../shared/tracking_ids';
+
+import { Banner } from './components/Banner_var';
+import { BannerType } from '../../shared/BannerType';
+import BannerPresenter from '../../shared/banner_presenter';
+import Translations from '../../shared/messages/de';
+import LocalTranslations from './translations';
+import TranslationsSoftClose from './soft_close_var';
+import TranslationsAlreadyDonated from '../../components/AlreadyDonatedModal/translations/de';
+import DonationForm from '../../components/MultistepDonationForm/MultistepDonationForm';
+import Donation from '../../components/MultistepDonationForm/forms/Donation';
+import UpgradeToYearly from '../../components/MultistepDonationForm/forms/UpgradeToYearly';
+import CustomAmount from '../../components/MultistepDonationForm/forms/CustomAmount';
+import Footer from '../../components/Footer/FooterAlreadyDonated';
+import BannerText from './content/BannerText';
+import Slides from './content/Slides';
+import SoftCloseContent from './content/SoftCloseContent_var';
+import AlreadyDonated from './content/AlreadyDonated';
+import useOfFundsText from 'fundraising-frontend-content/i18n/de_DE/data/use_of_funds_content.json';
+import { createCampaignProjection } from '../../shared/campaign_projection';
+import { createFormItems } from './form_items';
+import { LocalImpressionCount } from '../../shared/local_impression_count';
+import createFormController from './FormController';
+import getBannerDelay from '../../shared/banner_delay';
+
+const bannerContainer = document.getElementById( 'WMDE-Banner-Container' );
+const campaignParameters = createCampaignParameters();
+const campaignProjection = createCampaignProjection( campaignParameters );
+const trackingIds = getTrackingIds( bannerContainer );
+const trackingData = createTrackingData( trackingIds.bannerName );
+const bannerPresenter = new BannerPresenter(
+	trackingData,
+	getBannerDelay( 7500 ),
+	new LocalImpressionCount( trackingIds.bannerName ),
+	mw.centralNotice.internal.hide.setHideWithCloseButtonCookies
+);
+
+bannerPresenter.present(
+	Banner,
+	bannerContainer,
+	{
+		...trackingIds,
+		campaignParameters,
+		campaignProjection,
+		formatters,
+		useOfFundsText,
+		donationForm: DonationForm,
+		donationForms: [ Donation, UpgradeToYearly, CustomAmount ],
+		createFormController: createFormController,
+		footer: Footer,
+		bannerText: BannerText,
+		slides: Slides,
+		softCloseContent: SoftCloseContent,
+		alreadyDonatedContent: AlreadyDonated,
+		translations: Object.assign( Translations, TranslationsSoftClose, TranslationsAlreadyDonated, LocalTranslations ),
+		formItems: createFormItems( Translations, formatters.amountInputFormatter ),
+		bannerType: BannerType.VAR,
+		showCookieBanner: '0',
+		initialBannerWidth: window.innerWidth
+	}
+);
