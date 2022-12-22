c66cc4401f8cb766c9721d4f6ccb1ae361f88645
Sperling-0 <tanuja.doriya@wikimedia.de>
2022-12-22 12:19:26 +0100

    Archive C22_WMDE_Test_23
    Ticket: https://phabricator.wikimedia.org/T325720

---
+++ archive/desktop/C22_WMDE_Test_23/form_items.js
@@ -0,0 +1,19 @@
+import FormItemsBuilder, { Intervals, PaymentMethods } from '../../components/DonationForm/FormItemsBuilder';
+
+export function createFormItems( translations, amountFormatter ) {
+	const builder = new FormItemsBuilder( translations, amountFormatter );
+	builder.setIntervals(
+		Intervals.ONCE,
+		Intervals.MONTHLY,
+		Intervals.QUARTERLY,
+		Intervals.YEARLY
+	);
+	builder.setAmounts( 5, 10, 20, 25, 50, 100 );
+	builder.setPaymentMethods(
+		PaymentMethods.PAYPAL,
+		PaymentMethods.BANK_TRANSFER,
+		PaymentMethods.DIRECT_DEBIT,
+		PaymentMethods.CREDIT_CARD
+	);
+	return builder.getItems();
+}
