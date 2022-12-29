509df39246f8a26cb3142b99a208e4cc61fcf62d
Abban Dunne <abban.dunne@wikimedia.de>
2022-12-29 12:37:39 +0100

    Prepare campign for C22_WMDE_Test_27
    * Uses VAR banner from test 26
    * Campaign day sentence now has a variant for second last day
    
    Ticket: https://phabricator.wikimedia.org/T326004

--- banners/desktop/content/BannerText.jsx
+++ banners/desktop/content/BannerText.jsx
@@ -12,11 +12,11 @@ export default function BannerText( props ) {
 		<p>
 			Vielleicht kommen wir gerade ungelegen, aber dennoch: Klicken Sie jetzt bitte nicht weg! Am
 			heutigen { currentDayName } bitten wir Sie bescheiden, die Unabhängigkeit von Wikipedia zu
-			unterstützen. { campaignDaySentence } Wikipedia wird durch Spenden von durchschnittlich 22,66&nbsp;€
-			finanziert. Doch bereits eine Spende von 5&nbsp;€ hilft uns weiter. { animatedVisitorsVsDonorsSentence } Die
-			meisten Menschen spenden, weil sie Wikipedia nützlich finden. Hat Wikipedia Ihnen in diesem Jahr
-			Wissen im Wert einer Tasse Kaffee geschenkt? Dann entscheiden Sie sich, eine der seltenen Ausnahmen
-			zu sein, und geben Sie etwas zurück. Vielen Dank!
+			unterstützen. { campaignDaySentence } { animatedVisitorsVsDonorsSentence } Die meisten Menschen
+			spenden, weil sie Wikipedia nützlich finden. Die durchschnittliche Spende beträgt 22,66&nbsp;€,
+			doch bereits eine Spende von 5&nbsp;€ hilft uns weiter. Hat Wikipedia Ihnen in diesem Jahr Wissen
+			im Wert einer Tasse Kaffee geschenkt? Dann entscheiden Sie sich, eine der seltenen Ausnahmen zu
+			sein, und geben Sie etwas zurück. Vielen Dank!
 		</p>
 	</div>;
 }
