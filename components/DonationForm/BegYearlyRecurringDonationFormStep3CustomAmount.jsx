bd6bbaaf385a30f8235d97857f416afbac8ab274
Sperling-0 <tanuja.doriya@wikimedia.de>
2023-01-25 14:14:51 +0100

    Add missed selector for donation form step-3 submit button

--- components/DonationForm/BegYearlyRecurringDonationFormStep3CustomAmount.jsx
+++ components/DonationForm/BegYearlyRecurringDonationFormStep3CustomAmount.jsx
@@ -51,7 +51,7 @@ export default function BegYearlyRecurringDonationFormStep3CustomAmount( props )
 		</div>
 
 		<div className="wmde-banner-form-button-container form-step-3-button">
-			<button tabIndex="-1" className="wmde-banner-form-button" type="submit">
+			<button tabIndex="-1" className="wmde-banner-form-button t-submit-custom-amount" type="submit">
 				{ props.numericAmount > 0 ?
 					Translations[ 'form-step-3-button' ].replace( '{{amount}}', props.amount ) :
 					Translations[ 'form-step-3-button-blank' ] }
